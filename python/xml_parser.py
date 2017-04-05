896f5e4857b53ce21ab91da2099aeb2c9bccaac2
Hajime Osako <h.osako@bookmaker.com.au>
2017-04-05 20:26:20 +1000

    simple xml parser to compare two xml files

---
+++ python/xml_parser.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Tiny proxy for testing connection
+# Based on https://code.google.com/p/python-proxy/
+#
+
+import sys, pprint
+import xml.etree.ElementTree
+
+
+class XmlParser:
+    @staticmethod
+    def fatal(reason):
+        sys.stderr.write("FATAL " + reason + '\n')
+        sys.exit(1)
+
+    @staticmethod
+    def err(reason, level="ERROR"):
+        sys.stderr.write(level + " " + reason + '\n')
+        raise
+
+    @staticmethod
+    def xml2dict(filename, parent_element_name='property', key_element_name='name', value_element_name='value'):
+        rtn = {}
+        r = xml.etree.ElementTree.parse(filename).getroot()
+        for p in r.findall('.//' + parent_element_name):
+            try:
+                name = str(p.find(".//" + key_element_name).text).strip()
+                value = str(p.find(".//" + value_element_name).text).strip()
+                if len(name) > 0:
+                    rtn[name] = value
+            except:
+                XmlParser.err(name+" does not have value")
+        return rtn
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        XmlParser.fatal('Usage: ' + sys.argv[0] + ' file1.xml [file2.xml]')
+
+    f1 = XmlParser.xml2dict(sys.argv[1])
+
+    if len(sys.argv) == 2:
+        pprint.pprint(f1)
+        sys.exit(0)
+
+    f2 = XmlParser.xml2dict(sys.argv[2])
+    # create a list contains unique keys
+    f1k_and_f2k_u = list(set(f1.keys() + f2.keys()))
+
+    out = {}
+    for k in f1k_and_f2k_u:
+        if not k in f1 or not k in f2 or f1[k] != f2[k]:
+            if not k in f1:
+                out[k] = [None, f2[k]]
+            elif not k in f2:
+                out[k] = [f1[k], None]
+            else:
+                out[k] = [f1[k], f2[k]]
+
+    pprint.pprint(out, width=1)
