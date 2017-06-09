3191a26c34c1bd0946f64b96f6ed95989dfe2016
hajimeo <git@hajigle.com>
2017-06-09 17:09:27 +1000

    Added environmental varaible as a flag

--- java/SocketLocalAddrTest.java
+++ java/SocketLocalAddrTest.java
@@ -3,10 +3,23 @@ import java.net.Socket;
 
 public class SocketLocalAddrTest {
     public static void main(String[] args) throws Exception {
+        Socket socket=null;
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
 
-        Socket socket = new Socket(hostname, port);
+        String local_addr_ip = System.getenv("HADOOP_LOCAL_ADDR_IP");
+
+        if (args.length > 2) {
+            InetAddress local_addr = InetAddress.getByName(args[2]);
+            socket = new Socket(hostname, port, local_addr, 0);
+        }
+        else if (local_addr_ip != null) {
+            InetAddress local_addr = InetAddress.getByName(local_addr_ip);
+            socket = new Socket(hostname, port, local_addr, 0);
+        }
+        else {
+            socket = new Socket(hostname, port);
+        }
         InetAddress ia = socket.getLocalAddress();
         System.out.println(ia);
     }
