684ba672c2e4226d05798b18f0e932358fa74be8
hajimeo <git@hajigle.com>
2017-09-11 10:34:51 +1000

    added yum install

--- bash/start_sandbox.sh
+++ bash/start_sandbox.sh
@@ -279,6 +279,8 @@ if [ "$0" = "$BASH_SOURCE" ]; then
         # (optional) Fixing public hostname (169.254.169.254 issue) by appending public_hostname.sh"
         docker exec -it ${_NAME} bash -c 'grep "^public_hostname_script" /etc/ambari-agent/conf/ambari-agent.ini || ( echo -e "#!/bin/bash\necho \`hostname -f\`" > /var/lib/ambari-agent/public_hostname.sh && chmod a+x /var/lib/ambari-agent/public_hostname.sh && sed -i.bak "/run_as_user/i public_hostname_script=/var/lib/ambari-agent/public_hostname.sh\n" /etc/ambari-agent/conf/ambari-agent.ini )'
 
+        docker exec -d ${_NAME} yum -y install yum-utils sudo which vim net-tools strace lsof tcpdump openldap-clients nc
+
         echo "Resetting Ambari password (type 'admin' twice) ..."
         docker exec -it ${_NAME} /usr/sbin/ambari-admin-password-reset
     else
