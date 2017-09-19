4a42e7a4d483b741cafb63aef3804699220bce38
Hajime Osako <h.osako@bookmaker.com.au>
2017-09-20 09:20:25 +1000

    resetting root user's password

--- bash/start_sandbox.sh
+++ bash/start_sandbox.sh
@@ -287,6 +287,7 @@ if [ "$0" = "$BASH_SOURCE" ]; then
         if [ -s  ~/.ssh/id_rsa.pub ]; then
             docker exec -it ${_NAME} bash -c "grep -q \"^`cat ~/.ssh/id_rsa.pub`\" /root/.ssh/authorized_keys || echo \"`cat ~/.ssh/id_rsa.pub`\" >> ~/.ssh/authorized_keys"
         fi
+        docker exec -it ${_NAME} bash -c "chpasswd <<< root:hadoop"
 
         #echo "Resetting Ambari Agent just incase ..."
         #docker exec -it ${_NAME} /usr/sbin/ambari-agent stop
