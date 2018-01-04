eb9c62d73ac00d61937ae2364f82f44b8effd306
hajimeo <git@hajigle.com>
2018-01-04 10:41:21 +1000

    refactor _WORK_DIR to _work_dir (local)

--- bash/hwx_triage.sh
+++ bash/hwx_triage.sh
@@ -47,42 +47,44 @@ Get more help for a function
 
 function f_check_system() {
     local __doc__="Execute OS commands for performance issue"
-    [ -z "$_WORK_DIR" ] && _WORK_DIR="."
+    local _work_dir="${1-$_WORK_DIR}"
+    [ -z "$_work_dir" ] && _work_dir="."
     echo "INFO" "Collecting OS related information..." >&2
 
-    uname -a &> ${_WORK_DIR%/}/uname-a.out
-    hdp-select &> ${_WORK_DIR%/}/hdp-select.out
-    ls -l /usr/hdp/current/ >> ${_WORK_DIR%/}/hdp-select.out
-    ls -l /etc/security/keytabs/ &> ${_WORK_DIR%/}/ls-keytabs.out
-    getenforce &> ${_WORK_DIR%/}/getenforce.out
-    iptables -t nat -nL &> ${_WORK_DIR%/}/iptables.out
-    (which timeout && (timeout 3 time head -n 1 /dev/urandom > /dev/null;echo '-';timeout 3 time head -n 1 /dev/random > /dev/null)) &> ${_WORK_DIR%/}/random.out
-    vmstat 1 3 &> ${_WORK_DIR%/}/vmstat.out &
-    vmstat -d &> ${_WORK_DIR%/}/vmstat_d.out &
-    pidstat -dl 3 3 &> ${_WORK_DIR%/}/pstat.out &
-    sysctl -a &> ${_WORK_DIR%/}/sysctl.out
-    top -b -n 1 -c &> ${_WORK_DIR%/}/top.out
-    ps auxwwwf &> ${_WORK_DIR%/}/ps.out
-    netstat -aopen &> ${_WORK_DIR%/}/netstat.out
-    netstat -s &> ${_WORK_DIR%/}/netstat_s.out
-    ifconfig &> ${_WORK_DIR%/}/ifconfig.out
-    nscd -g &> ${_WORK_DIR%/}/nscd.out
-    getent ahostsv4 `hostname -f` &> ${_WORK_DIR%/}/getent_from_name.out
-    getent hosts `hostname -I` &> ${_WORK_DIR%/}/getent_from_ip.out
-    python -c 'import socket;print socket.getfqdn()' &> ${_WORK_DIR%/}/python_getfqdn.out
-    mount &> ${_WORK_DIR%/}/mount_df.out
-    df -h &> ${_WORK_DIR%/}/mount_df.out
-    sar -qrbd -p &> ${_WORK_DIR%/}/sar_qrbd.out # if no -p, ls -l /dev/sd*
-    cat /proc/net/dev &> ${_WORK_DIR%/}/net_dev.out
-    cat /proc/cpuinfo &> ${_WORK_DIR%/}/cpuinfo.out
-    cat /proc/meminfo &> ${_WORK_DIR%/}/meminfo.out
+    uname -a &> ${_work_dir%/}/uname-a.out
+    hdp-select &> ${_work_dir%/}/hdp-select.out
+    ls -l /usr/hdp/current/ >> ${_work_dir%/}/hdp-select.out
+    ls -l /etc/security/keytabs/ &> ${_work_dir%/}/ls-keytabs.out
+    getenforce &> ${_work_dir%/}/getenforce.out
+    iptables -t nat -nL &> ${_work_dir%/}/iptables.out
+    (which timeout && (timeout 3 time head -n 1 /dev/urandom > /dev/null;echo '-';timeout 3 time head -n 1 /dev/random > /dev/null)) &> ${_work_dir%/}/random.out
+    vmstat 1 3 &> ${_work_dir%/}/vmstat.out &
+    vmstat -d &> ${_work_dir%/}/vmstat_d.out &
+    pidstat -dl 3 3 &> ${_work_dir%/}/pstat.out &
+    sysctl -a &> ${_work_dir%/}/sysctl.out
+    top -b -n 1 -c &> ${_work_dir%/}/top.out
+    ps auxwwwf &> ${_work_dir%/}/ps.out
+    netstat -aopen &> ${_work_dir%/}/netstat.out
+    netstat -s &> ${_work_dir%/}/netstat_s.out
+    ifconfig &> ${_work_dir%/}/ifconfig.out
+    nscd -g &> ${_work_dir%/}/nscd.out
+    getent ahostsv4 `hostname -f` &> ${_work_dir%/}/getent_from_name.out
+    getent hosts `hostname -I` &> ${_work_dir%/}/getent_from_ip.out
+    python -c 'import socket;print socket.getfqdn()' &> ${_work_dir%/}/python_getfqdn.out
+    mount &> ${_work_dir%/}/mount_df.out
+    df -h &> ${_work_dir%/}/mount_df.out
+    sar -qrbd -p &> ${_work_dir%/}/sar_qrbd.out # if no -p, ls -l /dev/sd*
+    cat /proc/net/dev &> ${_work_dir%/}/net_dev.out
+    cat /proc/cpuinfo &> ${_work_dir%/}/cpuinfo.out
+    cat /proc/meminfo &> ${_work_dir%/}/meminfo.out
     wait
 }
 
 function f_check_process() {
     local __doc__="Execute PID related commands (jstack, jstat, jmap)"
     local _p="$1"	# Java PID ex: `cat /var/run/kafka/kafka.pid`
-    [ -z "$_WORK_DIR" ] && _WORK_DIR="."
+    local _work_dir="${2-$_work_dir}"
+    [ -z "$_work_dir" ] && _work_dir="."
 
     if [ -z "$_p" ]; then
         echo "ERROR" "No PID" >&2; return 1
@@ -96,46 +98,48 @@ function f_check_process() {
     fi
 
     echo "INFO" "Collecting PID related information..." >&2
-    su - $_user -c 'klist -eaf' &> ${_WORK_DIR%/}/klist_${_user}.out
+    su - $_user -c 'klist -eaf' &> ${_work_dir%/}/klist_${_user}.out
 
-    cat /proc/${_p}/limits &> ${_WORK_DIR%/}/proc_limits_${_p}.out
-    cat /proc/${_p}/status &> ${_WORK_DIR%/}/proc_status_${_p}.out
-    date > ${_WORK_DIR%/}/proc_io_${_p}.out; cat /proc/${_p}/io >> ${_WORK_DIR%/}/proc_io_${_p}.out
-    cat /proc/${_p}/environ | tr '\0' '\n' > ${_WORK_DIR%/}/proc_environ_${_p}.out
-    lsof -nPp ${_p} &> ${_WORK_DIR%/}/lsof_${_p}.out
+    cat /proc/${_p}/limits &> ${_work_dir%/}/proc_limits_${_p}.out
+    cat /proc/${_p}/status &> ${_work_dir%/}/proc_status_${_p}.out
+    date > ${_work_dir%/}/proc_io_${_p}.out; cat /proc/${_p}/io >> ${_work_dir%/}/proc_io_${_p}.out
+    cat /proc/${_p}/environ | tr '\0' '\n' > ${_work_dir%/}/proc_environ_${_p}.out
+    lsof -nPp ${_p} &> ${_work_dir%/}/lsof_${_p}.out
 
     if [ -d "$_cmd_dir" ]; then
         local _java_home="$(dirname $_cmd_dir)"
-        zipgrep CryptoAllPermission "$_java_home/jre/lib/security/local_policy.jar" &> ${_WORK_DIR%/}/jce_${_p}.out
-        grep "^securerandom.source=" "$_java_home/jre/lib/security/java.security" &> ${_WORK_DIR%/}/java_random_${_p}.out
+        zipgrep CryptoAllPermission "$_java_home/jre/lib/security/local_policy.jar" &> ${_work_dir%/}/jce_${_p}.out
+        grep "^securerandom.source=" "$_java_home/jre/lib/security/java.security" &> ${_work_dir%/}/java_random_${_p}.out
 
         # NO heap dump at this moment
-        [ -x "${_cmd_dir}/jmap" ] && sudo -u ${_user} ${_cmd_dir}/jmap -histo ${_p} &> ${_WORK_DIR%/}/jmap_histo_${_p}.out
-        top -Hb -n 3 -d 3 -p ${_p} &> ${_WORK_DIR%/}/top_${_p}.out &    # printf "%x\n" [PID]
-        [ -x "${_cmd_dir}/jstack" ] && for i in {1..3};do sudo -u ${_user} ${_cmd_dir}/jstack -l ${_p}; sleep 3; done &> ${_WORK_DIR%/}/jstack_${_p}.out &
-        [ -x "${_cmd_dir}/jstat" ] && sudo -u ${_user} ${_cmd_dir}/jstat -gccause ${_p} 1000 9 &> ${_WORK_DIR%/}/jstat_${_p}.out &
+        [ -x "${_cmd_dir}/jmap" ] && sudo -u ${_user} ${_cmd_dir}/jmap -histo ${_p} &> ${_work_dir%/}/jmap_histo_${_p}.out
+        top -Hb -n 3 -d 3 -p ${_p} &> ${_work_dir%/}/top_${_p}.out &    # printf "%x\n" [PID]
+        [ -x "${_cmd_dir}/jstack" ] && for i in {1..3};do sudo -u ${_user} ${_cmd_dir}/jstack -l ${_p}; sleep 3; done &> ${_work_dir%/}/jstack_${_p}.out &
+        [ -x "${_cmd_dir}/jstat" ] && sudo -u ${_user} ${_cmd_dir}/jstat -gccause ${_p} 1000 9 &> ${_work_dir%/}/jstat_${_p}.out &
     fi
 
-    #ps -eLo user,pid,lwp,nlwp,ruser,pcpu,stime,etime,args | grep -w "${_p}" &> ${_WORK_DIR%/}/pseLo_${_p}.out
-    pmap -x ${_p} &> ${_WORK_DIR%/}/pmap_${_p}.out
+    #ps -eLo user,pid,lwp,nlwp,ruser,pcpu,stime,etime,args | grep -w "${_p}" &> ${_work_dir%/}/pseLo_${_p}.out
+    pmap -x ${_p} &> ${_work_dir%/}/pmap_${_p}.out
     wait
-    date >> ${_WORK_DIR%/}/proc_io_${_p}.out; cat /proc/${_p}/io >> ${_WORK_DIR%/}/proc_io_${_p}.out
+    date >> ${_work_dir%/}/proc_io_${_p}.out; cat /proc/${_p}/io >> ${_work_dir%/}/proc_io_${_p}.out
 }
 
 function f_collect_config() {
     local __doc__="Collect HDP all config files and generate tgz file"
-    [ -z "$_WORK_DIR" ] && _WORK_DIR="."
+    local _work_dir="${1-$_WORK_DIR}"
+    [ -z "$_work_dir" ] && _work_dir="."
 
     echo "INFO" "Collecting HDP config files ..." >&2
     # no 'v' at this moment
-    tar czhf ${_WORK_DIR%/}/hdp_all_conf_$(hostname)_$(date +"%Y%m%d%H%M%S").tgz /usr/hdp/current/*/conf /etc/{ams,ambari}-* /etc/ranger/*/policycache /etc/hosts /etc/krb5.conf 2>/dev/null
+    tar czhf ${_work_dir%/}/hdp_all_conf_$(hostname)_$(date +"%Y%m%d%H%M%S").tgz /usr/hdp/current/*/conf /etc/{ams,ambari}-* /etc/ranger/*/policycache /etc/hosts /etc/krb5.conf 2>/dev/null
 }
 
 function f_collect_log_files() {
     local __doc__="Collect log files for past x days (default is 1 day) and generate tgz file"
     local _path="$1"
     local _day="${2-1}"
-    [ -z "$_WORK_DIR" ] && _WORK_DIR="."
+    local _work_dir="${3-$_WORK_DIR}"
+    [ -z "$_work_dir" ] && _work_dir="."
 
     if [ -z "$_path" ] || [ ! -d "$_path" ]; then
         echo "ERROR" "$_path is not a directory" >&2; return 1
@@ -143,22 +147,23 @@ function f_collect_log_files() {
 
     echo "INFO" "Collecting log files under ${_path} for past ${_day} day(s) ..." >&2
 
-    tar czhf ${_WORK_DIR%/}/hdp_log_$(hostname)_$(date +"%Y%m%d%H%M%S").tar.gz `find -L "${_path}" -type f -mtime -${_day}` 2>&1 | grep -v 'Removing leading'
+    tar czhf ${_work_dir%/}/hdp_log_$(hostname)_$(date +"%Y%m%d%H%M%S").tar.gz `find -L "${_path}" -type f -mtime -${_day}` 2>&1 | grep -v 'Removing leading'
     # grep -i "killed process" /var/log/messages* # TODO: should we also check OOM Killer?
 }
 
 function f_collect_webui() {
     local __doc__="TODO: Collect Web UI with wget"
     local _url="$1"
-    [ -z "$_WORK_DIR" ] && _WORK_DIR="."
+    local _work_dir="${1-$_WORK_DIR}"
+    [ -z "$_work_dir" ] && _work_dir="."
 
     if ! which wget &>/dev/null ; then
         echo "ERROR" "No wget in the PATH." >&2; return 1
     fi
 
     local _d="collect_webui"
-    mkdir ${_WORK_DIR%/}/$_d
-    wget -r -P${_WORK_DIR%/}/$_d -X logs -l 3 -t 1 -k --restrict-file-names=windows -E --no-check-certificate -o ${_WORK_DIR%/}/$_d/collect_webui_wget.log "$_url"
+    mkdir ${_work_dir%/}/$_d
+    wget -r -P${_work_dir%/}/$_d -X logs -l 3 -t 1 -k --restrict-file-names=windows -E --no-check-certificate -o ${_work_dir%/}/$_d/collect_webui_wget.log "$_url"
 }
 
 function f_collect_host_info_from_ambari() {
@@ -171,6 +176,7 @@ function f_collect_host_info_from_ambari() {
     local _date_end_string="$6"    # eg: "now" (or blank = now)
     local _protocol="${7-http}"    # if https, change to https
     local _ambari_port="${8-8080}" # if no default port
+    local _work_dir="${9-$_WORK_DIR}"
 
     local _cmd_opts="-s -k -u ${_admin}"
     [ -z "${_admin_pass}" ] || _cmd_opts="${_cmd_opts}:${_admin_pass}"
@@ -179,7 +185,7 @@ function f_collect_host_info_from_ambari() {
     [ -z "$_date_end_string" ] && _date_end_string="now"
     [ -z "$_protocol" ] && _protocol="http"
     [ -z "$_ambari_port" ] && _ambari_port="8080"
-    [ -z "$_WORK_DIR" ] && _WORK_DIR="."
+    [ -z "$_work_dir" ] && _work_dir="."
     echo "INFO" "Collecting ${_node} information from Ambari..." >&2
 
     local _ambari="`grep '^hostname=' /etc/ambari-agent/conf/ambari-agent.ini | cut -d= -f2`"
@@ -195,7 +201,7 @@ function f_collect_host_info_from_ambari() {
     fi
 
     # Looks like Ambari 2.5.x doesn't care of the case (lower or upper or mix) for hostname
-    curl ${_cmd_opts} "${_href}/hosts/${_node}" -o ${_WORK_DIR%/}/ambari_${_node}.json
+    curl ${_cmd_opts} "${_href}/hosts/${_node}" -o ${_work_dir%/}/ambari_${_node}.json
 
     # If no python, not collecting detailed metrics at this moment
     which python &>/dev/null || return
@@ -213,14 +219,14 @@ for k,v in a['metrics'].iteritems():
 print ','.join(r)"
 
     echo "INFO" "Collecting ${_node} metric from Ambari..." >&2
-    local _fields="`cat ${_WORK_DIR%/}/ambari_${_node}.json | python -c "${_script}"`"
-    [ -z "$_fields" ] || curl ${_cmd_opts} "${_href}/hosts/${_node}" -G --data-urlencode "fields=${_fields}" -o ${_WORK_DIR%/}/ambari_${_node}_metrics.json
+    local _fields="`cat ${_work_dir%/}/ambari_${_node}.json | python -c "${_script}"`"
+    [ -z "$_fields" ] || curl ${_cmd_opts} "${_href}/hosts/${_node}" -G --data-urlencode "fields=${_fields}" -o ${_work_dir%/}/ambari_${_node}_metrics.json
 
     if [ ! -z "$_comp" ]; then
         echo "INFO" "Collecting ${_node} ${_comp} metric from Ambari..." >&2
-        curl ${_cmd_opts} "${_href}/hosts/${_node}/host_components/${_comp^^}" -o ${_WORK_DIR%/}/ambari_${_node}_${_comp}.json
-        _fields="`cat ${_WORK_DIR%/}/ambari_${_node}_${_comp}.json | python -c "${_script}"`"
-        [ -z "$_fields" ] || curl ${_cmd_opts} "${_href}/hosts/${_node}/host_components/${_comp^^}" -G --data-urlencode "fields=${_fields}" -o ${_WORK_DIR%/}/ambari_${_node}_${_comp}_metrics.json
+        curl ${_cmd_opts} "${_href}/hosts/${_node}/host_components/${_comp^^}" -o ${_work_dir%/}/ambari_${_node}_${_comp}.json
+        _fields="`cat ${_work_dir%/}/ambari_${_node}_${_comp}.json | python -c "${_script}"`"
+        [ -z "$_fields" ] || curl ${_cmd_opts} "${_href}/hosts/${_node}/host_components/${_comp^^}" -G --data-urlencode "fields=${_fields}" -o ${_work_dir%/}/ambari_${_node}_${_comp}_metrics.json
     fi
 }
 
@@ -244,7 +250,11 @@ function f_tar_work_dir() {
 
     echo "INFO" "Creating ${_tar_file_path} file ..." >&2
 
-    tar --remove-files -czf ${_tar_file_path} ${_work_dir%/}/* 2>&1 | grep -v 'Removing leading'
+    if [ "${_work_dir}" = "./hwx_triage" ]; then
+        tar --remove-files -czf ${_tar_file_path} ${_work_dir%/}/* 2>&1 | grep -v 'Removing leading'
+    else
+        tar -czf ${_tar_file_path} ${_work_dir%/}/* 2>&1 | grep -v 'Removing leading'
+    fi
 }
 
 
