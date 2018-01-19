37500816f9e9b25b3a7377a3ae66ebb8588a71cc
hajimeo <git@hajigle.com>
2018-01-19 12:19:08 +1000

    no point of sleeping 5 seconds

--- bash/start_sandbox.sh
+++ bash/start_sandbox.sh
@@ -367,8 +367,7 @@ If you would like to fix this now, press Ctrl+c to stop (sleep 7 seconds)"
         docker exec -it ${_NAME} bash -c 'grep -q "^public_hostname_script" /etc/ambari-agent/conf/ambari-agent.ini || ( echo -e "#!/bin/bash\necho \`hostname -f\`" > /var/lib/ambari-agent/public_hostname.sh && chmod a+x /var/lib/ambari-agent/public_hostname.sh && sed -i.bak "/run_as_user/i public_hostname_script=/var/lib/ambari-agent/public_hostname.sh\n" /etc/ambari-agent/conf/ambari-agent.ini )'
         docker exec -it ${_NAME} bash -c "ambari-agent reset ${_HOSTNAME}"
 
-        echo "Resetting Ambari password (to 'admin') ..."
-        sleep 5
+        echo "Resetting Ambari password (to 'admin') and hostname to ${_HOSTNAME}..."
         #docker exec -it ${_NAME} /usr/sbin/ambari-admin-password-reset
         docker exec -it ${_NAME} bash -c "PGPASSWORD=bigdata psql -Uambari -tAc \"UPDATE users SET user_password='538916f8943ec225d97a9a86a2c6ec0818c1cd400e09e03b660fdaaec4af29ddbb6f2b1033b81b00', active=1 WHERE user_name='admin' and user_type='LOCAL';\""
         #docker exec -it ${_NAME} bash -c "PGPASSWORD=bigdata psql -Uambari -tAc \"UPDATE metainfo SET metainfo_value = '${_AMBARI_VERSION}' where metainfo_key = 'version';\""
