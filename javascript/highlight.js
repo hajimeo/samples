9aefc40f20d37d0d5bfc04d42bfeefd3b1bb36e3
Hajime Osako <h.osako@bookmaker.com.au>
2018-02-09 10:36:42 +1000

    added console.log

--- javascript/highlight.js
+++ javascript/highlight.js
@@ -1,32 +1,19 @@
 /**
  * Based on https://www.eriwen.com/javascript/highlight-search-results-with-js/
  */
-function highlight(searchWordsString) {
-    // Starting node, parent to all nodes you want to search
-    var textContainerNode = document.getElementsByTagName("body");
-
-    // Split search terms on '|' and iterate over resulting array
+function highlight(searchWordsString, textContainerNode) {
     var searchWords = searchWordsString.split(' ');
-    for (var i in searchWords) 	{
-        // The regex is the secret, it prevents text within tag declarations to be affected
-        var regex = new RegExp(">([^<]*)?("+searchWords[i]+")([^>]*)?<","ig");
+    for (var i in searchWords) {
+        var regex = new RegExp(">([^<]*)?(" + searchWords[i] + ")([^>]*)?<", "ig");
         highlightTextNodes(textContainerNode, regex, i);
-        // Add to info-string
     }
-
-    // Insert as very first child in searched node
-    textContainerNode.insertBefore(searchTermDiv, textContainerNode.childNodes[0]);
 }
 
-function highlightTextNodes(element, regex, termid) {
-    var tempinnerHTML = element.innerHTML;
+function highlightTextNodes(element, regex, i) {
     // TODO: up to 3 colors
-    var colors = ['background-color: #161633', 'background-color: #331616', 'background-color: #163316'];
-    var i = termid % colors.length;
-    // Do regex replace
-    // Inject span with class of 'highlighted termX' for google style highlighting
-    element.innerHTML = tempinnerHTML.replace(regex,'>$1<span style="'+colors[i]+'">$2</span>$3<');
+    var tmp_elm_html = element.innerHTML;
+    var colors = ['background-color:#fcfa9f', 'background-color:#9ff6fc', 'background-color:#b0fc9f'];
+    var i2 = i % colors.length;
+    console.log("DEBUG: i="+i+ " i2="+i2+" style="+colors[i2]+" regex="+regex+" innerHTML.len="+tmp_elm_html.length);
+    element.innerHTML = tmp_elm_html.replace(regex, '>$1<span style="' + colors[i2] + '">$2</span>$3<');
 }
-
-selected = "" + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text);
-highlight(prompt("Find:", selected));
