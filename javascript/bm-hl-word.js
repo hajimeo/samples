63196e3aac022c5dbbaa506de62a0d0fd2256f5e
hajimeo <git@hajigle.com>
2018-02-09 11:21:19 +1000

    added TODO

--- javascript/bm-hl-word.js
+++ javascript/bm-hl-word.js
@@ -1,4 +1,6 @@
-javascript:var highlight = function (searchWordsString, textContainerNode) {
+javascript:var cls="_hltr_";
+var highlight = function (searchWordsString, textContainerNode) {
+    /* TODO: remove highlights which has cls */
     var searchWords = searchWordsString.split(' ');
     for (var i in searchWords) {
         var regex = new RegExp(">([^<]*)?\\b("+searchWords[i]+")\\b([^>]*)?<", "ig");
@@ -11,7 +13,7 @@ var highlightTextNodes = function (element, regex, i) {
     var colors = ['background-color:#fcfa9f', 'background-color:#9ff6fc', 'background-color:#b0fc9f'];
     var i2 = i % colors.length;
     console.log("DEBUG: i="+i+ " i2="+i2+" style="+colors[i2]+" regex="+regex+" innerHTML.len="+tmp_elm_html.length);
-    element.innerHTML = tmp_elm_html.replace(regex, '>$1<span style="' + colors[i2] + '">$2</span>$3<');
+    element.innerHTML = tmp_elm_html.replace(regex, '>$1<span style="' + colors[i2] + '" class="'+cls+'">$2</span>$3<');
 };
 var selected = "" + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text);
 var sw = prompt("Highlighting words:", selected);
