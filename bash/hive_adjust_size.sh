5b048d30cb553345f956327b3c5b0a1e44f00bd7
hajimeo <git@hajigle.com>
2018-02-09 12:39:17 +1000

    Added proxyuser to * based on feedback. Also deleting temp file created by configs.py

--- bash/hive_adjust_size.sh
+++ bash/hive_adjust_size.sh
@@ -21,14 +21,23 @@ fi
 
 _CMD="/var/lib/ambari-server/resources/scripts/configs.py -a set -l localhost -n ${_CLUSTER} -u admin -p ${_ADM_PWD}"
 ${_CMD} -c yarn-site -k yarn.scheduler.minimum-allocation-mb -v 256
+
 ${_CMD} -c tez-site -k tez.am.resource.memory.mb -v 512
 ${_CMD} -c tez-site -k tez.task.resource.memory.mb -v 512
 ${_CMD} -c tez-site -k tez.runtime.io.sort.mb -v 256
 ${_CMD} -c tez-site -k tez.runtime.unordered.output.buffer.size-mb -v 48
+
 ${_CMD} -c hive-site -k hive.tez.container.size -v 512
 ${_CMD} -c hive-site -k tez.am.resource.memory.mb -v 512
+
 ${_CMD} -c hive-env -k hive.heapsize -v 1024
 ${_CMD} -c hive-env -k hive.metastore.heapsize -v 512
 
+${_CMD} -c core-site -k hadoop.proxyuser.hcat.hosts -v '*'
+${_CMD} -c core-site -k hadoop.proxyuser.hive.hosts -v '*'
+${_CMD} -c core-site -k hadoop.proxyuser.root.hosts -v '*'
+
 echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") INFO Restarting all required services..." 1>&2
 curl -s -u admin:"${_ADM_PWD}" -H "X-Requested-By:ambari" "http://`hostname -f`:8080/api/v1/clusters/${_CLUSTER}/requests" -X POST --data '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}'
+
+rm -f ./doSet_version*
