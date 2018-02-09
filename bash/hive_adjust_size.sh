28dabb1b0933c0a377e53152beb68edce0def4d7
hajimeo <git@hajigle.com>
2018-02-09 12:40:02 +1000

    minor rm command changge

--- bash/hive_adjust_size.sh
+++ bash/hive_adjust_size.sh
@@ -40,4 +40,4 @@ ${_CMD} -c core-site -k hadoop.proxyuser.root.hosts -v '*'
 echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") INFO Restarting all required services..." 1>&2
 curl -s -u admin:"${_ADM_PWD}" -H "X-Requested-By:ambari" "http://`hostname -f`:8080/api/v1/clusters/${_CLUSTER}/requests" -X POST --data '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}'
 
-rm -f ./doSet_version*
+rm -f ./doSet_*.json
