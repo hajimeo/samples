5b4ed3d2888f19e2249c6795a68f687a80af6492
hajimeo <git@hajigle.com>
2018-02-09 11:02:41 +1000

    working version

---
+++ javascript/bm-hl-word.js
@@ -0,0 +1,19 @@
+javascript:var highlight = function (searchWordsString, textContainerNode) {
+    var searchWords = searchWordsString.split(' ');
+    for (var i in searchWords) {
+        var regex = new RegExp(">([^<]*)?\\b("+searchWords[i]+")\\b([^>]*)?<", "ig");
+        highlightTextNodes(textContainerNode, regex, i);
+    }
+};
+var highlightTextNodes = function (element, regex, i) {
+    /* TODO: up to 3 colors */
+    var tmp_elm_html = element.innerHTML;
+    var colors = ['background-color:#fcfa9f', 'background-color:#9ff6fc', 'background-color:#b0fc9f'];
+    var i2 = i % colors.length;
+    console.log("DEBUG: i="+i+ " i2="+i2+" style="+colors[i2]+" regex="+regex+" innerHTML.len="+tmp_elm_html.length);
+    element.innerHTML = tmp_elm_html.replace(regex, '>$1<span style="' + colors[i2] + '">$2</span>$3<');
+};
+var selected = "" + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text);
+var sw = prompt("Highlighting words:", selected);
+highlight(sw, document.body);
+void 1;
