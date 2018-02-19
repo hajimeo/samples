eff111cc89959ce25a610d5aa1beb53be7f035d6
hajimeo <git@hajigle.com>
2018-02-19 10:56:06 +1000

    Added HDFS Audit (and TODO: Druid) Refactored to execute all quieries with one session.

--- bash/hive_dummies.sh
+++ bash/hive_dummies.sh
@@ -7,53 +7,68 @@
 #   bash ./hive_dummies.sh [dbname] [beeline conn str (No -n or -p)]
 #
 
-_dbname="${1}"
-[ -z "$_dbname" ] && _dbname="dummies"
-_beeline_u="${2}"
+g_LOG_FILE_PATH=""
+g_WORK_DIR="./hive_workspace"
 
-[ -d ./hive_workspace ] || mkdir ./hive_workspace
-rm -f ./hive_workspace/*.csv
+function _log() {
+    if [ -n "$g_LOG_FILE_PATH" ]; then
+        echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] $@" | tee -a $g_LOG_FILE_PATH
+    else
+        echo -e "[$(date +'%Y-%m-%d %H:%M:%S')] $@" 1>&2
+    fi
+}
 
-echo "[$(date +"%Y-%m-%d %H:%M:%S %z")] INFO: generating dummy csv files..."
-wget -nv -c -t 2 --timeout=10 --waitretry=3 https://raw.githubusercontent.com/hajimeo/samples/master/misc/sample_07.csv -O ./hive_workspace/sample_07.csv
-wget -nv -c -t 2 --timeout=10 --waitretry=3 https://raw.githubusercontent.com/hajimeo/samples/master/misc/sample_08.csv -O ./hive_workspace/sample_08.csv
-wget -nv -c -t 2 --timeout=10 --waitretry=3 https://raw.githubusercontent.com/hajimeo/samples/master/misc/census.csv -O ./hive_workspace/census.csv
-echo '101,Kyle,Admin,50000,A
-102,Xander,Admin,50000,B
-103,Jerome,Sales,60000,A
-104,Upton,Admin,50000,C
-105,Ferris,Admin,50000,C
-106,Stewart,Tech,12000,A
-107,Chase,Tech,12000,B
-108,Malik,Engineer,45000,B
-109,Samson,Admin,50000,A
-110,Quinlan,Manager,40000,A
-111,Joseph,Manager,40000,B
-112,Axel,Sales,60000,B
-113,Robert,Manager,40000,A
-114,Cairo,Engineer,45000,A
-115,Gavin,Ceo,100000,D
-116,Vaughan,Manager,40000,B
-117,Drew,Engineer,45000,D
-118,Quinlan,Admin,50000,B
-119,Gabriel,Engineer,45000,A
-120,Palmer,Ceo,100000,A' > ./hive_workspace/employee.csv
+### Main ###############################################################################################################
+if [ "$0" = "$BASH_SOURCE" ]; then
+    # Process arguments
+    _dbname="${1}"
+    [ -z "$_dbname" ] && _dbname="dummies"
+    _beeline_u="${2}"
+    _cmd="hive"
+    if [ -n "${_beeline_u}" ]; then
+        _cmd="beeline --verbose=true --outputformat=tsv2 -u '${_beeline_u}'"
+    fi
 
-echo "[$(date +"%Y-%m-%d %H:%M:%S %z")] INFO: executing hive queries under ${_dbname} database... kinit may require"
-# -hiveconf hive.root.logger=INFO,console
-_cmd="hive"
-if [ -n "${_beeline_u}" ]; then
-    _cmd="beeline --verbose=true --outputformat=tsv2 -u '${_beeline_u}'"
-fi
+    # Prepare (create a work dir and remove old csv files)
+    [ -d "${g_WORK_DIR%/}" ] || mkdir ${g_WORK_DIR%/}
+    rm -f ${g_WORK_DIR%/}/*.csv
+    _sql="CREATE DATABASE IF NOT EXISTS ${_dbname};USE ${_dbname};"
 
-# To support Beeline, uploading into HDFS
-hdfs dfs -mkdir /tmp/hive_workspace
-hdfs dfs -put -f ./hive_workspace/*.csv /tmp/hive_workspace/
+    _log "INFO" "generating dummy csv files..."
+    wget -nv -c -t 2 --timeout=10 --waitretry=3 https://raw.githubusercontent.com/hajimeo/samples/master/misc/sample_07.csv -O ${g_WORK_DIR%/}/sample_07.csv
+    wget -nv -c -t 2 --timeout=10 --waitretry=3 https://raw.githubusercontent.com/hajimeo/samples/master/misc/sample_08.csv -O ${g_WORK_DIR%/}/sample_08.csv
+    wget -nv -c -t 2 --timeout=10 --waitretry=3 https://raw.githubusercontent.com/hajimeo/samples/master/misc/census.csv -O ${g_WORK_DIR%/}/census.csv
+    echo '101,Kyle,Admin,50000,A
+    102,Xander,Admin,50000,B
+    103,Jerome,Sales,60000,A
+    104,Upton,Admin,50000,C
+    105,Ferris,Admin,50000,C
+    106,Stewart,Tech,12000,A
+    107,Chase,Tech,12000,B
+    108,Malik,Engineer,45000,B
+    109,Samson,Admin,50000,A
+    110,Quinlan,Manager,40000,A
+    111,Joseph,Manager,40000,B
+    112,Axel,Sales,60000,B
+    113,Robert,Manager,40000,A
+    114,Cairo,Engineer,45000,A
+    115,Gavin,Ceo,100000,D
+    116,Vaughan,Manager,40000,B
+    117,Drew,Engineer,45000,D
+    118,Quinlan,Admin,50000,B
+    119,Gabriel,Engineer,45000,A
+    120,Palmer,Ceo,100000,A' > ${g_WORK_DIR%/}/employee.csv
 
-$_cmd -e "
-set hive.tez.exec.print.summary;
-CREATE DATABASE IF NOT EXISTS ${_dbname};
-USE ${_dbname};
+    # To support Beeline, uploading into HDFS
+    _log "INFO" "Uploading csv files into HDFS ..."
+    hdfs dfs -mkdir /tmp/hive_workspace
+    hdfs dfs -chmod -R 777 /tmp/hive_workspace
+    hdfs dfs -put -f ${g_WORK_DIR%/}/*.csv /tmp/hive_workspace/
+
+    #_log "INFO" "executing hive queries under ${_dbname} database... kinit may require"
+    _log "INFO" "Generating SQLs against ${_dbname} database... "
+    # -hiveconf hive.root.logger=INFO,console -hiveconf hive.tez.exec.print.summary=true
+    _sql="${_sql}
 CREATE TABLE IF NOT EXISTS sample_07 (
   code string,
   description string,
@@ -118,14 +133,44 @@ INSERT OVERWRITE TABLE census_clus select * from census;
 # set hive.exec.max.dynamic.partitions.pernode=4;
 # set hive.exec.max.created.files=100000;
 
-# hive (1) returns ArrayIndexOutOfBoundsException if transactional is true and 'orc.bloom.filter.columns' is not '*'
-echo "[$(date +"%Y-%m-%d %H:%M:%S %z")] INFO: ACID needs Orc, buckets, transactional=true, also testing bloom filter, like below:"
-echo "${_cmd} -e \"USE ${_dbname};ALTER TABLE emp_part_bckt SET TBLPROPERTIES ('transactional'='true', 'orc.create.index'='true', 'orc.bloom.filter.columns'='*');TRUNCATE TABLE emp_part_bckt;INSERT INTO TABLE emp_part_bckt PARTITION(department) SELECT empid, name,designation,salary,department FROM emp_stage;\""
-#\""ANALYZE TABLE emp_part_bckt PARTITION(department) COMPUTE STATISTICS;ANALYZE TABLE emp_part_bckt COMPUTE STATISTICS for COLUMNS;\""
+if [ -s /var/log/hadoop/hdfs/hdfs-audit.log ]; then
+    _log "INFO" "Adding SQL for importing /var/log/hadoop/hdfs/hdfs-audit.log."
+    _file_size=`gstat -c"%s" /var/log/hadoop/hdfs/hdfs-audit.log`
+    if [ $(( 1024 * 1024 * 1024 )) -lt $_file_size ]; then
+        _log "WARN" "/var/log/hadoop/hdfs/hdfs-audit.log file size () is larger than 1GB so that not importing"
+    else
+        sed -r 's/([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]|FSNamesystem.audit:) /\1\t/g' /var/log/hadoop/hdfs/hdfs-audit.log > ${g_WORK_DIR%/}/hdfs-audit.csv
+        hdfs dfs -put -f ${g_WORK_DIR%/}/hdfs-audit.csv /tmp/hive_workspace/
+
+    _sql="${_sql}
+CREATE TABLE IF NOT EXISTS hdfs_audit (
+  datetime_str STRING,
+  log_class STRING,
+  allowed STRING,
+  ugi STRING,
+  ip STRING,
+  cmd STRING,
+  src STRING,
+  dst STRING,
+  perm STRING,
+  proto STRING,
+  callerContext STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE;
+LOAD DATA INPATH '/tmp/hive_workspace/hdfs-audit.csv' OVERWRITE into table hdfs_audit;
+"
+    fi
+fi
 
 if which hbase &>/dev/null; then
+    _log "INFO" "Creating HBase table 'emp_review' with hbase shell..."
+    # TODO: HBase doesn't seem to have 'create table if not exists' statement
     echo "create 'emp_review','review'" | hbase shell
-    ${_cmd} -e "USE ${_dbname};
+    _log "INFO" "Adding SQLs for creating HBase external table 'emp_review' ..."
+    _sql="${_sql}
 CREATE EXTERNAL TABLE IF NOT EXISTS emp_review(rowkey STRING, empid INT, score FLOAT)
 STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
 WITH SERDEPROPERTIES ('hbase.columns.mapping' = ':key, review:empid, review:score')
@@ -133,4 +178,44 @@ TBLPROPERTIES ('hbase.table.name' = 'emp_review');
 INSERT INTO emp_review select concat_ws('-',cast(empid as string),cast(CURRENT_TIMESTAMP as string)), empid, cast(rand() * 100 as int) from emp_stage;
 "
 fi
+
+# TODO: no good way to find if druid is installed and use *hive2*
+if false; then
+    _log "INFO" "Adding SQLs for creating a Druid table..."
+    # https://jp.hortonworks.com/blog/sub-second-analytics-hive-druid/
+    #set hive.druid.metadata.uri=jdbc:mysql://db.example.com/druid_benchmark;
+    #set hive.druid.indexer.partition.size.max=9000000;
+    #set hive.druid.indexer.memory.rownum.max=100000;
+    #set hive.tez.container.size=16000;
+    #set hive.tez.java.opts=-Xmx10g -XX:MaxDirectMemorySize=1024g -Duser.timezone="America/New_York";
+    #set hive.llap.execution.mode=none;
+
+    # NOTE: not sure if cast(from_unixtime( is needed for __time
+    _sql="${_sql}
+CREATE TABLE IF NOT EXISTS hdfs_audit_month
+STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
+TBLPROPERTIES ('druid.datasource' = 'hdfs_audit_day', 'druid.segment.granularity' = 'MONTH', 'druid.query.granularity' = 'DAY')
+AS
+SELECT
+ cast(from_unixtime(unix_timestamp(datetime_str, 'yyyy-MM-dd HH:mm:ss,SSS')) as timestamp) as `__time`,
+ allowed,
+ ugi,
+ cmd
+FROM
+ hdfs_audit;
+"
+fi
+
+_log "INFO" "Executing SQLs..."
+${_cmd} -e "${_sql}"
+
+# NOTE: hive (1) returns ArrayIndexOutOfBoundsException if transactional is true and 'orc.bloom.filter.columns' is not '*'
+_log "INFO" "Completed!
+NOTE: ACID needs Orc, buckets, transactional=true, also testing bloom filter, like below:
+${_cmd} -e \"USE ${_dbname};ALTER TABLE emp_part_bckt SET TBLPROPERTIES ('transactional'='true', 'orc.create.index'='true', 'orc.bloom.filter.columns'='*');TRUNCATE TABLE emp_part_bckt;INSERT INTO TABLE emp_part_bckt PARTITION(department) SELECT empid, name,designation,salary,department FROM emp_stage;\"
+"
+# May need below too?
+#\""ANALYZE TABLE emp_part_bckt PARTITION(department) COMPUTE STATISTICS;ANALYZE TABLE emp_part_bckt COMPUTE STATISTICS for COLUMNS;\""
+
+_log "INFO" "Listing HDFS /apps/hive/warehouse/${_dbname}.db/"
 hdfs dfs -ls /apps/hive/warehouse/${_dbname}.db/*/
