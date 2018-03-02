882e2dd0c7ab248d11483877fdb9cbab626ac8b0
hajimeo <git@hajigle.com>
2018-03-02 15:30:57 +1000

    echo messages hange and command order change to move similar one closer

--- bash/start_sandbox.sh
+++ bash/start_sandbox.sh
@@ -3,7 +3,7 @@
 # @see https://raw.githubusercontent.com/hortonworks/data-tutorials/master/tutorials/hdp/sandbox-port-forwarding-guide/assets/start-sandbox-hdp.sh
 
 function usage() {
-    echo "Get the latest script
+    echo "To get the latest script:
     curl https://raw.githubusercontent.com/hajimeo/samples/master/bash/start_sandbox.sh -O
 
 To create Sandbox (first time only)
@@ -40,7 +40,7 @@ function f_docker_image_setup() {
 
     which docker &>/dev/null
     if [ $? -ne 0 ]; then
-        echo "Please install docker - https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/"
+        echo "ERROR: Please install docker - https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/"
         echo "or "
         echo "./start_hdp.sh -f f_docker_setup"
         return 1
@@ -48,7 +48,7 @@ function f_docker_image_setup() {
 
     local _existing_img="`docker images --format "{{.Repository}}:{{.Tag}}" | grep -m 1 -E "^${_name}:"`"
     if [ ! -z "$_existing_img" ]; then
-        echo "Image $_name already exist. Exiting."
+        echo "WARN: Image $_name already exist. Exiting."
         echo "To rename image:"
         echo "    docker tag ${_existing_img} <new_name>:<new_tag>"
         echo "    docker rmi ${_existing_img}"
@@ -75,10 +75,10 @@ function f_docker_image_setup() {
     fi
 
     if [ -s "${_tmp_dir%/}/${_file_name}" ]; then
-        echo "${_tmp_dir%/}/${_file_name} exists. Reusing it..."
+        echo "INFO: ${_tmp_dir%/}/${_file_name} exists. Reusing it..."
         sleep 3
     else
-        echo "Executing \"cur \"${_url}\" -o ${_tmp_dir%/}/${_file_name}\""
+        echo "INFO: Executing \"cur \"${_url}\" -o ${_tmp_dir%/}/${_file_name}\""
         curl --retry 100 -C - "${_url}" -o "${_tmp_dir%/}/${_file_name}" || return $?
     fi
 
@@ -100,7 +100,7 @@ function f_ambari_start_all() {
     # NOTE: --retry-connrefused is from curl v 7.52.0
     for i in `seq 1 $_times`; do
         curl -sL -u admin:admin "http://$_host:$_port/api/v1/clusters/${_cluster}?fields=Clusters/health_report" | grep -qE '"Host/host_state/HEALTHY" : [1-9]' && break
-        echo "$_host:$_port is unreachable. Waiting ($i)..."
+        echo "INFO: $_host:$_port is unreachable. Waiting ($i)..."
         sleep $_interval
     done
     sleep 5
@@ -140,7 +140,7 @@ function _isEnoughDisk() {
     _available_space_gb=`_freeSpaceGB "${_dir_path}"`
 
     if [ -z "$_required_gb" ]; then
-        echo "${_available_space_gb}GB free space"
+        echo "INFO: ${_available_space_gb}GB free space"
         _required_gb=`_totalSpaceGB`
         _required_gb="`expr $_required_gb / 10`"
     fi
@@ -243,15 +243,16 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
 
     _HOST_HDP_IP=`ifconfig $_CUSTOM_NETWORK | grep -oP 'inet addr:\d+\.\d+\.\d+\.\d+' | cut -d":" -f2`
 
-    echo "Waiting for docker daemon to start up:"
+    echo "INFO: Waiting for docker daemon to start up:"
     until docker ps 2>&1| grep -q STATUS; do  sleep 1; done;  >/dev/null
 
     # Stop all containers (including $_NAME if it's already running)
     #docker stop $(docker ps -q)
 
     # If same container name exists start it
-    docker ps -a --format "{{.Names}}" | grep -qE "^${_NAME}$"
-    if [ $? -eq 0 ]; then
+    if docker ps --format "{{.Names}}" | grep -qE "^${_NAME}$"; then
+        echo "INFO: ${_NAME} is already running"
+    elif docker ps -a --format "{{.Names}}" | grep -qE "^${_NAME}$"; then
         docker start "${_NAME}" || exit $?
     else
         _network=""
@@ -411,8 +412,6 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
     # paste config.v2.json
     # start docker service, then cotainer
 
-    # startup_script modify /etc/resolv.conf so removing
-    docker exec -it ${_NAME} bash -c 'grep -q -F "> /etc/resolv.conf" /etc/rc.d/init.d/startup_script && tar -cvzf /root/startup_script.tgz `find /etc/rc.d/ -name '*startup_script'` --remove-files'
     #docker exec -t ${_NAME} /etc/init.d/startup_script start
     #docker exec -t ${_NAME} make --makefile /usr/lib/hue/tools/start_scripts/start_deps.mf  -B Startup -j -i
     #docker exec -t ${_NAME} nohup su - hue -c '/bin/bash /usr/lib/tutorials/tutorials_app/run/run.sh' &>/dev/null
@@ -420,73 +419,78 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
     #docker exec -t ${_NAME} chown oozie:hadoop /usr/hdp/current/oozie-server/oozie-server/work/Catalina/localhost/oozie/SESSIONS.ser
     #docker exec -d ${_NAME} /etc/init.d/splash  # to intentioanlly break hue?
 
-    if ${_NEW_CONTAINER} ; then
-        docker exec -it ${_NAME} bash -c "chpasswd <<< root:hadoop"
 
-        # As of this typing, sandbox repo for tutorial is broken so moving out for now
-        docker exec -it ${_NAME} bash -c 'mv /etc/yum.repos.d/sandbox.repo /root/' &>/dev/null
-        docker exec -dt ${_NAME} yum -q -y install yum-utils sudo which vim net-tools strace lsof tcpdump openldap-clients nc sharutils
+    echo "INFO: Clean up old logs to save disk space before starting anything ..."
+    docker exec -it ${_NAME} bash -c 'find /var/log/ -type f -group hadoop \( -name "*\.log*" -o -name "*\.out*" \) -mtime +7 -exec grep -Iq . {} \; -and -print0 | xargs -0 -t -n1 -I {} rm -f {}'
+    docker exec -it ${_NAME} bash -c 'find /var/log/ambari-server/ -type f \( -name "*\.log*" -o -name "*\.out*" \) -mtime +7 -exec grep -Iq . {} \; -and -print0 | xargs -0 -t -n1 -I {} rm -f {}'
+    docker exec -it ${_NAME} bash -c 'find /var/log/ambari-agent/ -type f \( -name "*\.log*" -o -name "*\.out*" \) -mtime +7 -exec grep -Iq . {} \; -and -print0 | xargs -0 -t -n1 -I {} rm -f {}'
+
+    echo "INFO: OS config changes, and starting SSHd, PostgreSQL, and MySQL ..."
+    # setting up password-less ssh to sandbox whenever it starts in case .ssh is updated.
+    if [ -s  ~/.ssh/id_rsa.pub ]; then
+        docker exec -it ${_NAME} bash -c "[ -f /root/.ssh/authorized_keys ] || ( install -D -m 600 /dev/null /root/.ssh/authorized_keys && chmod 700 /root/.ssh )"
+        docker exec -it ${_NAME} bash -c "grep -q \"^`cat ~/.ssh/id_rsa.pub`\" /root/.ssh/authorized_keys || echo \"`cat ~/.ssh/id_rsa.pub`\" >> /root/.ssh/authorized_keys"
     fi
+    docker exec -it ${_NAME} bash -c '[ ! -d /home/admin ] && mkdir -m 700 /home/admin && chown admin:admin /home/admin'
+    # for Knox LDAP demo users
+    docker exec -it ${_NAME} bash -c '[ ! -d /home/tom ] && useradd tom'
+    docker exec -it ${_NAME} bash -c '[ ! -d /home/sam ] && useradd sam'
+    # startup_script modify /etc/resolv.conf so removing
+    docker exec -it ${_NAME} bash -c 'grep -q -F "> /etc/resolv.conf" /etc/rc.d/init.d/startup_script && tar -cvzf /root/startup_script.tgz `find /etc/rc.d/ -name '*startup_script'` --remove-files'
 
-    echo "Starting SSHd, PostgreSQL, Ambari Server and Agent ..."
     docker exec -it ${_NAME} bash -c "service sshd start"
+
+    # PostgreSQL
     docker exec -it ${_NAME} bash -c "sed -i -r \"s/^#?log_line_prefix = ''/log_line_prefix = '%m '/\" /var/lib/pgsql/data/postgresql.conf"
     docker exec -it ${_NAME} bash -c "sed -i -r \"s/^#?log_statement = 'none'/log_statement = 'mod'/\" /var/lib/pgsql/data/postgresql.conf"
     docker exec -it ${_NAME} bash -c "ln -s /var/lib/pgsql/data/pg_log /var/log/postgresql"
     docker exec -it ${_NAME} bash -c "sysctl -w kernel.shmmax=${_SHMMAX};service postgresql start"
     #docker exec -d ${_NAME} /sbin/sysctl -p
-    if ${_NEW_CONTAINER} ; then
-        # (optional) Fixing public hostname (169.254.169.254 issue) by appending public_hostname.sh"
-        docker exec -it ${_NAME} bash -c 'grep -q "^public_hostname_script" /etc/ambari-agent/conf/ambari-agent.ini || ( echo -e "#!/bin/bash\necho \`hostname -f\`" > /var/lib/ambari-agent/public_hostname.sh && chmod a+x /var/lib/ambari-agent/public_hostname.sh && sed -i.bak "/run_as_user/i public_hostname_script=/var/lib/ambari-agent/public_hostname.sh\n" /etc/ambari-agent/conf/ambari-agent.ini )'
-        docker exec -it ${_NAME} bash -c "ambari-agent reset ${_HOSTNAME}"
 
-        echo "Resetting Ambari password (to 'admin') and hostname to ${_HOSTNAME}..."
-        #docker exec -it ${_NAME} /usr/sbin/ambari-admin-password-reset
-        docker exec -it ${_NAME} bash -c "(set -x;[ -S /tmp/.s.PGSQL.5432 ] || (sleep 5;service postgresql restart;sleep 5); PGPASSWORD=bigdata psql -h ${_HOSTNAME} -Uambari -tAc \"UPDATE users SET user_password='538916f8943ec225d97a9a86a2c6ec0818c1cd400e09e03b660fdaaec4af29ddbb6f2b1033b81b00', active=1 WHERE user_name='admin' and user_type='LOCAL';UPDATE hosts set host_name='${_HOSTNAME}', public_host_name='${_HOSTNAME}' where host_id=1;\")"
-        #docker exec -it ${_NAME} bash -c "PGPASSWORD=bigdata psql -h ${_HOSTNAME}  -Uambari -tAc \"UPDATE metainfo SET metainfo_value = '${_AMBARI_VERSION}' where metainfo_key = 'version';\""
-
-        # In case -v /hadoop was used. TODO: the following three lines should be removed later
-        docker exec -it ${_NAME} bash -c 'rm -rf /hadoop/yarn/{local,log}'
-        docker exec -it ${_NAME} bash -c 'cd /hadoop && for _n in `ls -1`; do chown -R $_n:hadoop ./$_n 2>/dev/null; done'
-        docker exec -it ${_NAME} bash -c 'chown -R mapred:hadoop /hadoop/mapreduce'
-    fi
-
-    docker exec -d ${_NAME} service ambari-server start --skip-database-check
-    docker exec -d ${_NAME} service ambari-agent start
-
-    # setting up password-less ssh to sandbox whenever it starts in case .ssh is updated.
-    if [ -s  ~/.ssh/id_rsa.pub ]; then
-        docker exec -it ${_NAME} bash -c "[ -f /root/.ssh/authorized_keys ] || ( install -D -m 600 /dev/null /root/.ssh/authorized_keys && chmod 700 /root/.ssh )"
-        docker exec -it ${_NAME} bash -c "grep -q \"^`cat ~/.ssh/id_rsa.pub`\" /root/.ssh/authorized_keys || echo \"`cat ~/.ssh/id_rsa.pub`\" >> /root/.ssh/authorized_keys"
-
-        docker exec -it ${_NAME} bash -c '[ ! -d /home/admin ] && mkdir -m 700 /home/admin && chown admin:admin /home/admin'
-        # for Knox LDAP demo users
-        docker exec -it ${_NAME} bash -c '[ ! -d /home/tom ] && useradd tom'
-        docker exec -it ${_NAME} bash -c '[ ! -d /home/sam ] && useradd sam'
-    fi
-
-    # for Hive, Oozie, Ranger, KMS etc, making sure mysql starts
+    # MySQL, for Hive, Oozie, Ranger, KMS etc, making sure mysql starts
     docker exec -it ${_NAME} bash -c 'chown -R mysql:mysql /var/lib/mysql /var/run/mysqld'
     docker exec -d ${_NAME} service mysqld start
     # TODO: may need to reset root db user password
     # mysql -uroot -phadoop mysql -e "select user, host from user where User='root' and Password =''"
     # mysql -uroot -phadoop mysql -e "set password for 'root'@'%'= PASSWORD('hadoop')"
+
+    if ${_NEW_CONTAINER} ; then
+        echo "INFO: New container only OS config changes..."
+        docker exec -it ${_NAME} bash -c "chpasswd <<< root:hadoop"
+        # In case -v /hadoop was used. TODO: the following three lines should be removed later
+        docker exec -it ${_NAME} bash -c 'rm -rf /hadoop/yarn/{local,log}'
+        docker exec -it ${_NAME} bash -c 'cd /hadoop && for _n in `ls -1`; do chown -R $_n:hadoop ./$_n 2>/dev/null; done'
+        docker exec -it ${_NAME} bash -c 'chown -R mapred:hadoop /hadoop/mapreduce'
+
+        # As of this typing, sandbox repo for tutorial is broken so moving out for now
+        docker exec -it ${_NAME} bash -c 'mv /etc/yum.repos.d/sandbox.repo /root/' &>/dev/null
+        docker exec -dt ${_NAME} yum -q -y install yum-utils sudo which vim net-tools strace lsof tcpdump openldap-clients nc sharutils
+
+        echo "INFO: New container only Ambari config changes..."
+        # (optional) Fixing public hostname (169.254.169.254 issue) by appending public_hostname.sh"
+        docker exec -it ${_NAME} bash -c 'grep -q "^public_hostname_script" /etc/ambari-agent/conf/ambari-agent.ini || ( echo -e "#!/bin/bash\necho \`hostname -f\`" > /var/lib/ambari-agent/public_hostname.sh && chmod a+x /var/lib/ambari-agent/public_hostname.sh && sed -i.bak "/run_as_user/i public_hostname_script=/var/lib/ambari-agent/public_hostname.sh\n" /etc/ambari-agent/conf/ambari-agent.ini )'
+        docker exec -it ${_NAME} bash -c "ambari-agent reset ${_HOSTNAME}"
+
+        echo "INFO: Resetting Ambari password (to 'admin') and hostname to ${_HOSTNAME}..."
+        #docker exec -it ${_NAME} /usr/sbin/ambari-admin-password-reset
+        docker exec -it ${_NAME} bash -c "(set -x;[ -S /tmp/.s.PGSQL.5432 ] || (sleep 5;service postgresql restart;sleep 5); PGPASSWORD=bigdata psql -h ${_HOSTNAME} -Uambari -tAc \"UPDATE users SET user_password='538916f8943ec225d97a9a86a2c6ec0818c1cd400e09e03b660fdaaec4af29ddbb6f2b1033b81b00', active=1 WHERE user_name='admin' and user_type='LOCAL';UPDATE hosts set host_name='${_HOSTNAME}', public_host_name='${_HOSTNAME}' where host_id=1;\")"
+        #docker exec -it ${_NAME} bash -c "PGPASSWORD=bigdata psql -h ${_HOSTNAME}  -Uambari -tAc \"UPDATE metainfo SET metainfo_value = '${_AMBARI_VERSION}' where metainfo_key = 'version';\""
+    fi
+
+    echo "INFO: Starting Ambari Server & Agent, and Knox Demo LDAP ..."
+    docker exec -d ${_NAME} service ambari-server start --skip-database-check
+    docker exec -d ${_NAME} service ambari-agent start
     docker exec -d ${_NAME} bash -c 'sudo -u knox -i /usr/hdp/current/knox-server/bin/ldap.sh start'
 
     #docker exec -d ${_NAME} /root/start_sandbox.sh
     #docker exec -d ${_NAME} /etc/init.d/shellinaboxd start
     #docker exec -d ${_NAME} /etc/init.d/tutorials start
 
-    echo "Clean up old logs to save disk space..."
-    docker exec -it ${_NAME} bash -c 'find /var/log/ -type f -group hadoop \( -name "*\.log*" -o -name "*\.out*" \) -mtime +7 -exec grep -Iq . {} \; -and -print0 | xargs -0 -t -n1 -I {} rm -f {}'
-    docker exec -it ${_NAME} bash -c 'find /var/log/ambari-server/ -type f \( -name "*\.log*" -o -name "*\.out*" \) -mtime +7 -exec grep -Iq . {} \; -and -print0 | xargs -0 -t -n1 -I {} rm -f {}'
-    docker exec -it ${_NAME} bash -c 'find /var/log/ambari-agent/ -type f \( -name "*\.log*" -o -name "*\.out*" \) -mtime +7 -exec grep -Iq . {} \; -and -print0 | xargs -0 -t -n1 -I {} rm -f {}'
-
     # NOTE: docker exec add '$' and '\r'
     _NETWORK_ADDR=`ssh -q ${_HOSTNAME} hostname -i | sed 's/\(.\+\)\.[0-9]\+$/\1/'`
     if [ -n "$_NETWORK_ADDR" ]; then
-        echo "Removing ${_NETWORK_ADDR%.}.0/24 via 0.0.0.0 which prevents container access ${_NETWORK_ADDR%.}.1 ..."
-        docker exec -it ${_NAME} bash -c "ip route del ${_NETWORK_ADDR%.}.0/24 via 0.0.0.0"
+        echo "INFO: Removing ${_NETWORK_ADDR%.}.0/24 via 0.0.0.0 which prevents container access ${_NETWORK_ADDR%.}.1 ..."
+        docker exec -it ${_NAME} bash -c "ip route del ${_NETWORK_ADDR%.}.0/24 via 0.0.0.0 || ip route del ${_NETWORK_ADDR%.}.0/16 via 0.0.0.0"
 
         if nc -z ${_NETWORK_ADDR%.}.1 28080; then
             docker exec -it ${_NAME} bash -c "grep -q ^proxy /etc/yum.conf || echo \"proxy=http://${_NETWORK_ADDR%.}.1:28080\" >> /etc/yum.conf"
@@ -494,7 +498,7 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
     fi
 
     if [ -e ./start_hdp.sh ]; then
-        echo "To stop/start particular services (otherwise will start all after 5 seconds):"
+        echo "INFO: To stop/start particular services (otherwise will start all after 5 seconds):"
         echo "./start_hdp.sh -f 'f_service \"ZEPPELIN SPARK SPARK2 STORM FALCON OOZIE FLUME ATLAS HBASE KAFKA\" \"STOP\" \"${_HOSTNAME}\"'"
         echo "./start_hdp.sh -f 'f_service \"ZOOKEEPER AMBARI_INFRA RANGER HDFS MAPREDUCE2 YARN HIVE\" \"START\" \"${_HOSTNAME}\"'"
         sleep 5
@@ -503,7 +507,7 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
     nohup bash -c "f_ambari_start_all ${_HOSTNAME} ${_AMBARI_PORT}" &
     echo ""
     sleep 1
-    echo "Completed! (except the following background job)"
+    echo "*** Completed! (except the following background job) ***"
     jobs -l
     #docker exec -it ${_NAME} bash
 fi
