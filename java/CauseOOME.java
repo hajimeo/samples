62d70d5cf717f7414af27f19d334a2270e0ec807
hajimeo <git@hajigle.com>
2018-03-26 17:46:41 +1000

    Generating OOME Java heap space

---
+++ java/CauseOOME.java
@@ -0,0 +1,43 @@
+/**
+ * java -verbose:gc -Xmx16m CauseOOME
+ * java -verbose:gc -XX:+PrintClassHistogramBeforeFullGC -Xmx16m CauseOOME
+ */
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class CauseOOME {
+    private void OOMEing(int initSize, int maxIteration) throws Exception {
+        List<long[]> _list = new LinkedList<long[]>();
+        int size = initSize;
+        for (int i = 1; i < maxIteration; i++) {
+            log("Free Mem: " + Runtime.getRuntime().freeMemory() + " (adding " + size + ")");
+            _list.add(new long[size]);
+            Thread.sleep(1000);
+            size = (int)(size * Math.sqrt(i))+initSize;
+        }
+    }
+
+    private static String getCurrentLocalDateTimeStamp() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+    }
+
+    private static void log(String msg) {
+        System.err.println("[" + getCurrentLocalDateTimeStamp() + "] " + msg);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int size = (args.length > 0) ? Integer.parseInt(args[0]) : 1024;
+        int maxIteration = (args.length > 1) ? Integer.parseInt(args[1]) : 100;
+
+        CauseOOME test = new CauseOOME();
+        log("Starting test with initial size=" + size + " / loop=" + maxIteration + "...");
+        try {
+            test.OOMEing(size, maxIteration);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            log("Completed test.");
+        }
+    }
+}
