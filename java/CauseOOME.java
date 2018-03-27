9a7922ca607cf536be80c6af9901e56d0dc8052a
hajimeo <git@hajigle.com>
2018-03-27 10:14:53 +1000

    While enough memory, adding more mem

--- java/CauseOOME.java
+++ java/CauseOOME.java
@@ -1,8 +1,8 @@
 /**
  * curl -O https://raw.githubusercontent.com/hajimeo/samples/master/java/CauseOOME.java
  * javac CauseOOME.java
- * java -verbose:gc -XX:+PrintGCDetails -Xmx16m CauseOOME
- * java -verbose:gc -XX:+PrintClassHistogramBeforeFullGC -Xmx16m CauseOOME
+ * java -verbose:gc -XX:+PrintGCDetails -Xmx8m CauseOOME
+ * java -XX:+PrintClassHistogramBeforeFullGC -Xmx8m CauseOOME
  */
 
 import java.time.LocalDateTime;
@@ -11,13 +11,20 @@ import java.util.*;
 
 public class CauseOOME {
     private void OOMEing(int initSize, int maxIteration) throws Exception {
-        List<long[]> _list = new LinkedList<long[]>();
+        List<byte[]> _list = new LinkedList<byte[]>();
         int size = initSize;
-        for (int i = 1; i < maxIteration; i++) {
-            log("Free Mem: " + Runtime.getRuntime().freeMemory() + " (adding " + size + ")");
-            _list.add(new long[size]);
-            Thread.sleep(1000);
-            size = (int)(size * Math.sqrt(i))+initSize;
+        long f;
+        for (int i = 0; i < maxIteration; i++) {
+            f = Runtime.getRuntime().freeMemory();
+            if (f > (10*size))
+                size = initSize * (i + 1);
+            else
+                size = (int) (initSize * Math.sqrt((i + 1)));
+
+            log("Free Mem: " + f + " (adding " + size + " bytes / loop " + (i + 1) + ")");
+            byte[] b = new byte[size];
+            _list.add(b);
+            Thread.sleep(500);
         }
     }
 
@@ -30,8 +37,8 @@ public class CauseOOME {
     }
 
     public static void main(String[] args) throws Exception {
-        int size = (args.length > 0) ? Integer.parseInt(args[0]) : 1024;
-        int maxIteration = (args.length > 1) ? Integer.parseInt(args[1]) : 100;
+        int size = (args.length > 0) ? Integer.parseInt(args[0]) : 10240;
+        int maxIteration = (args.length > 1) ? Integer.parseInt(args[1]) : 10000;
 
         CauseOOME test = new CauseOOME();
         log("Starting test with initial size=" + size + " / loop=" + maxIteration + "...");
