a4846df59f67147597d5f75466bc3a6a3192c89c
hajimeo <git@hajigle.com>
2018-03-27 15:05:59 +1000

    completed?

--- java/CauseLeaking.java
+++ java/CauseLeaking.java
@@ -2,58 +2,79 @@
  * curl -O https://raw.githubusercontent.com/hajimeo/samples/master/java/CauseLeaking.java
  * javac CauseLeaking.java
  * java -verbose:gc -XX:+PrintGCDetails -Xmx4m CauseLeaking
- * java -XX:+PrintClassHistogramBeforeFullGC -Xmx4m CauseLeaking 2>&1 | grep Socket
+ * java -XX:+PrintClassHistogramBeforeFullGC -Xmx4m CauseLeaking | grep -F '#instances' -A 20
+ *
+ * Ref: https://qiita.com/mmmm/items/f33b757119fc4dbd6aa1
  */
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.*;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class CauseLeaking {
-    private static int port = 18000;
-    private static HttpServer server;
-    private static Socket[] _list;
+    private static HttpServer httpServer;
+    private static String httpPath = "/test.txt";
+    private static String httpResponse = "Hello World!\n";
 
-    static class MyHandler implements HttpHandler {
+    public static class MyHandler implements HttpHandler {
         @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "Hello World!!";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+        public void handle(HttpExchange he) throws IOException {
+            int loop = 5;
+            he.sendResponseHeaders(200, (httpResponse.length() * loop));
+            OutputStream os = he.getResponseBody();
+            for (int i = 0; i < loop; i++) {
+                os.write(httpResponse.getBytes());
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    log("InterruptedException happened. but ignoring...");
+                }
+            }
             os.close();
         }
     }
 
+    public class ConnectCaller implements Callable<InputStream> {
+        private String spec;
+
+        public ConnectCaller(String spec) {
+            this.spec = spec;
+        }
+
+        public InputStream call() throws Exception {
+            URL url = new URL(this.spec);
+            InputStream is = url.openStream();
+            return is;
+        }
+    }
+
     private static void startWebServer(int port) throws IOException {
-        server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/test.txt", new MyHandler());
-        server.setExecutor(null);
-        server.start();
+        httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+        httpServer.createContext(httpPath, new MyHandler());
+        httpServer.setExecutor(null);
+        httpServer.start();
     }
 
     private static void stopWebServer() throws IOException {
-        server.stop(0);
+        httpServer.stop(0);
     }
 
-    private void OOMEing(int maxIteration) throws Exception {
-        _list = new Socket[maxIteration];
+    private void OOMEing(int maxIteration, String spec) throws Exception {
+        int numThreads = 50;
+        List<Future<InputStream>> _list = new ArrayList<Future<InputStream>>();
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         for (int i = 0; i < maxIteration; i++) {
-            if ((i % 100) == 0) {
+            if (i % numThreads == 0)
                 log("Free Mem: " + Runtime.getRuntime().freeMemory() + " (loop " + (i + 1) + ")");
-                Thread.sleep(1000);
-            }
-
-            _list[i] = new Socket(InetAddress.getLocalHost(), port);
-            _list[i] = null;
+            _list.add(executor.submit(new ConnectCaller(spec)));
+            Thread.sleep(100);
         }
     }
 
@@ -66,19 +87,22 @@ public class CauseLeaking {
     }
 
     public static void main(String[] args) throws Exception {
-        port = (args.length > 0) ? Integer.parseInt(args[0]) : 18000;
+        int port = (args.length > 0) ? Integer.parseInt(args[0]) : 18000;
         int maxIteration = (args.length > 1) ? Integer.parseInt(args[1]) : 10000;
         startWebServer(port);
 
-        CauseLeaking test = new CauseLeaking();
-        log("Starting test with port " + port + " / loop=" + maxIteration + "...");
-        try {
-            test.OOMEing(maxIteration);
-        } catch (OutOfMemoryError e) {
-            e.printStackTrace();
+        if (maxIteration > 0) {
+            String spec = "http://localhost:" + port + httpPath;
+            CauseLeaking cl = new CauseLeaking();
+            log("Starting test with port " + port + " / loop=" + maxIteration + " / url=" + spec + " ...");
+            try {
+                cl.OOMEing(maxIteration, spec);
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+                log("Completed test.");
+                stopWebServer();
+                System.exit(0);
+            }
         }
-
-        log("Completed test.");
-        stopWebServer();
     }
 }
