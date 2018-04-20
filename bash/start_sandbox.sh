c48b27cbfe2a551f6cd6e19cd38739be772f3ff4
hajimeo <git@hajigle.com>
2018-04-20 13:02:33 +1000

    Missing kafka port

--- bash/start_sandbox.sh
+++ bash/start_sandbox.sh
@@ -469,6 +469,8 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
             -p 6080:6080 \
             -p 6188:6188 \
             -p 6182:6182 \
+            -p 6667:6667 \
+            -p 6668:6668 \
             -p 8000:8000 \
             -p 8005:8005 \
             -p 8020:8020 \
@@ -605,6 +607,7 @@ If you would like to fis this now, press Ctrl+c to stop (sleep 7 seconds)"
         docker exec -it ${_NAME} bash -c 'grep -q "^public_hostname_script" /etc/ambari-agent/conf/ambari-agent.ini || ( echo -e "#!/bin/bash\necho \`hostname -f\`" > /var/lib/ambari-agent/public_hostname.sh && chmod a+x /var/lib/ambari-agent/public_hostname.sh && sed -i.bak "/run_as_user/i public_hostname_script=/var/lib/ambari-agent/public_hostname.sh\n" /etc/ambari-agent/conf/ambari-agent.ini )'
         docker exec -it ${_NAME} bash -c "ambari-agent reset ${_HOSTNAME}"
         #docker exec -it ${_NAME} /usr/sbin/ambari-admin-password-reset
+        #curl -s -u admin:admin -H 'X-Requested-By:ambari' "http://${_HOSTNAME}:8080/api/v1/users/admin" -X PUT -d '{"Users/pasword":"admin","Users/old_password":"admin"}'
         docker exec -it ${_NAME} bash -c "(set -x;[ -S /tmp/.s.PGSQL.5432 ] || (sleep 5;service postgresql restart;sleep 5); PGPASSWORD=bigdata psql -h ${_HOSTNAME} -Uambari -tAc \"UPDATE users SET user_password='538916f8943ec225d97a9a86a2c6ec0818c1cd400e09e03b660fdaaec4af29ddbb6f2b1033b81b00', active=1 WHERE user_name='admin' and user_type='LOCAL';UPDATE hosts set host_name='${_HOSTNAME}', public_host_name='${_HOSTNAME}' where host_id=1;\")"
         #docker exec -it ${_NAME} bash -c "PGPASSWORD=bigdata psql -h ${_HOSTNAME}  -Uambari -tAc \"UPDATE metainfo SET metainfo_value = '${_AMBARI_VERSION}' where metainfo_key = 'version';\""
         docker exec -it ${_NAME} bash -c '_javahome="`grep java.home /etc/ambari-server/conf/ambari.properties | cut -d "=" -f2`" && grep -q "^securerandom.source=file:/dev/random" ${_javahome%/}/jre/lib/security/java.security && sed -i.bak -e "s/^securerandom.source=file:\/dev\/random/securerandom.source=file:\/dev\/urandom/" ${_javahome%/}/jre/lib/security/java.security'
