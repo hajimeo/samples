d296db862c1d40c25109c61703a945cf9da75a78
hajime <github@hajigle.com>
2018-06-15 16:49:52 +1000

    modified for my usage (Past year)

--- javascript/bookmarklet.js
+++ javascript/bookmarklet.js
@@ -9,7 +9,21 @@ if (q != null) {
         w.open("https://drive.google.com/drive/search?ltmpl=drive&q=" + q, "t_drive");
         w.open("http://search.osakos.com/index.php?query=" + q + "&rows=50&submit=Search&indexes%5B%5D=hadoop&indexes%5B%5D=hajime&indexes%5B%5D=public&adv=1", "t_search");
         w.open("https://hadoop-and-hdp.blogspot.com.au/search?q=" + q, "t_blog");
-        w.open("https://www.google.com/search?q=" + q, "t_ggl");
+        w.open("https://www.google.com/search?q=" + q +"&tbs=qdr:y", "t_ggl");
+    })(q)
+}
+void 1;
+
+javascript:w = window;
+d = document;
+s = "" + (w.getSelection ? w.getSelection() : d.getSelection ? d.getSelection() : d.selection.createRange().text);
+q = prompt("search word", s);
+if (q != null) {
+    (function (s) {
+        q = encodeURIComponent(s);
+        w.open("http://localhost:38080/slack/search?query=" + q, "t_slack");
+        w.open("https://mail.google.com/mail/u/1/#search/" + q, "t_gmail2");
+        w.open("https://drive.google.com/drive/u/1/search?q=" + q, "t_drive2");
     })(q)
 }
 void 1;
