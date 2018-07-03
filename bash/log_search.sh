40ab0234bbc40d38b3471ca9d55ce08c544693d5
hajime <github@hajigle.com>
2018-07-03 14:23:19 +1000

    ripgrep is so good!!!

--- bash/log_search.sh
+++ bash/log_search.sh
@@ -36,6 +36,40 @@ Or
 }
 ### Public functions ###################################################################################################
 
+function f_rg() {
+    local __doc__="Search current directory with rg"
+    local _regex="$1"
+    local _date_regex="$2"
+    local _tmp_file="./.rg_log_sorted.out"
+
+    if ! which rg &>/dev/null; then
+        echo "'rg' is required (eg: brew install rg)"; return 101
+    fi
+    for j in `rg -l "${_regex}" -g '*.json'`; do
+        echo "$j"
+        python -m json.tool "$j" | rg -A1 -B3 "${_regex}"
+    done
+    rg --search-zip -l "${_regex}" -g '!*.json'
+    echo "//===================================================================="
+    echo "# REGEX = ${_regex}" > "${_tmp_file}"
+    rg --search-zip --no-line-number --no-filename "${_regex}" -g '*.log*' | sed 's/^\([0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]\)T/\1 /' | sort -n | uniq >> "${_tmp_file}"
+    rg --no-line-number -o '\b(FATAL|ERROR|WARN|WARNING|INFO|DEBUG|TRACE) +\[[^\[]+\]|\b[Ff]ailed\b|\b[Ss]low\b|\[Tt]oo .+\b' "${_tmp_file}" | sort | uniq -c | sort -n | tail -n 40
+    echo "====================================================================//"
+    if which bar_chart.py &>/dev/null; then
+        # sudo -H python -mpip install matplotlib
+        # sudo -H pip install data_hacks
+        if [ -z "${_date_regex}" ]; then
+            if [ `wc -l ${_tmp_file} | awk '{print $1}'` -gt 400 ]; then
+                _date_regex="^[0-9-/]+ \d\d:\d"
+            else
+                _date_regex="^[0-9-/]+ \d\d:\d\d:"
+            fi
+        fi
+        rg --no-line-number -o "${_date_regex}" "${_tmp_file}" | bar_chart.py # | sed 's/T/ /'
+    fi
+    ls -lh "${_tmp_file}"
+}
+
 function f_topCausedByExceptions() {
     local __doc__="List Caused By xxxxException"
     local _path="$1"
@@ -197,7 +231,9 @@ function f_hdfsAuditLogCountPerTime() {
     fi
 
     if ! which bar_chart.py &>/dev/null; then
-        echo "## bar_chart.py is missing..."
+        echo "### bar_chart.py is missing..."
+        echo "# sudo -H python -mpip install matplotlib"
+        echo "# sudo -H pip install data_hacks"
         local _cmd="uniq -c"
     else
         local _cmd="bar_chart.py"
