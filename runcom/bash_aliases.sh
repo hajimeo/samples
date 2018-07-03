39250f8132682e64bad2ce1677e11613320f34ab
hajime <github@hajigle.com>
2018-07-03 13:01:00 +1000

    using python only

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -1,17 +1,5 @@
 alias cdl='cd "`ls -dtr ./*/ | tail -n 1`"'
 alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
-alias utc2int2='python -c "import sys,time,dateutil.parser;print int(time.mktime(dateutil.parser.parse(sys.argv[1]).timetuple()))"'
-alias int2utc2='python -c "import sys,time;print time.asctime(time.gmtime(int(sys.argv[1])))+\" UTC\""'
-#alias int2utc='date -u -r'
-# brew install coreutils
-function int2utc() {
-    gdate -u -d "1970/01/01 UTC $1 sec"
-}
-function utc2int() {
-    if which php &>/dev/null; then
-        php -r "echo strtotime(\"$1\").\"\n\";"
-    else
-        gdate -u '+%s' -d"$1"
-    fi
-}
+alias utc2int='python -c "import sys,time,dateutil.parser;print int(time.mktime(dateutil.parser.parse(sys.argv[1]).timetuple()))"'
+alias int2utc='python -c "import sys,time;print time.asctime(time.gmtime(int(sys.argv[1])))+\" UTC\""'
