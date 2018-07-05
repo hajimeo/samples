3d8e7dd5b097ba02ae708c4a5d63b2a947f0f50c
hajime <github@hajigle.com>
2018-07-05 18:26:12 +1000

    using json.dumps after cleaning up the line

--- runcom/pjson.sh
+++ runcom/pjson.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 # ln -s ~/IdeaProjects/samples/runcom/pjson.sh /usr/local/bin/pjson
-python -m json.tool <&0
+#python -m json.tool <&0
+_max_length="${1:-1000}"
+python -c 'import sys,json,encodings.idna
+for l in sys.stdin:
+    l2=l.strip().lstrip("[").rstrip(",]")[:'${_max_length}']
+    try:
+        jo=json.loads(l2)
+        print json.dumps(jo, indent=4)
+    except ValueError:
+        print l2'
