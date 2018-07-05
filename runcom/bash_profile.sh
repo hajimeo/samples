8f90cf542066acee0dc4914dbcff5e7082a0368e
hajime <github@hajigle.com>
2018-07-05 10:15:13 +1000

    added GOROOT (not sure if needs though)

--- runcom/bash_profile.sh
+++ runcom/bash_profile.sh
@@ -1,7 +1,14 @@
 # ln -s ~/IdeaProjects/samples/runcom/bash_profile.sh ~/.bash_profile
 
+# Go/Golang related
+export GOROOT=/usr/local/opt/go/libexec
 export GOPATH=$HOME/go
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+# Kerberos client
 export KRB5_CONFIG=$HOME/krb5.conf
+
+# ripgrep(rg)
 export RIPGREP_CONFIG_PATH=$HOME/.rgrc
 
 if [ -f ~/IdeaProjects/samples/runcom/bash_aliases.sh ]; then
