111b5f6378f1d81652cf21f68e267f4218a2ae23
hajime <github@hajigle.com>
2018-07-18 17:23:01 -0700

    installing resolvconf

--- bash/start_hdp.sh
+++ bash/start_hdp.sh
@@ -2333,7 +2333,7 @@ function p_host_setup() {
 
         # NOTE: psql (postgresql-client) is required
         _log "INFO" "Starting apt-get install packages"
-        apt-get -y install ntpdate curl wget sshfs sysv-rc-conf tcpdump sharutils unzip postgresql-client libxml2-utils expect netcat nscd libmysql-java ppp at &>> /tmp/p_host_setup.log
+        apt-get -y install ntpdate curl wget sshfs sysv-rc-conf tcpdump sharutils unzip postgresql-client libxml2-utils expect netcat nscd libmysql-java ppp at resolvconf &>> /tmp/p_host_setup.log
         #mailutils postfix mysql-client htop
 
         _log "INFO" "Starting f_docker_setup"
