9a702c50546cdfd27f08dc67e8d569ec17dd7d62
hajime <github@hajigle.com>
2018-07-31 15:12:43 +1000

    If result will be 00 to 23 hours, just use date (YYYY-MM-DD)

--- golang/dateregex.go
+++ golang/dateregex.go
@@ -25,41 +25,54 @@ func main() {
     }
 
     start_str := os.Args[1]
-    st, err := time.Parse(layout_in, start_str)
+    start_time, err := time.Parse(layout_in, start_str)
     if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
-    stu := st.Unix()
+    start_unixtime := start_time.Unix()
 
-    et := time.Now()
+    end_time := time.Now()
     if len(os.Args) > 2 && len(os.Args[2]) > 0 {
         end_str := os.Args[2]
-        et, err = time.Parse(layout_in, end_str)
+        end_time, err = time.Parse(layout_in, end_str)
         if err != nil {
             fmt.Println(err)
             os.Exit(1)
         }
     }
-    //fmt.Println(et.Format(layout_in))
-    etu := et.Unix()
+    //fmt.Println(end_time.Format(layout_in))
+    end_unixtime := end_time.Unix()
 
     if len(os.Args) > 4 && len(os.Args[4]) > 0 {
         layout_out = os.Args[4]
     } else {
-        if (etu - stu) >= 3600 {
+        if (end_unixtime - start_unixtime) >= 3600 {
             layout_out = "2006-01-02 15:"
             interval, err = strconv.ParseInt("3600", 10, 64) // 1 hour
         }
         // TODO: need more sophisticated logic
     }
-    //fmt.Println(layout_out)
 
-    for ctu := stu; ctu <= etu; ctu += interval {
-        ct := time.Unix(ctu, 0).In(loc)
-        cts := ct.Format(layout_out)
-        fmt.Print(cts[:(len(cts)-1)])
-        if (ctu + interval) <= etu {
+    for current_unixtime := start_unixtime; current_unixtime <= end_unixtime; current_unixtime += interval {
+        current_layout_out := layout_out
+        current_time := time.Unix(current_unixtime, 0).In(loc)
+        hr, _, _ := current_time.Clock()
+        if hr == 0 && (end_unixtime-current_unixtime) > (60*60*24) {
+            current_layout_out = "2006-01-02"
+            current_unixtime += (60*60*24)
+        }
+
+        //fmt.Println(current_layout_out)
+        current_time_str := current_time.Format(current_layout_out)
+
+        last_c := current_time_str[(len(current_time_str)-1):]
+        if last_c == ":" {
+            fmt.Print(current_time_str[:(len(current_time_str)-1)])
+        } else {
+            fmt.Print(current_time_str)
+        }
+        if (current_unixtime + interval) <= end_unixtime {
             fmt.Print("|")
         }
     }
