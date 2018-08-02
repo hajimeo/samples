1c2d0e0f2fa8da1c3a4087f97c4338c1f3e5b0bb
hajime <github@hajigle.com>
2018-08-02 15:15:41 +1000

    updating...

--- python/line_parser.py
+++ python/line_parser.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# line_parser.py < some_file.txt
+# line_parser.py func_name [args] < some_file.txt
 #
 
-import sys,re
+import sys, re
 
-for line in sys.stdin:
-    # DO something against 'line'
-    print line
+_PREV_COUNTER = 0
+_PREV_LABEL = ""
+
+# All functions need to use "lp_" prefix
+def lp_thread_num(line):
+    global _PREV_COUNTER
+    global _PREV_LABEL
+
+    if len(line) > 0:
+        (label, start_line_num) = line.strip().split(" ", 2)
+    else:
+        label = ""
+        start_line_num = sys.argv[2]
+
+    if _PREV_COUNTER > 0:
+        print "%s %s" % (_PREV_LABEL, (int(start_line_num) - _PREV_COUNTER))
+    _PREV_LABEL = label
+    _PREV_COUNTER = int(start_line_num)
+
+
+if __name__ == '__main__':
+    func_name = sys.argv[1]
+
+    for line in sys.stdin:
+        globals()['lp_'+func_name](line)
+
+    # Some function needs to be called with empty line
+    globals()['lp_'+func_name]("")
+
