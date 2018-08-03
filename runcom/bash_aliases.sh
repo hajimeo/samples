a41390ec2f4699bf3a9e49be3abd3ef3c9329115
hajime <github@hajigle.com>
2018-08-03 10:57:34 +1000

    asftpl (Atscale SFTP ls) improvement and addes sWeb, jargrep()

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -4,15 +4,7 @@ alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.ar
 alias utc2int='python -c "import sys,time,dateutil.parser;print int(time.mktime(dateutil.parser.parse(sys.argv[1]).timetuple()))"'
 alias int2utc='python -c "import sys,time;print time.asctime(time.gmtime(int(sys.argv[1])))+\" UTC\""'
 
-alias asftpl='ssh asftp "ls -ltr /home/ubuntu/upload/ | tail"'
-function asftpd() {
-    [ -z "$1" ] && return 1
-    local _ext="${1##*.}"
-    local _rsync_opts="-Phz"
-    [[ "${_ext}" =~ gz|zip ]] && _rsync_opts="-Ph"
-    #sftp asftp:/home/ubuntu/upload/$1
-    rsync ${_rsync_opts} asftp:/home/ubuntu/upload/$1 ./
-}
+alias sWeb='python ~/IdeaProjects/samples/python/SympleWebServer.py &'
 
 #alias pandas='python -i <(echo "import sys,json;import pandas as pd;f=open(sys.argv[1]);jd=json.load(f);pdf=pd.DataFrame(jd);")'
 alias pandas='python -i <(echo "import sys,json;import pandas as pd;pdf=pd.read_json(sys.argv[1]);")'
@@ -34,20 +26,37 @@ jupyter notebook'
 
 
 
-# Route to Docker Host. As alias doesn't allow to use sudo, using a function
-function r2dh() {
-    local _3rd_dec="${1:-100}"
-    local _dh_ip="${2:-192.168.0.31}"
-    # Mac OS X
-    sudo route delete -net 172.17.${_3rd_dec}.0/24 &>/dev/null;sudo route add -net 172.17.${_3rd_dec}.0/24 ${_dh_ip}
-    # Linux (Ubuntu)
-    #sudo ip route del 172.17.${_3rd_dec}.0/24 &>/dev/null;sudo route add -net 172.17.${_3rd_dec}.0/24 gw ${_dh_ip} ens3
-    # Windows (Cygwin)
-    #route delete 172.17.${_3rd_dec}.0 &>/dev/null;route add 172.17.${_3rd_dec}.0 mask 255.255.255.0 ${_dh_ip};
+### Functions (some command syntax does not work with alias eg: sudo) ###############################
+function asftpl() {
+    ssh -q asftp -t 'cd /home/ubuntu/upload && ls -lt '$1' | head'
+}
+function asftpd() {
+    [ -z "$1" ] && return 1
+    local _ext="${1##*.}"
+    local _rsync_opts="-Phz"
+    [[ "${_ext}" =~ gz|zip ]] && _rsync_opts="-Ph"
+    #sftp asftp:/home/ubuntu/upload/$1
+    rsync ${_rsync_opts} asftp:/home/ubuntu/upload/$1 ./
+}
+
+function jargrep() {
+    local _cmd="jar -tf"
+    which jar &>/dev/null || _cmd="less"
+    find -L ${2:-./} -type f -name '*.jar' -print0 | xargs -0 -n1 -I {} bash -c ''${_cmd}' {} | grep -wi '$1' && echo {}'
 }
 
 function bar() {
-    local _path="${1-./*}"  # TODO: need to sort?
-    local _datetime_regex="${2:-^\d\d\d\d-\d\d-\d\d.\d\d:\d\d}"
-    ggrep -oP "$_datetime_regex" $_path | bar_chart.py
+    ggrep -oP "${2:-^\d\d\d\d-\d\d-\d\d.\d\d:\d\d}" ${1-./*} | bar_chart.py # TODO: need to sort?
+}
+
+function r2dh() {
+    local _3rd="${1:-100}"  # 3rd decimal in network address
+    local _dh="${2:-192.168.0.31}"  # docker host IP
+    if [ "Darwin" = "`uname`" ]; then
+        sudo route delete -net 172.17.${_3rd}.0/24 &>/dev/null;sudo route add -net 172.17.${_3rd}.0/24 ${_dh}
+    elif [ "Linux" = "`uname`" ]; then
+        sudo ip route del 172.17.${_3rd}.0/24 &>/dev/null;sudo route add -net 172.17.${_3rd}.0/24 gw ${_dh} ens3
+    else    # Assuming windows (cygwin)
+        route delete 172.17.${_3rd}.0 &>/dev/null;route add 172.17.${_3rd}.0 mask 255.255.255.0 ${_dh};
+    fi
 }
