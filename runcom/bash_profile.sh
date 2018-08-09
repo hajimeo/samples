aa349db94c8d011108d938ed04435193f9aedd85
hajime <github@hajigle.com>
2018-08-09 17:26:47 +1000

    Using java 8 for my work

--- runcom/bash_profile.sh
+++ runcom/bash_profile.sh
@@ -1,4 +1,7 @@
 # ln -s ~/IdeaProjects/samples/runcom/bash_profile.sh ~/.bash_profile
+if [ -f ~/IdeaProjects/samples/runcom/bash_aliases.sh ]; then
+    source ~/IdeaProjects/samples/runcom/bash_aliases.sh
+fi
 
 # Go/Golang related
 export GOROOT=/usr/local/opt/go/libexec
@@ -11,8 +14,9 @@ export KRB5_CONFIG=$HOME/krb5.conf
 # ripgrep(rg)
 export RIPGREP_CONFIG_PATH=$HOME/.rgrc
 
-if [ -f ~/IdeaProjects/samples/runcom/bash_aliases.sh ]; then
-    source ~/IdeaProjects/samples/runcom/bash_aliases.sh
-fi
-
+# python
 export PYTHONPATH=~/IdeaProjects/samples/python:$PYTHONPATH
+
+# java
+#export JAVA_HOME=`/usr/libexec/java_home -v 10`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
