969451fcc93294de1ba7e668800482c8f14156fe
hajime <github@hajigle.com>
2018-08-10 11:02:54 +1000

    I think customer portal is easy to read

--- javascript/replace_url/background.js
+++ javascript/replace_url/background.js
@@ -1,7 +1,8 @@
 // "persistent": true, is required for onBeforeRequest
-// Also https://developer.chrome.com/apps/match_patterns for url match pattern
+// Also https://developer.chrome.com/apps/match_patterns for url match pattern. Also '#' + '*' doesn't work as it is named alias
 chrome.webRequest.onBeforeRequest.addListener(replaceUrl, {
-    urls: ['https://*.visual.force.com/apex/Case_Lightning*', 'https://customers.atscale.com/50*'],
+    //urls: ['https://*/*'],  // for debug
+    urls: ['https://*.salesforce.com/console*', 'https://*.visual.force.com/*'], //'https://customers.atscale.com/50*'
     types: ["main_frame"]
 });
 
@@ -9,7 +10,8 @@ function replaceUrl(details) {
     // Assuming ID starts with 50
     //var url_regex = new RegExp('https://(na63\.salesforce\.com|customers\..+.com)/50*');
     //if (!url_regex.test(details.url)) return;
-    var caseId_regex = new RegExp("^http.+\.com/(50[^\?&]+)");
+    //var caseId_regex = new RegExp("^http.+\.com/(50[^\?&]+)");
+    var caseId_regex = new RegExp("^http.+\.salesforce.com/console#%2F(50[^\?&]+)");
     var caseId_regex_Lightning = new RegExp("^http.+\.com/.+/Case_Lightning.+&id=(50[^\?&]+)");
     console.log("Request: TabID = " + details.tabId + " | url = " + details.url);
 
@@ -20,12 +22,15 @@ function replaceUrl(details) {
         }
         if (!tab) return;
         var match = caseId_regex.exec(details.url);
+        console.log("match 1 = " + match);
         if (! match) {
             match = caseId_regex_Lightning.exec(details.url)
+            console.log("match 2 = " + match);
         }
 
         if (match && match.length > 1) {
-            var new_url = "https://na63.salesforce.com/console#%2F" + match[1];
+            //var new_url = "https://na63.salesforce.com/console#%2F" + match[1];
+            var new_url = "https://customers.atscale.com/" + match[1];
             console.log("New URL = " + new_url);
             chrome.tabs.update(tab.id, {url: new_url});
         }
