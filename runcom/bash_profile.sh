57693b060d38861f5abf523a5302bda9e45603cf
hajime <github@hajigle.com>
2018-08-17 12:59:44 +1000

    source iterm2 goodies

--- runcom/bash_profile.sh
+++ runcom/bash_profile.sh
@@ -20,3 +20,9 @@ export PYTHONPATH=~/IdeaProjects/samples/python:$PYTHONPATH
 # java
 #export JAVA_HOME=`/usr/libexec/java_home -v 10`
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+
+# iterm2
+#curl -L https://iterm2.com/shell_integration/bash -o ~/.iterm2_shell_integration.bash
+if [ -f ~/.iterm2_shell_integration.bash ]; then
+    source ~/.iterm2_shell_integration.bash
+fi
