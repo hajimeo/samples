bde47216635309ed251067e34745dd26f7e0b254
hajime <github@hajigle.com>
2018-08-20 08:32:28 +1000

    Using one regex for all

--- javascript/replace_url/background.js
+++ javascript/replace_url/background.js
@@ -8,13 +8,10 @@ chrome.webRequest.onBeforeRequest.addListener(replaceUrl, {
 
 function replaceUrl(details) {
     // Assuming ID starts with 50
-    //var url_regex = new RegExp('https://(na63\.salesforce\.com|customers\..+.com)/50*');
-    //if (!url_regex.test(details.url)) return;
-    //var caseId_regex = new RegExp("^http.+\.com/(50[^\?&]+)");
-    var caseId_regex = new RegExp("^http.+\.salesforce.com/console#%2F(50[^\?&]+)");
-    var caseId_regex_Lightning = new RegExp("^http.+\.com/.+/Case_Lightning.+&id=(50[^\?&]+)");
-    console.log("Request: TabID = " + details.tabId + " | url = " + details.url);
+    // protocol + (hostname/path_to_id=) + (caseId), so that the index of groups is 2
+    var caseId_regex = new RegExp("^https://(.+\.salesforce.com/console#%2F|.+\.visual\.force\.com/apex/Case_Lightning.*[?&]id=)(50[^\?&]+)");
 
+    console.log("Request: TabID = " + details.tabId + " | url = " + details.url);
     chrome.tabs.get(details.tabId, function (tab) {
         if (chrome.runtime.lastError) {
             console.log(chrome.runtime.lastError.message);
@@ -23,14 +20,10 @@ function replaceUrl(details) {
         if (!tab) return;
         var match = caseId_regex.exec(details.url);
         console.log("match 1 = " + match);
-        if (! match) {
-            match = caseId_regex_Lightning.exec(details.url)
-            console.log("match 2 = " + match);
-        }
 
         if (match && match.length > 1) {
-            var new_url = "https://na63.salesforce.com/console#%2F" + match[1];
-            //var new_url = "https://customers.atscale.com/" + match[1];
+            //var new_url = "https://na63.salesforce.com/console#%2F" + match[1];
+            var new_url = "https://customers.atscale.com/" + match[2];
             console.log("New URL = " + new_url);
             chrome.tabs.update(tab.id, {url: new_url});
         }
