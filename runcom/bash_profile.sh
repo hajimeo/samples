a2252a7b3af377fccd582454ab2dd4954d03ee9e
hajime <github@hajigle.com>
2018-08-21 19:41:40 +1000

    If no iTerm2, no source

--- runcom/bash_profile.sh
+++ runcom/bash_profile.sh
@@ -23,6 +23,6 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
 # iterm2
 #curl -L https://iterm2.com/shell_integration/bash -o ~/.iterm2_shell_integration.bash
-if [ -f ~/.iterm2_shell_integration.bash ]; then
+if [ -n "$ITERM_SESSION_ID" ] && [ -f ~/.iterm2_shell_integration.bash ]; then
     source ~/.iterm2_shell_integration.bash
 fi
