ef2086bf316876a67f3119c3a4865a97aca400d7
hajime <github@hajigle.com>
2018-09-19 13:57:18 +1000

    refactored jupyter command

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -16,33 +16,63 @@ alias ht='for _f in `ls -1tr`; do echo "$_f"; head -n1 $_f | sed "s/^/  /"; tail
 # Load/source my log searching utility functions
 #mkdir -p ~/IdeaProjects/samples/bash; curl -o ~/IdeaProjects/samples/bash/log_search.sh https://raw.githubusercontent.com/hajimeo/samples/master/bash/log_search.sh
 alias logS="source ~/IdeaProjects/samples/bash/log_search.sh"
-# Start metabase on port: 30000
-alias mb='java -jar ~/Applications/metabase.jar'
+
+# Java / jar related
+alias mb='java -jar ~/Applications/metabase.jar'    # port is 3000
 alias vnc='nohup java -jar ~/Applications/tightvnc-jviewer.jar &>/tmp/tightvnc-jviewer.out &'
-# Start Jupyter Notebook with Aggregation template (and backup-ing)
-alias jn='if [ -d ~/backup/jupyter-notebook ]; then
-    cp ~/backup/jupyter-notebook/Aggregation.ipynb ./ && jupyter notebook &
-    while true; do
-        sleep 300
-        if [ "`ls -1 ./*.ipynb 2>/dev/null | wc -l`" -gt 0 ]; then
-            rsync -a --exclude="Untitled.ipynb" ./*.ipynb ~/backup/jupyter-notebook/ || break
-        fi
-        if ! nc -z localhost 8888 &>/dev/null; then
-            mv -f ./Aggregation.ipynb /tmp/
-            break
-        fi
-    done &
-fi'
-# Start python simple http server from the specific dir
+
+# Python simple http server from the specific dir
 # To setup: asDocSync <server ip>
 alias webs='cd ~/Public/atscale_latest/ && nohup python -m SimpleHTTPServer 38081 &>/tmp/python_simplehttpserver.out & nohup python ~/IdeaProjects/samples/python/SympleWebServer.py &>/tmp/python_simplewebserver.out &'
 # List and grep some specific files from s3. NOTE: https:// requires s3-us-west-1.amazonaws.com
-alias asS3='s3cmd ls s3://files.atscale.com/installer/package/ | grep -E "atscale-[6789].+latest-el6\.x86_64\.tar\.gz$"'
-# TODO: public-repo-1.hortonworks.com private-repo-1.hortonworks.com
+
+# Work specific aliases
+alias asS3='s3cmd ls s3://files.atscale.com/installer/package/ | grep -E "atscale-[6789].+latest-el6\.x86_64\.tar\.gz$"'    # TODO: public-repo-1.hortonworks.com private-repo-1.hortonworks.com
 alias asPupInst='scp -C ~/IdeaProjects/samples/atscale/install_atscale.sh root@192.168.0.31:/var/tmp/share/atscale/;scp -C ~/IdeaProjects/samples/atscale/install_atscale.sh root@192.168.6.162:/var/tmp/share/atscale/'
 
 
 ### Functions (some command syntax does not work with alias eg: sudo) ##################################################
+# Start Jupyter Notebook with Aggregation template (and backup-ing)
+function jp() {
+    local _id="${1}"
+    local _backup_dir="${2-$HOME/backup/jupyter-notebook}"
+    local _template="${3-Aggregation.ipynb}"
+    local _sleep="${4:-180}"
+    local _port="${5:-8889}"
+
+    if [ -n "${_backup_dir}" ] && [ ! -d "${_backup_dir}" ]; then
+        mkdir -p "${_backup_dir}" || return 11
+    fi
+
+    [ -n "${_id}" ] && _id="_${_id}"
+
+    if [ -d "${_backup_dir}" ]; then
+        # http://ipython.org/ipython-doc/1/config/overview.html#startup-files
+        if [ ! -f ~/.ipython/profile_default/startup/${_template%.*}.py ] && [ -s ${_backup_dir%/}/${_template%.*}.py ]; then
+            cp ${_backup_dir%/}/${_template%.*}.py ~/.ipython/profile_default/startup/${_template%.*}.py
+        fi
+
+        [ -s "${_backup_dir%/}/${_template}" ] && cp -f "${_backup_dir%/}/${_template}" ./${_template%.*}${_id}.ipynb
+        while true; do
+            sleep ${_sleep}
+            if [  "`ls -1 ./*.ipynb 2>/dev/null | wc -l`" -gt 0 ]; then
+                rsync -a --exclude="Untitled.ipynb" ./*.ipynb "${_backup_dir%/}/" || break
+                # TODO: if no ipynb file to backup, should break?
+            fi
+            if ! lsof -ti:${_port} &>/dev/null; then
+                if [ -d ~/.Trash ]; then
+                    mv -f ${_template%.*}${_id}.ipynb ~/.Trash/
+                else
+                    mv -f ${_template%.*}${_id}.ipynb /tmp/
+                fi
+                break
+            fi
+        done &
+    fi
+
+    jupyter lab --ip='localhost' --port=${_port} &
+}
+
 # Mac only: Start Google Chrome in incognito with proxy
 # NOTE: Below didn't work
 #open -na "Google Chrome" --args "--user-data-dir=${_tmp_dir} --proxy-server=${_proxy}"
@@ -50,8 +80,16 @@ alias asPupInst='scp -C ~/IdeaProjects/samples/atscale/install_atscale.sh root@1
 function chromep() {
     local _host="${1:-192.168.6.162}"
     local _port=${2:-28081}
+    local _url=${3-$_URL}
     [ ! -d $HOME/.chromep/${_host}_${_port} ] && mkdir -p $HOME/.chromep/${_host}_${_port}
-    nohup "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir=$HOME/.chromep/${_host}_${_port} --proxy-server="socks5://${_host}:${_port}" &>/tmp/chrome.out &
+    if [ -n "${_url}" ]; then
+        if [[ ! "${_url}" =~ ^http ]]; then
+            _url="--app http://${_url}"
+        else
+            _url="--app ${_url}"
+        fi
+    fi
+    nohup "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --user-data-dir=$HOME/.chromep/${_host}_${_port} --proxy-server="socks5://${_host}:${_port}" ${_url} &>/tmp/chrome.out &
 }
 
 # List files against hostname 'asftp'. NOTE: the hostname 'asftp' is specified in .ssh_config
