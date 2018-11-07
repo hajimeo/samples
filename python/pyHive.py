53dd78b6bb3fbba0a5d6153ac317ffe5310a6165
hajime <github@hajigle.com>
2018-11-07 19:09:15 +1000

    still broken though

--- python/pyHive.py
+++ python/pyHive.py
@@ -7,8 +7,15 @@ print(curs.fetchall())
 
 import jaydebeapi
 curs = jaydebeapi.connect("org.apache.hive.jdbc.HiveDriver",
-                          "jdbc:hive2://node1.support.localdomain:10000/test", ["admin", "admin"],
-                          ["./hive-jdbc-1.0.0-standalone.jar",
-                           "./hadoop-core-1.0.3.jar"]).cursor()
+                          "jdbc:hive2://node1.support.localdomain:11111/test", ["admin", "admin"],
+                          ["/var/tmp/share/atscale/hive-jdbc-1.0.0-standalone.jar",
+                           "/var/tmp/share/atscale/hadoop-core-1.0.3.jar"]).cursor()
+'''
+# https://github.com/andreasnuesslein/JayDeBeApi3
+curs = jaydebeapi.connect("org.apache.hive.jdbc.HiveDriver",
+                          ["jdbc:hive2://node1.support.localdomain:11111/Sales+Insights", "admin", "admin"],
+                          ["/var/tmp/share/atscale/hive-jdbc-1.0.0-standalone.jar",
+                           "/var/tmp/share/atscale/hadoop-core-1.0.3.jar"]).cursor()
+'''
 curs.execute("show tables")
 print(curs.fetchall())
