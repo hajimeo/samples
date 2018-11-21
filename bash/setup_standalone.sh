7187472635719982746982d3d1f3685a0eaf757e
hajime <github@hajigle.com>
2018-11-21 14:11:26 +1000

    often 10 seconds is not long enough

--- bash/setup_standalone.sh
+++ bash/setup_standalone.sh
@@ -498,11 +498,11 @@ function f_as_start() {
         # NOTE: At this moment, assuming only one postgresql version. Not using 'at' command as minimum time unit is minutes
         _log "INFO" "UPDATE engines SET host='${_hostname}' where default_engine is true AND host='${_old_hostname}'"
         docker exec -it ${_name} bash -c 'cd /usr/local/'${_service}'/share/postgresql-*
-for _i in {1..5}; do
-  sleep 2
+for _i in {1..9}; do
+  sleep 4
   LD_LIBRARY_PATH=./lib ./bin/pg_isready -p 10520 -q && break
 done
-sleep 2
+sleep 1
 sudo -u '${_service}' ./bin/postgres_psql -cl "-c \"UPDATE engines SET host='${_hostname}' where host='${_old_hostname}'\""
 '
     fi
