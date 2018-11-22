1afe84fb218aed45e4e4640c078cdea703f6a319
hajime <github@hajigle.com>
2018-11-23 08:26:14 +1000

    typo....

--- bash/setup_standalone.sh
+++ bash/setup_standalone.sh
@@ -743,7 +743,7 @@ function f_shellinabox() {
     fi
 
     if ! grep -qE "^SHELLINABOX_ARGS.+${_user}:.+/shellinabox_login\"" /etc/default/shellinabox; then
-        [ ! -s /etc/default/shellinabox.org ] && cp -p /etc/default/shellinabox /etc/default/shellinabox.orig
+        [ ! -s /etc/default/shellinabox.orig ] && cp -p /etc/default/shellinabox /etc/default/shellinabox.orig
         sed -i 's@^SHELLINABOX_ARGS=.\+@SHELLINABOX_ARGS="--no-beep -s /'${_user}':'${_user}':'${_user}':HOME:/usr/local/bin/shellinabox_login"@' /etc/default/shellinabox
         service shellinabox restart || return $?
     fi
