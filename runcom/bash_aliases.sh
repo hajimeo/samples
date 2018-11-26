9f008723fa842805508599c63c1523a6c76bd852
Hajime Osako <hajimeo@users.noreply.github.com>
2018-11-26 20:25:30 +1000

    added brew command for mac

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -9,7 +9,7 @@ alias int2utc='python -c "import sys,time;print time.asctime(time.gmtime(int(sys
 #alias pandas='python -i <(echo "import sys,json;import pandas as pd;f=open(sys.argv[1]);jd=json.load(f);pdf=pd.DataFrame(jd);")'
 alias pandas='python -i <(echo "import sys,json;import pandas as pd;pdf=pd.read_json(sys.argv[1]);")'
 alias rmcomma='sed "s/,$//g; s/^\[//g; s/\]$//g"'
-# https://dev.to/rhymes/how-to-make-python-code-concurrent-with-3-lines-of-code-2fpe
+# brew install gnu-time --with-default-names
 alias timef='/usr/bin/time -f"[%Us user %Ss sys %es real %MkB mem]"'
 
 
