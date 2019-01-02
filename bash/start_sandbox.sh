e237a858d024ec69e1074c603a4f25519d60e235
hajime <github@hajigle.com>
2019-01-02 11:33:51 +1000

    refactoring f_ssh_config

--- bash/start_sandbox.sh
+++ bash/start_sandbox.sh
@@ -226,23 +226,29 @@ function f_useradd() {
 
 function f_ssh_config() {
     local __doc__="Copy keys and setup authorized key to a node (container)"
-    local _name="${1-$_NAME}"
-    local _key="$2"
-    local _pub_key="$3"
-    # ssh -q -oBatchMode=yes ${_name} echo && return 0
+    local _name="${1:-${_NAME}}"
+    local _key="${2:-"$HOME/.ssh/id_rsa"}"
+    local _pub_key="${3:-"$HOME/.ssh/id_rsa.pub"}"
 
-    if [ -z "${_key}" ] && [ -r ~/.ssh/id_rsa ]; then
-        _key=~/.ssh/id_rsa
+    # ssh -q -oBatchMode=yes ${_name} echo && return 0
+    if [ -z "${_name}" ]; then
+        echo "ERROR: Need a container name to setup password less ssh"
+        return 1
     fi
 
-    if [ -z "${_pub_key}" ] && [ -r ~/.ssh/id_rsa.pub ]; then
-        _pub_key=~/.ssh/id_rsa.pub
+    if [ ! -r "${_key}" ]; then
+        echo "ERROR: key: ${_key} is not readable"
+        return 1
+    fi
+    if [ ! -r "${_pub_key}" ]; then
+        echo "ERROR: publikc key: ${_pub_key} is not readable. Use 'ssh-keygen -y -f ${_key} > ~/.ssh/id_rsa.pub'"
+        return 1
     fi
 
     docker exec -it ${_name} bash -c "[ -f /root/.ssh/authorized_keys ] || ( install -D -m 600 /dev/null /root/.ssh/authorized_keys && chmod 700 /root/.ssh )"
     docker exec -it ${_name} bash -c "[ -f /root/.ssh/id_rsa.orig ] && exit; [ -f /root/.ssh/id_rsa ] && mv /root/.ssh/id_rsa /root/.ssh/id_rsa.orig; echo \"`cat ${_key}`\" > /root/.ssh/id_rsa; chmod 600 /root/.ssh/id_rsa;echo \"`cat ${_pub_key}`\" > /root/.ssh/id_rsa.pub; chmod 644 /root/.ssh/id_rsa.pub"
     docker exec -it ${_name} bash -c "grep -q \"^`cat ${_pub_key}`\" /root/.ssh/authorized_keys || echo \"`cat ${_pub_key}`\" >> /root/.ssh/authorized_keys"
-    docker exec -it ${_name} bash -c "[ -f /root/.ssh/config ] || echo -e \"Host *\n  StrictHostKeyChecking no\n  UserKnownHostsFile /dev/null\" > /root/.ssh/config"
+    docker exec -it ${_name} bash -c "[ -f /root/.ssh/config ] || echo -e \"Host *\n  StrictHostKeyChecking no\n  UserKnownHostsFile /dev/null\n  LogLevel ERROR\" > /root/.ssh/config"
 }
 
 function _isEnoughDisk() {
