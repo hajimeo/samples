179907699041043334860d74fca25637eaee7271
hajime <github@hajigle.com>
2019-01-23 12:36:02 +1000

    Added VNC

--- misc/shellinabox_login.sh
+++ misc/shellinabox_login.sh
@@ -29,35 +29,40 @@ if len(_ss_args) > 0:
 '`"
         [ -n "${_CMD}" ] && eval "${_CMD}"
     else
-        echo "SSH login to a running container:"
+        echo "# SSH login to a running container:"
         docker ps --format "{{.Names}}" | grep -E "^(node|atscale|cdh|hdp)" | sort | sed "s/^/  ssh root@/g"
         echo ""
 
         if [ -x /usr/local/bin/setup_standalone.sh ]; then
-            echo "To start a container (setup_standalone.sh -h for help):"
+            echo "# To start a container (setup_standalone.sh -h for help):"
             (docker images --format "{{.Repository}}";docker ps -a --format "{{.Names}}" --filter "status=exited") | grep -E "^atscale" | sort | uniq | sed "s/^/  setup_standalone.sh -n /g"
             echo ""
         fi
 
-        echo "URLs (NOTE: need Proxy or Routing by using one of below commands):"
+        echo "# URLs (NOTE: Chrome via Proxy or Route required):"
         for _n in `docker ps --format "{{.Names}}" | grep -E "^(node|atscale|cdh|hdp)" | sort`; do for _p in 10500 8080 7180; do if nc -z $_n $_p; then echo "  http://$_n:$_p/"; fi done done
         echo ""
     fi
 
     if nc -z localhost %_proxy_port%; then
         _URL=""; [ -n "${_NAME}" ] && _URL="http://${_NAME}:10500"
-        echo "If you are using VPN, paste below into *Mac* terminal to access web UIs:"
-        echo "  open -na \"Google Chrome\" --args --user-data-dir=\$HOME/.chrome_pxy --proxy-server=socks5://`hostname -I | cut -d" " -f1`:%_proxy_port% ${_URL}"
-        echo "For Windows:"
-        echo '  "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=%USERPROFILE%\.chrome_pxy --proxy-server=socks5://'`hostname -I | cut -d" " -f1`':%_proxy_port% '${_URL}
+        echo "# Start Chrome via Proxy to access web UIs:"
+        echo "  Mac: open -na \"Google Chrome\" --args --user-data-dir=\$HOME/.chrome_pxy --proxy-server=socks5://`hostname -I | cut -d" " -f1`:%_proxy_port% ${_URL}"
+        echo '  Win: "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=%USERPROFILE%\.chrome_pxy --proxy-server=socks5://'`hostname -I | cut -d" " -f1`':%_proxy_port% '${_URL}
         echo ""
     fi
 
     if [ -n "%_net_addr%" ]; then
-        echo "If not using VPN but in same network, an example of route command for Mac:"
-        echo "  sudo route add -net %_net_addr%/24 `hostname -I | cut -d" " -f1`"
-        echo "For Windows (after opening a terminal as administrator):"
-        echo "  route add -net %_net_addr% mask 255.255.255.0 `hostname -I | cut -d" " -f1`"
+        echo "# Route command examples (root/admin privilege required):"
+        echo "  Mac: route add -net %_net_addr%/24 `hostname -I | cut -d" " -f1`"
+        echo "  Win: route add -net %_net_addr% mask 255.255.255.0 `hostname -I | cut -d" " -f1`"
+        echo ""
+    fi
+
+    _vnc_port="`ps auxwww | sed -n -r 's/^webuser.+Xtightvnc.+rfbport ([0-9]+).+/\1/p'`"
+    if [ -n "${_vnc_port}" ]; then
+        echo "# VNC:"
+        echo "  vnc://`hostname -I | cut -d" " -f1`:${_vnc_port}"
         echo ""
     fi
 
