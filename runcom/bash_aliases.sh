daaa4a7f9f0e00929cad30b9b708f2f3f0d81a74
hajime <github@hajigle.com>
2019-02-19 09:00:27 +1000

    not sure why but suddenly it stopped working if (short) hostname is used.

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -24,9 +24,9 @@ alias mb='java -jar ~/Applications/metabase.jar'    # port is 3000
 alias vnc='nohup java -jar ~/Applications/tightvnc-jviewer.jar &>/tmp/tightvnc-jviewer.out &'
 
 # Chrome aliases for Mac
-alias shib-dh1='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/dh1 --proxy-server=socks5://dh1:28081 https://localhost:4200/webuser/'
-alias shib-spt='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/spt --proxy-server=socks5://support:28081 https://localhost:4200/webuser/'
-alias shib-haj='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/haj --proxy-server=socks5://hajime:28081 https://localhost:4200/webuser/'
+alias shib-dh1='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/dh1 --proxy-server=socks5://dh1:28081 https://192.168.1.31:4200/webuser/'
+alias shib-spt='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/spt --proxy-server=socks5://support:28081 https://192.168.6.162:4200/webuser/'
+alias shib-haj='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/haj --proxy-server=socks5://hajime:28081 https://192.168.6.163:4200/webuser/'
 alias hblog='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/hajigle https://www.blogger.com/blogger.g?blogID=9018688091574554712&pli=1#allposts'
 
 # Python simple http server from the specific dir
