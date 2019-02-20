90ce94a538f9e8e9b459e379f7402342a144453b
hajime <github@hajigle.com>
2019-02-20 11:59:11 +1000

    bug fix as getSelected returns the URL which is going to be used, not currently active tab's URL.

--- javascript/replace_url/background.js
+++ javascript/replace_url/background.js
@@ -13,44 +13,42 @@ function replaceUrl(details) {
     var caseId_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/r/Case/|.+\.visual\.force\.com/apex/Case_Lightning.*[?&]id=|customers\.atscale\.com/s/case/)(50[^\?&/]+)");
     var tab_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/[ro]/Case/)");
 
-    chrome.tabs.getSelected(null, function (tab) {
-        if (tab_regex.exec(tab.url)) {
-            console.log('Current selected tab is the target tab (so no action required.)');
+    if (tab_regex.exec(window.location.toString())) {
+        console.log('Current selected tab is the target tab (so no action required.): ', window.location.toString());
+        return {redirectUrl: details.url}
+    }
+
+    chrome.tabs.query({currentWindow: true}, function (tabs) {
+        var tab_id = null;
+        tabs.forEach(function (tab) {
+            if (tab_id === null && tab.id != details.tabId) {
+                if (tab_regex.exec(tab.url)) {
+                    tab_id = tab.id;
+                    console.log('The target tab is already opened and id is ', tab_id);
+                    console.log('and URL is ', tab.url);
+                }
+            }
+        });
+
+        if (tab_id) {
+            console.log('Closing the new tab as going to re-use existing tab:', tab_id);
+            chrome.tabs.remove(details.tabId, function () {
+            });
+        } else {
+            console.log('Could not find the target tab, so using this new tab.');
             return {redirectUrl: details.url}
         }
 
-        chrome.tabs.query({currentWindow: true}, function (tabs) {
-            var tab_id = null;
-            tabs.forEach(function (tab) {
-                if (tab_id === null && tab.id != details.tabId) {
-                    if (tab_regex.exec(tab.url)) {
-                        tab_id = tab.id;
-                        console.log('The target tab is already opened and id is ', tab_id);
-                        console.log('and URL is ', tab.url);
-                    }
-                }
-            });
+        var match = caseId_regex.exec(details.url);
+        console.log("matches = " + match);
 
-            if (tab_id) {
-                console.log('Closing the new tab as going to re-use existing tab:', tab_id);
-                chrome.tabs.remove(details.tabId, function () {
-                });
-            } else {
-                console.log('Could not find the target tab, so using this new tab.');
-                return {redirectUrl: details.url}
-            }
-
-            var match = caseId_regex.exec(details.url);
-            console.log("matches = " + match);
-
-            if (match && match.length > 1) {
-                var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
-                //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
-                //var new_url = "https://customers.atscale.com/" + match[2];
-                //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
-                console.log("New URL = " + new_url);
-                chrome.tabs.update(tab_id, {"active": true, url: new_url});
-            }
-        });
+        if (match && match.length > 1) {
+            var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
+            //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
+            //var new_url = "https://customers.atscale.com/" + match[2];
+            //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
+            console.log("New URL = " + new_url);
+            chrome.tabs.update(tab_id, {"active": true, url: new_url});
+        }
     });
 }
