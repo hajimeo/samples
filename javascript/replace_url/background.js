2e234f7a770730d3eb0585fb57dab06a454e40f3
hajime <github@hajigle.com>
2019-02-20 11:53:08 +1000

    Comment changes (to make those more clear). Added getSelected so that when a link is clicked from the target tab, do not refresh the page (Salesforce doesn't need to refresh the page to re-use the URL)

--- javascript/replace_url/background.js
+++ javascript/replace_url/background.js
@@ -8,44 +8,49 @@ chrome.webRequest.onBeforeRequest.addListener(replaceUrl, {
 
 function replaceUrl(details) {
     console.log("=== Start 'replaceUrl' ================================");
-    // Assuming ID starts with 50
-    // protocol + (hostname/path_to_id=) + (caseId), so that the index of groups is 2
-    var caseId_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/r/Case/|.+\.visual\.force\.com/apex/Case_Lightning.*[?&]id=|customers\.atscale\.com/s/case/)(50[^\?&/]+)");
-    var tab_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/r/Case/)");
-
     console.log("Request: details.tabId = " + details.tabId + " | url = " + details.url);
+    // Assuming ID starts with 50, and protocol + (hostname/path_to_id=) + (caseId), so that the index of groups is 2
+    var caseId_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/r/Case/|.+\.visual\.force\.com/apex/Case_Lightning.*[?&]id=|customers\.atscale\.com/s/case/)(50[^\?&/]+)");
+    var tab_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/[ro]/Case/)");
 
-    chrome.tabs.query({currentWindow: true}, function (tabs) {
-        var tab_id = null;
-        tabs.forEach(function (tab) {
-            if (tab_id === null && tab.id != details.tabId) {
-                if (tab_regex.exec(tab.url)) {
-                    tab_id = tab.id;
-                    console.log('Already opened tab ID: ', tab_id);
-                    console.log('and tab URL: ', tab.url);
-                }
-            }
-        });
-
-        if (tab_id) {
-            console.log('Closing : ', details.tabId);
-            chrome.tabs.remove(details.tabId, function () {
-            });
-        } else {
-            console.log('Returning : ', details.url);
+    chrome.tabs.getSelected(null, function (tab) {
+        if (tab_regex.exec(tab.url)) {
+            console.log('Current selected tab is the target tab (so no action required.)');
             return {redirectUrl: details.url}
         }
 
-        var match = caseId_regex.exec(details.url);
-        console.log("matches = " + match);
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+            var tab_id = null;
+            tabs.forEach(function (tab) {
+                if (tab_id === null && tab.id != details.tabId) {
+                    if (tab_regex.exec(tab.url)) {
+                        tab_id = tab.id;
+                        console.log('The target tab is already opened and id is ', tab_id);
+                        console.log('and URL is ', tab.url);
+                    }
+                }
+            });
 
-        if (match && match.length > 1) {
-            var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
-            //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
-            //var new_url = "https://customers.atscale.com/" + match[2];
-            //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
-            console.log("New URL = " + new_url);
-            chrome.tabs.update(tab_id, {"active": true, url: new_url});
-        }
+            if (tab_id) {
+                console.log('Closing the new tab as going to re-use existing tab:', tab_id);
+                chrome.tabs.remove(details.tabId, function () {
+                });
+            } else {
+                console.log('Could not find the target tab, so using this new tab.');
+                return {redirectUrl: details.url}
+            }
+
+            var match = caseId_regex.exec(details.url);
+            console.log("matches = " + match);
+
+            if (match && match.length > 1) {
+                var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
+                //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
+                //var new_url = "https://customers.atscale.com/" + match[2];
+                //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
+                console.log("New URL = " + new_url);
+                chrome.tabs.update(tab_id, {"active": true, url: new_url});
+            }
+        });
     });
 }
