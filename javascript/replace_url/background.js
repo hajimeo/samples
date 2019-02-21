4dd267d1f4b8011ffce30578bd7de94144044161
hajime <github@hajigle.com>
2019-02-21 10:49:29 +1000

    refactored

--- javascript/replace_url/background.js
+++ javascript/replace_url/background.js
@@ -6,21 +6,37 @@ chrome.webRequest.onBeforeRequest.addListener(replaceUrl, {
     types: ["main_frame"]
 }, ["blocking"]);
 
+// Assuming ID starts with 50, and protocol + (hostname/path_to_id=) + (caseId), so that the index of groups is 2
+var caseId_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/r/Case/|.+\.visual\.force\.com/apex/Case_Lightning.*[?&]id=|customers\.atscale\.com/s/case/)(50[^\?&/]+)");
+var tab_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/[ro]/Case/)");
+var ignore_regex = new RegExp("^https://.+\.lightning\.force\.com/lightning/(_classic/)");
+
 function replaceUrl(details) {
     console.log("=== Start 'replaceUrl' ================================");
     console.log("Request: details.tabId = " + details.tabId + " | url = " + details.url);
-    // Assuming ID starts with 50, and protocol + (hostname/path_to_id=) + (caseId), so that the index of groups is 2
-    var caseId_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/r/Case/|.+\.visual\.force\.com/apex/Case_Lightning.*[?&]id=|customers\.atscale\.com/s/case/)(50[^\?&/]+)");
-    var tab_regex = new RegExp("^https://(.+\.lightning\.force\.com/lightning/[ro]/Case/)");
-    var ignore_regex = new RegExp("^https://.+\.lightning\.force\.com/lightning/([ro]/Case/|_classic/)");
 
-    console.log('Current tab: ', window.location.toString());
-    if (ignore_regex.exec(window.location.toString())) {
-        console.log('Current tab is in ignore_regex (so no action required.');
+    console.log('Current URL: ', window.location.toString());
+    if (tab_regex.exec(window.location.toString())) {
+        console.log('Current URL is almost same as the target URL (so no action required.');
         return {redirectUrl: details.url}
     }
 
+    console.log('Requested URL: ', details.url);
+    if (ignore_regex.exec(details.url)) {
+        console.log('Requested URL is in ignore_regex (so no action required.');
+        return {redirectUrl: details.url}
+    }
+
+    var match = caseId_regex.exec(details.url);
+    console.log("matches = " + match);
+    if (!match) {
+        console.log("no match, so returning the original URL");
+        return {redirectUrl: details.url}
+    }
+
+    // Get the list of currently opened tabs, to find the target/updating tab
     chrome.tabs.query({currentWindow: true}, function (tabs) {
+        // It seems no 'break' in forEach?, so storing id into tab_id
         var tab_id = null;
         tabs.forEach(function (tab) {
             if (tab_id === null && tab.id != details.tabId) {
@@ -37,20 +53,15 @@ function replaceUrl(details) {
             chrome.tabs.remove(details.tabId, function () {
             });
         } else {
-            console.log('Could not find the target tab, so using this new tab.');
+            console.log('Could not find the target tab or tab ID is same, so using a new tab.');
             return {redirectUrl: details.url}
         }
 
-        var match = caseId_regex.exec(details.url);
-        console.log("matches = " + match);
-
-        if (match && match.length > 1) {
-            var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
-            //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
-            //var new_url = "https://customers.atscale.com/" + match[2];
-            //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
-            console.log("New URL = " + new_url);
-            chrome.tabs.update(tab_id, {"active": true, url: new_url});
-        }
+        var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
+        //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
+        //var new_url = "https://customers.atscale.com/" + match[2];
+        //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
+        console.log("New URL = " + new_url);
+        chrome.tabs.update(tab_id, {"active": true, url: new_url});
     });
 }
