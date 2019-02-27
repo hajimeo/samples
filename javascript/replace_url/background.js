bad6fc5721dfd5381bbe6623bad8a0c5c1c87bee
hajime <github@hajigle.com>
2019-02-28 09:57:07 +1000

    Trying to fix duplicate runs (one URL request calls this function twice), but not working because SalesForce changes URL after replacing.

--- javascript/replace_url/background.js
+++ javascript/replace_url/background.js
@@ -15,6 +15,7 @@ function replaceUrl(details) {
     console.log("=== Start 'replaceUrl' ================================");
     console.log("Request: details.tabId = " + details.tabId + " | url = " + details.url);
 
+    // TODO: this is not working as expected. always extension url
     console.log('Current URL: ', window.location.toString());
     if (tab_regex.exec(window.location.toString())) {
         console.log('Current URL is almost same as the target URL (so no action required.');
@@ -34,34 +35,47 @@ function replaceUrl(details) {
         return {redirectUrl: details.url}
     }
 
+    var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
+    //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
+    //var new_url = "https://customers.atscale.com/" + match[2];
+    //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
+    console.log("New URL = " + new_url);
+
     // Get the list of currently opened tabs, to find the target/updating tab
     chrome.tabs.query({currentWindow: true}, function (tabs) {
-        // It seems no 'break' in forEach?, so storing id into tab_id
-        var tab_id = null;
+        // It seems no 'break' in forEach?, so storing the target tab.
+        var target_tab = null;
         tabs.forEach(function (tab) {
-            if (tab_id === null && tab.id.toString() != details.tabId.toString()) {
+            //console.log('Checking id:' + tab.id + ' vs. ' + details.tabId + ' url:' + tab.url);
+            if (target_tab === null) {
                 if (tab_regex.exec(tab.url)) {
-                    tab_id = tab.id;
-                    console.log('The target tab is already opened and id is ', tab_id);
-                    console.log('and URL is ', tab.url);
+                    target_tab = tab;
+                    console.log('Found the target tab to replace URL, which id is ', target_tab.id);
+                    console.log('and URL is ', target_tab.url);
+
+                    // TODO: This is not working as SalesForce changes URL slightly (potentially salesforce bug?)
+                    if (target_tab.url.toString() == new_url.toString()) {
+                        console.log('New URL is exactly same, so that just focusing. url:' + new_url.toString());
+                        chrome.tabs.update(target_tab.id, {"active": true});
+                    } else if (target_tab.url.toString() == details.url.toString()) {
+                        console.log('URL is exactly same, so that just focusing. url:' + details.url.toString());
+                        chrome.tabs.update(target_tab.id, {"active": true});
+                    } else {
+                        console.log('Redirecting to url:' + new_url.toString());
+                        chrome.tabs.update(target_tab.id, {"active": true, url: new_url});
+                    }
+
+                    if (target_tab.id.toString() != details.tabId.toString()) {
+                        console.log('Closing the newly opened tab ' + details.tabId.toString());
+                        chrome.tabs.remove(details.tabId, function () { });
+                    }
                 }
             }
         });
 
-        if (tab_id && tab_id.toString() != details.tabId.toString()) {
-            console.log('Closing the new tab '+details.tabId.toString()+' as going to re-use existing tab:'+tab_id.toString());
-            chrome.tabs.remove(details.tabId, function () {
-            });
-        } else {
+        if (!target_tab) {
             console.log('Could not find the target tab or tab ID is same, so using a new tab.');
             return {redirectUrl: details.url}
         }
-
-        var new_url = "https://atscale2ndorg.lightning.force.com/lightning/r/Case/" + match[2] + "/view";
-        //var new_url = "https://na63.salesforce.com/console#%2F" + match[2];
-        //var new_url = "https://customers.atscale.com/" + match[2];
-        //var new_url = "https://customers.atscale.com/s/case/" + match[2] + "/detail";
-        console.log("New URL = " + new_url);
-        chrome.tabs.update(tab_id, {"active": true, url: new_url});
     });
 }
