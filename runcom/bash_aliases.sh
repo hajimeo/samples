4b0a4890c1dfdaea07f081a931b2519148839093
hajime <github@hajigle.com>
2019-02-28 17:58:05 +1000

    using IPs

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -28,10 +28,10 @@ alias ss="bash $HOME/IdeaProjects/samples/bash/setup_standalone.sh"
 alias mb='java -jar ~/Applications/metabase.jar'    # port is 3000
 alias vnc='nohup java -jar ~/Applications/tightvnc-jviewer.jar &>/tmp/tightvnc-jviewer.out &'
 
-# Chrome aliases for Mac
+# Chrome aliases for Mac (URL needs to be IP as hostname wouldn't be resolvable on remote)
 alias shib-dh1='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/dh1 --proxy-server=socks5://dh1:28081 https://192.168.0.31:4200/webuser/'
-alias shib-spt='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/spt --proxy-server=socks5://support:28081 https://support:4200/webuser/'
-alias shib-haj='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/haj --proxy-server=socks5://hajime:28081 https://hajime:4200/webuser/'
+alias shib-spt='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/spt --proxy-server=socks5://support:28081 https://192.168.6.162:4200/webuser/'
+alias shib-haj='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/haj --proxy-server=socks5://hajime:28081 https://192.168.6.163:4200/webuser/'
 alias hblog='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/hajigle https://www.blogger.com/blogger.g?blogID=9018688091574554712&pli=1#allposts'
 
 # Python simple http server from the specific dir
