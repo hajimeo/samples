273576ad6c184898eeeea23a437bf86de2dc7f96
hajime <github@hajigle.com>
2019-03-05 17:05:25 +1000

    no todo

--- bash/start_hdp.sh
+++ bash/start_hdp.sh
@@ -3069,7 +3069,6 @@ function f_socks5_proxy() {
     apt-get install -y autossh || return $?
     f_useradd "socks5user" "socks5user" "Y" || return $?
 
-    # TODO: currently using ssh
     touch /tmp/ssh_socks5.out
     chmod 777 /tmp/ssh_socks5.out
     local _cmd="autossh -4gC2TxnNf -D${_port} socks5user@localhost &> /tmp/ssh_socks5.out"
