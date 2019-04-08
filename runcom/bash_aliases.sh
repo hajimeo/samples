8328c5d9fd6fe49fbda00e5b9d98ad73bcf8ea18
hajime <github@hajigle.com>
2019-04-08 21:06:24 +1000

    another IP change

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -29,7 +29,7 @@ alias mb='java -jar ~/Applications/metabase.jar'    # port is 3000
 alias vnc='nohup java -jar ~/Applications/tightvnc-jviewer.jar &>/tmp/tightvnc-jviewer.out &'
 
 # Chrome aliases for Mac (URL needs to be IP as hostname wouldn't be resolvable on remote)
-alias shib-dh1='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/dh1 --proxy-server=socks5://dh1:28081 https://192.168.0.31:4200/webuser/'
+alias shib-dh1='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/dh1 --proxy-server=socks5://dh1:28081 https://192.168.1.31:4200/webuser/'
 alias shib-sth='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/spt --proxy-server=socks5://sethdesktop:28081 https://192.168.6.160:4200/webuser/'
 alias shib-haj='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/haj --proxy-server=socks5://hajime:28081 https://192.168.6.163:4200/webuser/'
 alias hblog='open -na "Google Chrome" --args --user-data-dir=$HOME/.chromep/hajigle https://www.blogger.com/blogger.g?blogID=9018688091574554712&pli=1#allposts'
