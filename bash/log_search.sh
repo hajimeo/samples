4d95ae9e69c5072780609bbd13d1da3217191d39
hajime <github@hajigle.com>
2019-04-26 10:29:25 +1000

    accept rg extra options (sometimes i wanted to stop using -A 1)

--- bash/log_search.sh
+++ bash/log_search.sh
@@ -405,16 +405,16 @@ function f_grep_multilines() {
     local __doc__="Multiline search with 'rg'. TODO: dot and brace can't be used in _str_in_1st_line"
     local _str_in_1st_line="$1"         # TODO: At this moment, grep-ing lines which *first* line contain this string
     local _glob="${2:-"debug.*log*"}"
-    local _boundary_str="${3:-"^2\\d\\d\\d-\\d\\d-\\d\\d.\\d\\d:\\d\\d:\\d\\d"}"
-    local _how_many="${4:-2000}"
+    local _rg_extra_opt="${3-"-A 1 -m 2000"}"   # Accept *empty string*
+    local _boundary_str="${4:-"^2\\d\\d\\d-\\d\\d-\\d\\d.\\d\\d:\\d\\d:\\d\\d"}"
 
     # TODO: if next line also contains the queryId, it won't be included in the output, so using -A 1 at this moment. ("(?!^2\d\d\d-\d\d-\d\d.\d\d:\d\d:\d\d)" didn't work)
     local _regex="${_boundary_str}[^\n]+?${_str_in_1st_line}.+?(${_boundary_str}|\z)"
-    echo "# regex:${_regex}" >&2
+    echo "# regex:${_regex} -g '${_glob}' ${_rg_extra_opt}" >&2
     rg "${_regex}" \
         --multiline --multiline-dotall --no-line-number --no-filename -z \
-        -g "${_glob}" -A 1 --sort path -m ${_how_many}
-    # not sure if rg sorts properly with --sort, so best effort.
+        -g "${_glob}" ${_rg_extra_opt} --sort=path
+    # not sure if rg sorts properly with --sort, so best effort (can not use ' | sort' as multi-lines)
 }
 
 function f_genLdapsearch() {
