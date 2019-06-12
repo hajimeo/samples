219a021d893f615ffc4887115c3a3c50ef99805a
hajime <github@hajigle.com>
2019-06-12 16:40:28 +1000

    added a command example (to recommend to use "Y" in 5th arg)

--- bash/patch_java.sh
+++ bash/patch_java.sh
@@ -148,7 +148,8 @@ if [ "$0" = "$BASH_SOURCE" ]; then
         if [ -n "${_JAR_FILEPATH}" ] && [ -d "${_JAR_FILEPATH}" ]; then
             f_jargrep "${_CLASS_NAME}.class" "${_JAR_FILEPATH}"
             [ -n "${_UPDATING_CLASSNAME}" ]  && f_jargrep "${_UPDATING_CLASSNAME}.class" "${_JAR_FILEPATH}"
-            echo "Please pick a jar file from above, and re-run the script."
+            echo "Please pick a jar file from above, and re-run the script:
+$0 '$1' '$2' '<jar path from above>' '$4' 'Y'"
             exit 0
         fi
     fi
