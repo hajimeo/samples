2d90755844baab8ae4bb46acb77fe442c8477f06
hajime <github@hajigle.com>
2019-08-02 15:53:12 +1000

    initial commit

---
+++ bash/collect_stats.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# NOTE: This script requires "lsof", so may require root priv.
+#       Put this script in /etc/cron.hourly/ with *execution* permission.
+
+_PORT="10502"
+_PID="$(lsof -ti:${_PORT} -s TCP:LISTEN)" || exit 1
+_USER="$(stat -c '%U' /proc/${_PID})" || exit 1
+_DIR="$(strings /proc/${_PID}/environ | sed -nr 's/^AS_LOG_DIR=(.+)/\1/p')" || exit 1
+[ ! -d "${_DIR}" ] && _DIR="/tmp"
+
+function cmds() {
+    local _user="${1:-${_USER}}"
+    date -u
+    lsof -nPu ${_user} | awk '{print "PID-->"$2}' | uniq -c | sort -n | tail -5
+    echo -n "Total AtScale Usage -->";lsof -u ${_user} | wc -l
+    echo -n "Total System Usage -->";lsof | wc -l
+    echo "AtScale User processes are -->"; lsof -nPu ${_user} | awk '{print $2}' | uniq -c | sort -n | tail -5 | awk '{print $2}' |while IFS= read a_pid ; do pwdx $a_pid; done
+    echo "Memory Usage --->"; free -tm
+    echo "Disk Usage --->"; df -h /
+    echo "CPU Usage --->"; mpstat -P ALL
+    echo ""
+}
+
+if [ "$0" = "$BASH_SOURCE" ]; then
+    _FILE_PATH="${_DIR%/}/as_usage_$(date +%u).out"
+
+    if [ -s "${_FILE_PATH}" ]; then
+        _LAST_MOD_TS=$(stat -c%Y ${_FILE_PATH})
+        _NOW=$(date +%s)
+        # If older than one day, clear
+        [ 86400 -lt $((${_NOW} - ${_LAST_MOD_TS})) ] && > ${_FILE_PATH}
+    fi
+
+    cmds
+    sleep 30m
+    cmds
+fi
