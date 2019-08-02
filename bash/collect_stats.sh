f2c997d90547c7273672cb9e354160764d6bd9ad
hajime <github@hajigle.com>
2019-08-02 16:34:53 +1000

    added health check

--- bash/collect_stats.sh
+++ bash/collect_stats.sh
@@ -4,6 +4,7 @@
 # curl -o /etc/cron.hourly/collect_stats.sh https://raw.githubusercontent.com/hajimeo/samples/master/bash/collect_stats.sh && chmod a+x /etc/cron.hourly/collect_stats.sh
 
 _PORT="10502"
+_URLS=""    # url per line
 
 function cmds() {
     local _user="${1:-${_USER}}"
@@ -15,7 +16,21 @@ function cmds() {
     echo "Memory Usage --->"; free -tm
     echo "Disk Usage --->"; df -h /
     echo "CPU Usage --->"; mpstat -P ALL
+    top -c -b -n 1 | head -n 20
+    netstat -i
     echo ""
+}
+
+function health() {
+    local _url="$1"
+    [[ "${_url}" =~ ^https?://([^:/]+) ]]
+    local _host="${BASH_REMATCH[1]}"
+    if [ -n "${_host}" ]; then
+        echo "Ping to ${_host} check --->"
+        ping -n -W 1 -c 1 ${_host}
+    fi
+    echo "${_url} check --->"
+    curl -s -m 10 --retry 1 -w " - %{time_starttransfer}\n" -k "${_url}"
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then
@@ -37,6 +52,9 @@ if [ "$0" = "$BASH_SOURCE" ]; then
 
     for i in `seq 1 ${_PER_HOUR}`; do
         cmds &> ${_FILE_PATH}
+        if [ -n "${_URLS}" ]; then
+            echo "${_URLS}" | while read -r _u; do health "$_u"; done
+        fi
         sleep ${_INTERVAL}
     done
 fi
