441522a688bc99dbec3f3225afc03f4bf379f200
hajime <github@hajigle.com>
2019-08-13 13:32:19 +1000

    added f_jarp

--- java/byteman/byteman-install.sh
+++ java/byteman/byteman-install.sh
@@ -3,14 +3,39 @@
 #
 
 _VER="${1:-"4.0.7"}"
+_WORD_DIR="/var/tmp/share"
 
 function f_setup() {
     local _ver="${1:-"${_VER}"}"
+    local _dir="${2:-"${_WORD_DIR}"}"
+
     which bmjava && return 0
-    curl -O -C - --retry 3 "https://downloads.jboss.org/byteman/${_ver}/byteman-download-${_ver}-bin.zip"
-    unzip byteman-download-${_ver}-bin.zip
-    export BYTEMAN_HOME=$(pwd)/byteman-download-${_ver}
+
+    if [ ! -d "${_dir}" ]; then
+        mkdir -p -m 777 "${_dir}" || return $?
+    fi
+    curl -o "${_dir%/}/byteman-download-${_ver}-bin.zip" -C - --retry 3 "https://downloads.jboss.org/byteman/${_ver}/byteman-download-${_ver}-bin.zip"
+    unzip "${_dir%/}/byteman-download-${_ver}-bin.zip" -d ${_dir%/} || return $?
+    export BYTEMAN_HOME=${_dir%/}/byteman-download-${_ver}
     export PATH=$BYTEMAN_HOME/bin:$PATH
+}
+
+function f_jarp() {
+    local _jarfile="$1"
+
+    classes=$(unzip -l "${_jarfile}" | awk '/\.class$/ {print $4}' | sed -e 's,/,.,g' -e 's,\.class,,g' | sort)
+
+    javap -classpath "${_jarfile}" -p $classes | grep -v lambda | while IFS= read -r line; do
+        line=" $line"
+        method=
+        case $line in
+            *\ class\ *) class=${line/* class }; class=${class/ *}; ;;
+            *\(*) method=${line/(*}; method=${method/* } ; method=${method/*.} ;;
+        esac
+        if [ -n "$class" -a -n "$method" ]; then
+            echo $class\#$method
+        fi
+    done
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then
