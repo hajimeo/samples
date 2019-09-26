07588a4ccec1b162761485e08858be662f87d934
hajime <github@hajigle.com>
2019-09-26 12:08:51 +1000

    base64 encode/decode

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -2,9 +2,12 @@
 # 'cd' to last modified directory
 alias cdl='cd "`ls -dtr ./*/ | tail -n 1`"'
 alias pjt='python -m json.tool'
-alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+alias urldecode='python -c "import sys, urllib as ul; print(ul.unquote_plus(sys.argv[1]))"'
 #alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote(sys.argv[1])"'
+alias urlencode='python -c "import sys, urllib as ul; print( )ul.quote(sys.argv[1]))"'
+# base64 encode/decode (coreutils base64 or openssl base64 -e|-d)
+alias b64encode='python -c "import sys, base64; print(base64.b64encode(sys.argv[1]))"'
+alias b64decode='python -c "import sys, base64; print(base64.b64decode(sys.argv[1]))"'
 alias utc2int='python -c "import sys,time,dateutil.parser;print int(time.mktime(dateutil.parser.parse(sys.argv[1]).timetuple()))"'  # doesn't work with yy/mm/dd (2 digits year)
 alias int2utc='python -c "import sys,time;print time.asctime(time.gmtime(int(sys.argv[1])))+\" UTC\""'
 #alias pandas='python -i <(echo "import sys,json;import pandas as pd;f=open(sys.argv[1]);jd=json.load(f);pdf=pd.DataFrame(jd);")'   # Start python interactive after loading json object in 'pdf' (pandas dataframe)
