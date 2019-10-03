f611dba0ea61056d15d3ef53326c5785d52f2f6b
hajime <github@hajigle.com>
2019-10-04 09:38:44 +1000

    refactoring + checking all windows (previously only current window)

--- javascript/no_newtab/background.js
+++ javascript/no_newtab/background.js
@@ -5,31 +5,39 @@ chrome.webRequest.onBeforeRequest.addListener(noNewTab, {
   types: ["main_frame"]
 }, ["blocking"]);
 
-function noNewTab(r) {
-  console.log("=== Start 'noNewTab' ================================");
-  console.log("Request: r.tabId = " + r.tabId + " | url = " + r.url);
+function noNewTab(req) {
+  // NOTE: You can tick Show timestamp in DevTools settings
+  console.log("New req:", req);
 
-  // Get the list of currently opened tabs, to find the target/updating tab
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    // It seems no 'break' in forEach?, so storing the target tab.
+  // Get the list of currently opened tabs from *all* windows.
+  chrome.tabs.query({currentWindow: false}, function(tabs) {
+    // It seems no 'break' in forEach?, so using 'target_tab'.
     var target_tab = null;
     tabs.forEach(function(tab) {
-      //console.log('Checking id:' + tab.id + ' vs. ' + r.tabId + ' url:' + tab.url);
+      //console.log('Checking tab:', tab);
       if (target_tab === null) {
-        if (tab.url.toString() == r.url.toString() && tab.id.toString() != r.tabId.toString()) {
+        if (tab.url.toString() === req.url.toString() && tab.id.toString() !== req.tabId.toString()) {
           target_tab = tab;
-          console.log('Found the target tab which uses same URL but different tab (id):', target_tab.id);
-          console.log('and URL is ', target_tab.url);
-          chrome.tabs.update(target_tab.id, {"active": true});
-          console.log('Closing the newly opened tab ' + r.tabId.toString());
-          chrome.tabs.remove(r.tabId, function() {
-          });
         }
       }
     });
 
-    if (!target_tab) {
-      return {redirectUrl: r.url}
+    if (target_tab) {
+      console.log('Found a target tab which uses same URL:', target_tab);
+      chrome.tabs.update(target_tab.id, {"active": true});
+      chrome.windows.update(target_tab.windowId, {focused: true});
+      chrome.tabs.remove(req.tabId, function() {
+        if (chrome.runtime.lastError) {
+          console.log("Last Error after chrome.tabs.remove:", chrome.runtime.lastError);
+        }
+        else {
+          console.log('Closed the newly opened tab (req)');
+        }
+      });
+    }
+    else {
+      console.log('No matching tab, so just redirecting to URL:', req.url);
+      return {redirectUrl: req.url}
     }
   });
 }
