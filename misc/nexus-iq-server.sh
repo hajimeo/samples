179bfcffba49424c84faa9908bcfa810669c706e
hajime <github@hajigle.com>
2020-03-12 19:05:28 +1000

    "log" directory was created in unwanted location.

--- misc/nexus-iq-server.sh
+++ misc/nexus-iq-server.sh
@@ -16,6 +16,7 @@
 ### END INIT INFO
 
 NEXUS_IQ_SERVER_HOME=/opt/sonatype/nexus-iq-server
+NEXUS_IQ_SONATYPEWORK=/opt/sonatype/sonatype-work/clm-server
 # The user ID which should be used to run the IQ Server
 # # IMPORTANT - Make sure that the user has the required privileges to write into the IQ Server work directory.
 RUN_AS_USER="${_NXIQ_USER:-"sonatype"}"
@@ -25,21 +26,21 @@ JAVA_OPTIONS="-Xms${_XMX} -Xmx${_XMX} -XX:+UseG1GC -verbose:gc -XX:+PrintGC -XX:
 
 do_start()
 {
-    cd ${NEXUS_IQ_SERVER_HOME}
+    cd ${NEXUS_IQ_SONATYPEWORK}
     # Original uses su -m which can inherits almost all env of current user (eg: root), not sure if it was intentional
-    sudo -u ${RUN_AS_USER} java ${JAVA_OPTIONS} -jar ./nexus-iq-server-*.jar server ./config.yml &> /tmp/nexus_iq_server.out &
+    sudo -u ${RUN_AS_USER} java ${JAVA_OPTIONS} -jar ${NEXUS_IQ_SERVER_HOME%/}/nexus-iq-server-*.jar server ${NEXUS_IQ_SERVER_HOME%/}/config.yml &> /tmp/nexus_iq_server.out &
     echo "Started nexus-iq-server"
 }
 
 do_console()
 {
-    cd ${NEXUS_IQ_SERVER_HOME}
-    sudo -u ${RUN_AS_USER} java ${JAVA_OPTIONS} -jar ./nexus-iq-server-*.jar server ./config.yml
+    cd ${NEXUS_IQ_SONATYPEWORK}
+    sudo -u ${RUN_AS_USER} java ${JAVA_OPTIONS} -jar ${NEXUS_IQ_SERVER_HOME%/}/nexus-iq-server-*.jar server ${NEXUS_IQ_SERVER_HOME%/}/config.yml
 }
 
 do_stop()
 {
-    local pid=$(ps -o pid,command -u ${RUN_AS_USER} -U ${RUN_AS_USER} | grep -m1 -P '^java .+/nexus-iq-server.*jar server\b' | awk '{print $1}')
+    local pid=$(ps -o pid,command -u ${RUN_AS_USER} -U ${RUN_AS_USER} | grep -m1 -P '\bjava .+/nexus-iq-server.*jar server ' | awk '{print $1}')
     if [ -n "${pid}" ]; then
         kill $pid || return $?
         echo "Killed nexus-iq-server - PID $pid"
