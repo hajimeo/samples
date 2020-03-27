9937e2ef8947df0c6de9f8a9ef42484e89eb743e
hajime <github@hajigle.com>
2020-03-27 18:38:10 +1000

    added locale

--- bash/hwx_triage.sh
+++ bash/hwx_triage.sh
@@ -61,6 +61,8 @@ function f_check_system() {
 
     uname -a &> ${_work_dir%/}/uname-a.out
     lsb_release -a &>> ${_work_dir%/}/uname-a.out
+    localectl status &> ${_work_dir%/}/locale.out
+    locale &>> ${_work_dir%/}/locale.out
     dmesg | grep 'link up' &> ${_work_dir%/}/dmesg_link_up.out
     hdp-select &> ${_work_dir%/}/hdp-select.out
     ls -l /usr/hdp/current/ >> ${_work_dir%/}/hdp-select.out
