f0f434b0fa7eb29c4bc7a9946f4dbc6ae2ac2557
hajime <github@hajigle.com>
2020-05-14 10:07:38 +1000

    f_log2csv with example command

--- bash/log_search.sh
+++ bash/log_search.sh
@@ -935,6 +935,20 @@ function f_request2csv() {
         -o -r "${_pattern_out}" ${_g_opt} "${_glob}" >> ${_out_file}
 }
 
+#f_log2csv "(Starting|Finished) upload to key (.+) in bucket" nexus.log ",\"\$6\",\"\$7\"" ",\"\$6\",\"\$7\"" ",start_end,key" > ./s3_upload.csv
+#qcsv -H "SELECT min(datetime) as min_dt, max(datetime) as max_dt, CAST((julianday(max(datetime)) - julianday(min(datetime))) * 8640000 AS INT) as duration_ms, SUM(CASE WHEN start_end = 'Starting' THEN 1 WHEN start_end = 'Finished' THEN -1 ELSE -99999 END) as sum_start_end, key FROM ./s3_upload.csv GROUP BY key HAVING sum_start_end = 0 ORDER BY min_dt"
+function f_log2csv() {
+    local _log_regex="$1"
+    local _glob="${2:-"*.log"}"
+    local _r_from_6_append="${3}"
+    local _col_append="${4}"
+    # Sqlite does not like "," before milliseconds
+    rg "^(${_DATE_FORMAT}.\d\d:\d\d:\d\d).([\d]+)[^\[]+\[([^\]]+)\] [^ ]* ([^ ]*) ([^ ]+) .*${_log_regex}" -o -r '"$1.$2","$3","$4","$5"'${_r_from_6_append} --no-filename -g "${_glob}" > /tmp/f_log2csv_$$.out
+    if [ -s /tmp/f_log2csv_$$.out ]; then
+        echo "datetime,thread,user,class${_col_append}" | cat - /tmp/f_log2csv_$$.out
+    fi
+}
+
 function _gen_pattern() {
     local _pattern_str="${1}"
     local _pattern=""
