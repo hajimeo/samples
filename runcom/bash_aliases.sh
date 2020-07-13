1131137dc31c078857f8cc23453d65ae7cfc9096
hajime <github@hajigle.com>
2020-07-13 16:38:05 +1000

    added mvn-arch-gen

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -436,6 +436,20 @@ function iqMvn() {
     # https://help.sonatype.com/display/NXI/Sonatype+CLM+for+Maven
     mvn com.sonatype.clm:clm-maven-plugin:evaluate -Dclm.additionalScopes=test,provided,system -Dclm.applicationId=sandbox-application -Dclm.serverUrl=http://dh1.standalone.localdomain:8070/ -Dclm.username=admin -Dclm.password=admin123
 }
+function mvn-arch-gen() {
+    # https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
+    local _gav="${1:-"com.example:my-app:1.0"}"
+    local _type="${2:-"maven-archetype-quickstart"}"
+    local _localrepo="${3-"./local_repo"}"
+    local _options="${4-"-Dorg.slf4j.simpleLogger.showDateTime=true -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss,SSS -U -X"}"
+    if [[ "${_gav}" =~ ^([^:]+):([^:]+):([^:]+)$ ]]; then
+        local _g="${BASH_REMATCH[1]}"
+        local _a="${BASH_REMATCH[2]}"
+        local _v="${BASH_REMATCH[3]}"
+        [ -n "${_localrepo}" ] && _options="${_options% } -Dmaven.repo.local=${_localrepo}"
+        mvn archetype:generate -DgroupId=${_g} -DartifactId=${_a} -DarchetypeArtifactId=${_type} -DarchetypeVersion=${_v} -DinteractiveMode=false ${_options}
+    fi
+}
 function mvn-get() {
     # maven/mvn get/download
     local _gav="$1" # eg: junit:junit:4.12
