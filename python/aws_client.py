f3b173749e952b311d79010a526784aca51266c4
hajime <github@hajigle.com>
2020-09-10 15:43:22 +1000

    a bit of cleanup

--- python/aws_client.py
+++ python/aws_client.py
@@ -1,15 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Test / demo / example of AWS client in Python
+
+aws_client.py <access-key> <secret-key> <bucket> [<prefix>]
+'''
+
+import sys, pprint
 import boto3
-accessKey = "*********"
-secretKey = "*********"
-s3_bucket = "xxxxxx"
-s3_prefix = "yyyyyy"
 
+accessKey = sys.argv[1]
+secretKey = sys.argv[2]
+s3_bucket = sys.argv[3]
+s3_prefix = ""
+if len(sys.argv) > 4:
+    s3_prefix = sys.argv[4]
+
+contents = None
 s3 = boto3.client('s3', aws_access_key_id=accessKey, aws_secret_access_key=secretKey)
+# @see: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.list_objects_v2
+response = s3.list_objects_v2(Bucket=s3_bucket, Prefix=s3_prefix)
 
-partial_list = s3.list_objects_v2(Bucket=s3_bucket, Prefix=s3_prefix)
-obj_list = partial_list['Contents']
-while partial_list['IsTruncated']:
-    next_token = partial_list['NextContinuationToken']
-    partial_list = s3.list_objects_v2(Bucket=s3_bucket, Prefix=s3_prefix, ContinuationToken=next_token)
-    obj_list.extend(partial_list['Contents'])
-print(obj_list)
+if 'Contents' in response:
+    contents = response['Contents']
+
+if 'IsTruncated' in response:
+    while response['IsTruncated']:
+        next_token = response['NextContinuationToken']
+        response = s3.list_objects_v2(Bucket=s3_bucket, Prefix=s3_prefix, ContinuationToken=next_token)
+        contents.extend(response['Contents'])
+
+pprint.pprint(contents)
