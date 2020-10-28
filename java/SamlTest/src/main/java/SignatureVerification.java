f0dad4577442788576f59b33d91f74e1ab9dbf66
hajime <github@hajigle.com>
2020-10-28 15:00:07 +1000

    still not working "Signature did not validate against the credential's key"

--- java/SamlTest/src/main/java/SignatureVerification.java
+++ java/SamlTest/src/main/java/SignatureVerification.java
@@ -3,33 +3,32 @@
  *  https://wiki.shibboleth.net/confluence/plugins/viewsource/viewpagesrc.action?pageId=3277047
  *  https://stackoverflow.com/questions/17890642/opensaml-2-0-signature-validation-not-working
  *  http://www.java2s.com/Tutorials/Java/org.w3c.dom/Document/0280__Document.getDocumentElement_.htm
+ *  Invalid signature file digest for Manifest main attributes
+ *  https://stackoverflow.com/questions/42540485/how-to-stop-maven-shade-plugin-from-blocking-java-util-serviceloader-initializat
+ *
+ *  export CLASSPATH=$(echo $PWD/lib/*.jar | tr ' ' ':'):$PWD/target/SamlTest-1.0-SNAPSHOT.jar:.
  */
 
+import org.opensaml.Configuration;
+import org.opensaml.DefaultBootstrap;
 import org.opensaml.saml2.core.Response;
 import org.opensaml.xml.io.Unmarshaller;
 import org.opensaml.xml.io.UnmarshallerFactory;
-import org.opensaml.*;
-import org.opensaml.xml.io.UnmarshallingException;
 import org.opensaml.xml.security.x509.BasicX509Credential;
+import org.opensaml.xml.signature.Signature;
 import org.opensaml.xml.signature.SignatureValidator;
-import org.opensaml.xml.validation.ValidationException;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 
-public class SignatureVarification {
+public class SignatureVerification {
     public static void main(String[] args) {
         try {
             String cert_path = args[0];
@@ -44,31 +43,41 @@ public class SignatureVarification {
 
             File samlXmlFile = new File(saml_xml);
             Document document = builder.parse(samlXmlFile);
+
+            DefaultBootstrap.bootstrap();
             UnmarshallerFactory unmarshallerFactory = Configuration.getUnmarshallerFactory();
             Unmarshaller unmarshaller = unmarshallerFactory.getUnmarshaller(document.getDocumentElement());
             Response response = (Response) unmarshaller.unmarshall(document.getDocumentElement());
+            //System.err.println("response: "+response.getDOM().getTextContent());
+            //System.err.println("response: "+response.isSigned());
 
             //Get Public Key
             BasicX509Credential publicCredential = new BasicX509Credential();
             File publicKeyFile = new File(cert_path);
 
-            if (publicKeyFile.exists()) {
-                CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
-                InputStream fileStream = new FileInputStream(publicKeyFile);
-                X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(fileStream);
-                fileStream.close();
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            InputStream fileStream = new FileInputStream(publicKeyFile);
+            X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(fileStream);
+            fileStream.close();
 
-                X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(certificate.getPublicKey().getEncoded());
-                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-                PublicKey key = keyFactory.generatePublic(publicKeySpec);
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(certificate.getPublicKey().getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey key = keyFactory.generatePublic(publicKeySpec);
 
-                //Validate Public Key against Signature
-                if (key != null) {
-                    publicCredential.setPublicKey(key);
-                    SignatureValidator signatureValidator = new SignatureValidator(publicCredential);
-                    signatureValidator.validate(response.getSignature());
-                }
+            //Validate Public Key against Signature
+            if (key == null) {
+                System.err.println(cert_path + " is not a correct X.509 certificate");
+                return;
             }
+
+            publicCredential.setPublicKey(key);
+            SignatureValidator signatureValidator = new SignatureValidator(publicCredential);
+            Signature signature = response.getSignature();
+            if (signature == null) {
+                signature = response.getAssertions().get(0).getSignature();
+            }
+            System.err.println("signature: "+signature.getCanonicalizationAlgorithm());
+            signatureValidator.validate(signature);
 
             // No error meas all good
             System.out.println("All good.");
