adc71df8305f019fbe9508e58fe2645c9fe746dd
hajime <github@hajigle.com>
2020-11-02 15:27:06 +1000

    python 3.8.6 in pyenv looks OK

--- bash/setup_work_env.sh
+++ bash/setup_work_env.sh
@@ -175,8 +175,8 @@ function f_setup_python() {
 
     # Jupyter related
     # TODO: Autocomplete doesn't work if different version is used. @see https://github.com/ipython/ipython/issues/11530
-    #       However, using 7.1.1 with python 3.8 now causes TypeError: required field "type_ignores" missing from Module
-    #${_python} -m pip install ipython==7.1.1
+    #       However, using 7.1.1 with python 3.8 may cause TypeError: required field "type_ignores" missing from Module
+    ${_python} -m pip install ipython==7.1.1 prettytable==0.7.2
     ${_python} -m pip install -U ipython jupyter jupyterlab --log /tmp/pip.log &>/dev/null || return $?
     # Need "-H"? eg: sudo -H ${_python} -m pip uninstall -y jupyterlab && sudo -H ${_python} -m pip install jupyterlab
     # Need to add /usr/local/Cellar/python/3.7.1/Frameworks/Python.framework/Versions/3.7/bin in PATH?
