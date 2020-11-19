b9984742ad1406288a60567272e851ab5ea5175d
hajime <github@hajigle.com>
2020-11-19 16:48:43 +1000

    didn't work but updated.

--- java/btrace/CatchAny.java
+++ java/btrace/CatchAny.java
@@ -10,22 +10,22 @@ import com.sun.btrace.annotations.*;
 @BTrace
 public class CatchAny {
     //@OnMethod(clazz="org.apache.ranger.security.web.filter.RangerKRBAuthenticationFilter",
-    @OnMethod(clazz="org.apache.ambari.server.controller.internal.UserPrivilegeResourceProvider",
-            method="toResource",
+    @OnMethod(clazz="org.apache.http.client.utils.HttpClientUtils",
+            method="closeQuietly",
             location=@Location(value=Kind.CALL, clazz="/.*/", method="/.*/")
     )
     public static void m1(@ProbeClassName String pcn, @ProbeMethodName String pmn, AnyType[] args) {
-        println("=== CALL toResource ========================");
+        println("=== CALL closeQuietly ========================");
         println(pcn);
         println(pmn);
         printArray(args);
     }
 
     // TODO: does not work?
-    @OnMethod(clazz = "/.*InvalidCacheLoadException/",
-            method = "/.*/",
-            location = @Location(Kind.RETURN))
-    public static void endMethod(@Self Exception self) {
-        jstack();
-    }
+    //@OnMethod(clazz = "/.*InvalidCacheLoadException/",
+    //        method = "/.*/",
+    //        location = @Location(Kind.RETURN))
+    //public static void endMethod(@Self Exception self) {
+    //    jstack();
+    //}
 }
