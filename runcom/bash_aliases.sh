92a04650d2a12fdb2ea9f20e2233d535518a1087
hajime <github@hajigle.com>
2020-12-01 19:14:08 +1000

    added avg_time()

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -291,6 +291,27 @@ function jargrep() {
     which jar &>/dev/null || _cmd="less"
     find -L ${2:-./} -type f -name '*.jar' -print0 | xargs -0 -n1 -I {} bash -c "${_cmd} {} | grep -wi '$1' && echo {}"
 }
+# from https://stackoverflow.com/questions/54920113/calculate-average-execution-time-of-a-program-using-bash
+function avg_time() {
+    #
+    # usage: avg_time n command ...
+    #
+    n=$1; shift
+    (($# > 0)) || return                   # bail if no command given
+    for ((i = 0; i < n; i++)); do
+        { time -p "$@" &>/dev/null; } 2>&1 # ignore the output of the command
+                                           # but collect time's output in stdout
+    done | awk '
+        /real/ { real = real + $2; nr++ }
+        /user/ { user = user + $2; nu++ }
+        /sys/  { sys  = sys  + $2; ns++}
+        END    {
+                 if (nr>0) printf("real %f\n", real/nr);
+                 if (nu>0) printf("user %f\n", user/nu);
+                 if (ns>0) printf("sys %f\n",  sys/ns)
+               }'
+}
+# convert .mov file to .gif
 function mov2gif() {
     local _in="$1"
     local _out="$2"
