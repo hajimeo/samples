e05a8879913d281075d590732c1779ca7264c97d
hajime <github@hajigle.com>
2020-12-16 10:17:33 +1000

    committing an example before custimising.

---
+++ python/ldapproxy.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+# @see: https://ldaptor.readthedocs.io/en/latest/cookbook/ldap-proxy.html
+#       https://github.com/twisted/ldaptor
+#
+#       python3 -m pip install ldaptor
+
+from ldaptor.protocols import pureldap
+from ldaptor.protocols.ldap.ldapclient import LDAPClient
+from ldaptor.protocols.ldap.ldapconnector import connectToLDAPEndpoint
+from ldaptor.protocols.ldap.proxybase import ProxyBase
+from twisted.internet import defer, protocol, reactor
+from twisted.python import log
+from functools import partial
+import sys
+
+class LoggingProxy(ProxyBase):
+    """
+    A simple example of using `ProxyBase` to log requests and responses.
+    """
+    def handleProxiedResponse(self, response, request, controls):
+        """
+        Log the representation of the responses received.
+        """
+        log.msg("Request => " + repr(request))
+        log.msg("Response => " + repr(response))
+        return defer.succeed(response)
+
+def ldapBindRequestRepr(self):
+    l=[]
+    l.append('version={0}'.format(self.version))
+    l.append('dn={0}'.format(repr(self.dn)))
+    l.append('auth=****')
+    if self.tag!=self.__class__.tag:
+        l.append('tag={0}'.format(self.tag))
+    l.append('sasl={0}'.format(repr(self.sasl)))
+    return self.__class__.__name__+'('+', '.join(l)+')'
+
+pureldap.LDAPBindRequest.__repr__ = ldapBindRequestRepr
+
+if __name__ == '__main__':
+    """
+    Demonstration LDAP proxy; listens on localhost:10389 and
+    passes all requests to localhost:8080.
+    """
+    log.startLogging(sys.stderr)
+    factory = protocol.ServerFactory()
+    proxiedEndpointStr = 'tcp:host=localhost:port=8080'
+    use_tls = False
+    clientConnector = partial(
+        connectToLDAPEndpoint,
+        reactor,
+        proxiedEndpointStr,
+        LDAPClient)
+
+    def buildProtocol():
+        proto = LoggingProxy()
+        proto.clientConnector = clientConnector
+        proto.use_tls = use_tls
+        return proto
+
+    factory.protocol = buildProtocol
+    reactor.listenTCP(10389, factory)
+    reactor.run()
