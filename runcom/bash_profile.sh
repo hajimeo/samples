2dc515354c2e5770239b593019bfce9af815a241
hajime <github@hajigle.com>
2021-02-11 11:13:05 +1000

    updaed the comment for v 11

--- runcom/bash_profile.sh
+++ runcom/bash_profile.sh
@@ -53,7 +53,7 @@ if [ "$(uname)" = "Darwin" ]; then
     fi
     # java related
     if [ -f /usr/libexec/java_home ]; then
-        #[ -z "${JAVA_HOME}" ] && export JAVA_HOME=`/usr/libexec/java_home -v 10 2>/dev/null`
+        #[ -z "${JAVA_HOME}" ] && export JAVA_HOME=`/usr/libexec/java_home -v 11 2>/dev/null`
         [ -z "${JAVA_HOME}" ] && export JAVA_HOME=`/usr/libexec/java_home -v 1.8 2>/dev/null`
     fi
 fi
