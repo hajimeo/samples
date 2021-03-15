a86b8e95dd0bc98ac998cd5b1d771e80e6e84b56
hajime <github@hajigle.com>
2021-03-15 15:20:46 +1000

    wouldn't need to copy this script.

--- runcom/bash_aliases.sh
+++ runcom/bash_aliases.sh
@@ -533,7 +533,7 @@ function pubS() {
     cp -f $HOME/IdeaProjects/samples/bash/utils*.sh $HOME/share/sonatype/
     cp -f $HOME/IdeaProjects/samples/bash/setup_nexus3_repos.sh $HOME/share/sonatype/
     cp -f $HOME/IdeaProjects/samples/bash/setup_nexus3_repos.sh $HOME/IdeaProjects/nexus-toolbox/scripts/
-    cp -f $HOME/IdeaProjects/work/nexus-groovy/src2/TrustStoreConverter.groovy $HOME/IdeaProjects/nexus-toolbox/scripts/
+    #cp -f $HOME/IdeaProjects/work/nexus-groovy/src2/TrustStoreConverter.groovy $HOME/IdeaProjects/nexus-toolbox/scripts/
     wait
     date
     sync_nexus_binaries &>/dev/null &
