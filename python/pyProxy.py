d0161811ab9e119dce8f103a3090957d461d5d7f
hajime <github@hajigle.com>
2021-03-29 14:46:43 +1000

    formatting

--- python/pyProxy.py
+++ python/pyProxy.py
@@ -10,8 +10,9 @@ from socket import errno
 
 __version__ = '0.1.0 Draft 1 +modified by Hajime'
 BUFLEN = 8192
-VERSION = 'Python Proxy/'+__version__
+VERSION = 'Python Proxy/' + __version__
 HTTPVER = 'HTTP/1.1'
+
 
 class ConnectionHandler:
     def __init__(self, connection, address, timeout):
@@ -19,7 +20,7 @@ class ConnectionHandler:
         self.client_buffer = ''
         self.timeout = timeout
         self.method, self.path, self.protocol = self.get_base_header()
-        if self.method=='CONNECT':
+        if self.method == 'CONNECT':
             self.method_CONNECT()
         elif self.method in ('OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE'):
             self.method_others()
@@ -27,23 +28,23 @@ class ConnectionHandler:
         self.target.close()
 
     def log(self, msg):
-        sys.stdout.write('[%s] %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), str(msg))) #debug
+        sys.stdout.write('[%s] %s\n' % (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), str(msg)))  # debug
         sys.stdout.flush()
-    
+
     def get_base_header(self):
         while 1:
             self.client_buffer += self.client.recv(BUFLEN)
             end = self.client_buffer.find('\n')
-            if end!=-1:
+            if end != -1:
                 break
         self.log(self.client_buffer)
-        data = (self.client_buffer[:end+1]).split()
-        self.client_buffer = self.client_buffer[end+1:]
+        data = (self.client_buffer[:end + 1]).split()
+        self.client_buffer = self.client_buffer[end + 1:]
         return data
 
     def method_CONNECT(self):
         self._connect_target(self.path)
-        _str = HTTPVER+' 200 Connection established\n'+'Proxy-agent: %s\n\n'%VERSION
+        _str = HTTPVER + ' 200 Connection established\n' + 'Proxy-agent: %s\n\n' % VERSION
         self.client.send(_str)
         self.log(_str)
         self.client_buffer = ''
@@ -55,25 +56,25 @@ class ConnectionHandler:
         host = self.path[:i]
         path = self.path[i:]
         self._connect_target(host)
-        _str = '%s %s %s\n'%(self.method, path, self.protocol)+self.client_buffer
+        _str = '%s %s %s\n' % (self.method, path, self.protocol) + self.client_buffer
         self.log(_str)
         self.client_buffer = ''
         self._read_write()
 
     def _connect_target(self, host):
         i = host.find(':')
-        if i!=-1:
-            port = int(host[i+1:])
+        if i != -1:
+            port = int(host[i + 1:])
             host = host[:i]
         else:
             port = 80
-        #(soc_family, _, _, _, address) = socket.getaddrinfo(host, port)[0]
+        # (soc_family, _, _, _, address) = socket.getaddrinfo(host, port)[0]
         (soc_family, _, _, _, address) = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM, socket.SOL_TCP)[0]
         self.target = socket.socket(soc_family)
         self.target.connect(address)
 
     def _read_write(self):
-        time_out_max = self.timeout/3
+        time_out_max = self.timeout / 3
         socs = [self.client, self.target]
         count = 0
         while 1:
@@ -95,18 +96,19 @@ class ConnectionHandler:
             if count == time_out_max:
                 break
 
+
 def start_server(host='localhost', port=8080, IPv6=False, timeout=60, handler=ConnectionHandler):
-    if IPv6==True:
-        soc_type=socket.AF_INET6
+    if IPv6 == True:
+        soc_type = socket.AF_INET6
     else:
-        soc_type=socket.AF_INET
+        soc_type = socket.AF_INET
 
     soc = socket.socket(soc_type)
     ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     try:
         soc.bind((host, port))
-    except socket.error, e:
+    except socket.error as e:
         if e[0] == errno.EADDRINUSE:
             sys.stdout.write("[%s] Port %d in use. Do nothing...\n" % (ts, port))
             sys.stdout.flush()
@@ -114,11 +116,12 @@ def start_server(host='localhost', port=8080, IPv6=False, timeout=60, handler=Co
         else:
             raise
 
-    print "[%s] Serving on %s:%d"%(ts, host, port) #debug
+    print
+    "[%s] Serving on %s:%d" % (ts, host, port)  # debug
     soc.listen(0)
     while 1:
         try:
-            thread.start_new_thread(handler, soc.accept()+(timeout,))
+            thread.start_new_thread(handler, soc.accept() + (timeout,))
         except KeyboardInterrupt:
             ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             sys.stdout.write("[%s] Keyboard Interrupted (ctrl-c). Exiting ...\n" % (ts))
@@ -127,11 +130,12 @@ def start_server(host='localhost', port=8080, IPv6=False, timeout=60, handler=Co
         except:
             raise
 
+
 def cron():
-    cron_script_path="/etc/cron.d/pyProxy"
-    script_full_path=os.path.abspath(__file__)
+    cron_script_path = "/etc/cron.d/pyProxy"
+    script_full_path = os.path.abspath(__file__)
     os.chmod(script_full_path, 0755)
-    if(os.path.isfile(cron_script_path)):
+    if (os.path.isfile(cron_script_path)):
         sys.stdout.write("\"%s\" already exists. Skipping...\n")
         sys.stdout.flush()
         sys.exit(0)
@@ -140,18 +144,19 @@ def cron():
     file.close()
     sys.exit(0)
 
+
 if __name__ == '__main__':
-    host='0.0.0.0'
-    port=8080
+    host = '0.0.0.0'
+    port = 8080
 
     if (len(sys.argv) == 3):
-        host=sys.argv[1]
-        port=int(sys.argv[2])
+        host = sys.argv[1]
+        port = int(sys.argv[2])
     elif (len(sys.argv) == 2):
         if (sys.argv[1] == "cron"):
             cron()
         else:
-            host=sys.argv[1]
-            port=8080
+            host = sys.argv[1]
+            port = 8080
 
     start_server(host=host, port=port)
