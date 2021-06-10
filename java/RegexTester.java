1878a6a5c90528c3481efe71196f0cf83af0c90e
hajime <github@hajigle.com>
2021-06-10 11:47:04 +1000

    got from Srini :) (and modified slightly)

---
+++ java/RegexTester.java
@@ -0,0 +1,31 @@
+import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+public class RegexTester
+{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    try {
+      while (true) {
+          System.err.println("Enter regex pattern:");
+          Pattern pattern = Pattern.compile(sc.nextLine());
+          System.err.println("Enter text:");
+          Matcher matcher = pattern.matcher(sc.nextLine());
+          boolean found = false;
+          while (matcher.find()) {
+            System.out.println("Found the text " + matcher.group() + " starting at index " +
+                matcher.start() + " and ending at index " + matcher.end());
+            found = true;
+          }
+          if (!found) {
+            System.out.println("No match found.");
+          }
+        }
+      }
+    catch (NoSuchElementException e) {
+      System.err.println("Stopping ...");
+    }
+  }
+}
