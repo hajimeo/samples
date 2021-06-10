cf06df03726d076cdbef362eb607c615aeea4d39
hajime <github@hajigle.com>
2021-06-10 11:47:59 +1000

    got from Srini :)

--- java/RegexTester.java
+++ java/RegexTester.java
@@ -1,4 +1,3 @@
-import java.util.NoSuchElementException;
 import java.util.regex.Pattern;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -7,25 +6,20 @@ public class RegexTester
 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    try {
-      while (true) {
-          System.err.println("Enter regex pattern:");
-          Pattern pattern = Pattern.compile(sc.nextLine());
-          System.err.println("Enter text:");
-          Matcher matcher = pattern.matcher(sc.nextLine());
-          boolean found = false;
-          while (matcher.find()) {
-            System.out.println("Found the text " + matcher.group() + " starting at index " +
-                matcher.start() + " and ending at index " + matcher.end());
-            found = true;
-          }
-          if (!found) {
-            System.out.println("No match found.");
-          }
-        }
+    while (true) {
+      System.err.println("Enter regex pattern:");
+      Pattern pattern = Pattern.compile(sc.nextLine());
+      System.err.println("Enter text:");
+      Matcher matcher = pattern.matcher(sc.nextLine());
+      boolean found = false;
+      while (matcher.find()) {
+        System.out.println("Found the text " + matcher.group() + " starting at index " +
+            matcher.start() + " and ending at index " + matcher.end());
+        found = true;
       }
-    catch (NoSuchElementException e) {
-      System.err.println("Stopping ...");
+      if (!found) {
+        System.out.println("No match found.");
+      }
     }
   }
 }
