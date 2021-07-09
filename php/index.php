e78786652462852f4839c5255b47fd52cdf7b680
hajime <github@hajigle.com>
2021-07-09 10:20:52 +1000

    for webhook etc.

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * php -S 0.0.0.0:7999 index.php
+ * curl -X PUT -T test.txt localhost:7999
+ * PHP web server returns 200 for POST, so just handling PUT
+ */
+
+// @see: https://www.php.net/manual/en/features.file-upload.put-method.php
+$save_to = tempnam(sys_get_temp_dir(), 'put_');
+$putdata = fopen("php://input", "r");
+$fp = fopen($save_to, "w");
+while ($data = fread($putdata, 1024))
+    fwrite($fp, $data);
+fclose($fp);
+fclose($putdata);
+
+$stderr = fopen('php://stderr', 'w');
+fwrite($stderr, "Wrote PUT data into '" . $save_to . "'" . PHP_EOL);
+fclose($stderr);
