27a0e6d5cfe525062abeabe28f26c42365e68dc9
hajime <github@hajigle.com>
2021-07-14 18:50:02 +1000

    today i searched with capital DEBUG to find this line, so i changed to capital.

--- java/sslclient/src/main/java/SSLClientWithKeyStore.java
+++ java/sslclient/src/main/java/SSLClientWithKeyStore.java
@@ -85,7 +85,7 @@ public class SSLClientWithKeyStore
     if (debug.equalsIgnoreCase("true")) {
       System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
       System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
-      System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "debug");
+      System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "DEBUG");
     }
 
     boolean ignore = false;
