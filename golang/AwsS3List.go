cce1ad46d07e0a2d039d0bd5710d1306ccd1074c
hajime <github@hajigle.com>
2021-08-16 17:28:20 +1000

    it's probably working

--- golang/AwsS3List.go
+++ golang/AwsS3List.go
@@ -1,19 +1,31 @@
-// https://docs.aws.amazon.com/code-samples/latest/catalog/gov2-s3-ListObjects-ListObjectsv2.go.html
+/*
+https://docs.aws.amazon.com/code-samples/latest/catalog/gov2-s3-ListObjects-ListObjectsv2.go.html
+https://docs.aws.amazon.com/AmazonS3/latest/userguide/ListingKeysUsingAPIs.html
+
+#go mod init github.com/hajimeo/samples/golang
+#go mod tidy
+
+go build -o ../misc/aws-s3-list_$(uname) AwsS3List.go
+env GOOS=linux GOARCH=amd64 go build -o ../misc/aws-s3-list_Linux AwsS3List.go
+
+export AWS_REGION=ap-southeast-2 AWS_ACCESS_KEY_ID=xxx AWS_SECRET_ACCESS_KEY=yyyy
+../misc/aws-s3-list_Darwin -b apac-support-bucket -p node-nxrm-ha1/
+*/
+
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX - License - Identifier: Apache - 2.0
-
-// export AWS_REGION=ap-southeast-2
-// export AWS_ACCESS_KEY_ID=
-// export AWS_SECRET_ACCESS_KEY=
 
 package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"log"
+	"time"
 )
 
 // S3ListObjectsAPI defines the interface for the ListObjectsV2 function.
@@ -37,17 +49,21 @@ func GetObjects(c context.Context, api S3ListObjectsAPI, input *s3.ListObjectsV2
 }
 
 func main() {
-	bucket := flag.String("b", "", "The name of the bucket")
-	prefix := flag.String("p", "", "The name of the prefix")
+	bucket := flag.String("b", "", "The name of the Bucket")
+	prefix := flag.String("p", "", "The name of the Prefix")
+	// Casting/converting int to int32 is somehow hard ...
+	maxkeys := 0
+	flag.IntVar(&maxkeys, "m", 1000, "Integer value for Max Keys (<= 1000)")
 	flag.Parse()
 
 	if *bucket == "" {
-		fmt.Println("You must supply the name of a bucket (-b BUCKET)")
+		log.Printf("ERROR: You must supply the name of a bucket (-b BUCKET_NAME)")
 		return
 	}
 
 	if *prefix == "" {
-		fmt.Println("Not in use yet. No prefix (-p PREFIX)")
+		log.Printf("INFO: No prefix (-p PREFIX_STRING). Getting all ...")
+		time.Sleep(3 * time.Second)
 	}
 
 	cfg, err := config.LoadDefaultConfig(context.TODO())
@@ -58,26 +74,39 @@ func main() {
 	client := s3.NewFromConfig(cfg)
 
 	input := &s3.ListObjectsV2Input{
-		Bucket: bucket,
+		Bucket:  bucket,
+		Prefix:  prefix,
+		MaxKeys: int32(maxkeys),
 	}
 
+	found_ttl := 0
+	found := 0
 	resp, err := GetObjects(context.TODO(), client, input)
 	if err != nil {
-		fmt.Println("Got error retrieving list of objects:")
-		fmt.Println(err)
-		return
+		println("Got error retrieving list of objects:")
+		panic(err.Error())
 	}
 
-	fmt.Println("Objects in " + *bucket + ":")
-
+	fmt.Println("[")
+	found = len(resp.Contents)
+	i := 0
 	for _, item := range resp.Contents {
-		fmt.Println("Name:          ", *item.Key)
-		fmt.Println("Last modified: ", *item.LastModified)
-		fmt.Println("Size:          ", item.Size)
-		fmt.Println("Storage class: ", item.StorageClass)
-		fmt.Println("")
+		j, err := json.Marshal(item)
+		if err != nil {
+			panic(err)
+		}
+		i++
+		fmt.Print("  ", string(j))
+		if !resp.IsTruncated && i < found {
+			fmt.Println(",")
+		}
 	}
 
-	fmt.Println("Found", len(resp.Contents), "items in bucket", *bucket)
+	found_ttl += found
+
 	fmt.Println("")
+	fmt.Println("]")
+
+	println("")
+	log.Printf("Found", found_ttl, "items in bucket", *bucket, "with prefix:", prefix)
 }
