f18002ce540e3925e384591f6373b66abbca1154
hajime <github@hajigle.com>
2021-08-16 12:37:59 +1000

    probably fixed?

--- golang/blobdummygen.go
+++ golang/blobdummygen.go
@@ -1,7 +1,7 @@
-/* DEPRECATED: Use ../java/blobpath, because not tested
+/* May recommend to use ../java/blobpath, because not tested enough
  *
- * 	go build -o blobdummygen_Darwin blobdummygen.go
- * 	env GOOS=linux GOARCH=amd64 go build -o blobdummygen_Linux blobdummygen.go
+ * 	go build -o ../misc/blobdummygen_Darwin blobdummygen.go
+ * 	env GOOS=linux GOARCH=amd64 go build -o ../misc/blobdummygen_Linux blobdummygen.go
  */
 package main
 
@@ -14,11 +14,11 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func _usage() {
 	fmt.Println(`
-*** DEPRECATED ***
 Generate a dummy .property and .byte files
 
 DOWNLOAD and INSTALL:
@@ -26,7 +26,7 @@ DOWNLOAD and INSTALL:
     sudo chmod a+x /usr/local/bin/blobdummygen
     
 USAGE EXAMPLE:
-    cat '{"blob_ref":"default@9C281...:56df5d9d-a...","created_by":"admin","size":1461,"repository_name":"maven-releases","blob_created":"2020-01-23 01:46:07","created_by_ip":"192.168.1.31","content_type":"application/java-archive","name":"com/example/nexus-proxy/1.1/nexus-proxy-1.1.jar","sha1":"9c024..."}' | blobdummygen [out_dir]
+    echo '{"blob_ref":"default@9C281...:56df5d9d-a...","created_by":"admin","size":1461,"repository_name":"maven-releases","blob_created":"2020-01-23 01:46:07","created_by_ip":"192.168.1.31","content_type":"application/java-archive","name":"com/example/nexus-proxy/1.1/nexus-proxy-1.1.jar","sha1":"9c024..."}' | blobdummygen [out_dir]
 `)
 }
 
@@ -72,7 +72,7 @@ func createDummyBlob(path string, size int64) {
 }
 
 func main() {
-	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+	if len(os.Args) > 1 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
 		_usage()
 		os.Exit(0)
 	}
@@ -98,27 +98,34 @@ func main() {
 			os.Exit(3)
 		}
 		//fmt.Println(scanner.Text())
+		// Casting/converting interface to string with Sprintf
 		blobRef := fmt.Sprintf("%s", js["blob_ref"])
 		matches := BLOB_REF_PATTERN.FindStringSubmatch(blobRef)
 		blobName := matches[1]
 		blobId := matches[3]
 		path := blobdir(blobId)
 		finalPath := fullpath(outDir, blobName, path)
-		// TODO: creationTime should be Unix Timestamp with milliseconds
+		// creationTime should be Unix Timestamp with milliseconds
+		created_str := fmt.Sprintf("%s", js["blob_created"])
+		created_time, err := time.Parse("2006-01-02 15:04:03", created_str)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Printf("# Will use the original value: %s\n", js["blob_created"])
+		}
 		// TODO: The first line (modified date) is using last_updated which does not have milliseconds and timezone
-		// TODO: The second line does not match with the first line
+		//       Also, the second line does not match with the first line
 		propsStr := fmt.Sprintf(
 			`#%s,000+0000
 #Mon Jan 01 00:00:00 UTC 2020
 @BlobStore.created-by=%s
 size=%d
 @Bucket.repo-name=%s
-creationTime=%s
+creationTime=%d000
 @BlobStore.created-by-ip=%s
 @BlobStore.content-type=%s
 @BlobStore.blob-name=%s
 sha1=%s
-`, js["last_updated"], js["created_by"], int64(int(js["size"].(float64))), js["repository_name"], js["blob_created"], js["created_by_ip"], js["content_type"], js["name"], js["sha1"])
+`, js["last_updated"], js["created_by"], int64(int(js["size"].(float64))), js["repository_name"], created_time.Unix(), js["created_by_ip"], js["content_type"], js["name"], js["sha1"])
 		os.MkdirAll(finalPath, os.ModePerm)
 		err2 := ioutil.WriteFile(finalPath+"/"+blobId+".properties", []byte(propsStr), 0644)
 		if err2 != nil {
