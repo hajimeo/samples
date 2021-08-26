e6fe5bc0455dd125a4da932ca087a0bf2b79d6a9
hajime <github@hajigle.com>
2021-08-26 16:19:49 +1000

    added another example

--- python/gen_delete.py
+++ python/gen_delete.py
@@ -9,8 +9,21 @@
 import sys, json
 with open(sys.argv[1]) as f:
     jsList = json.load(f)
+
 with open(sys.argv[2], 'w') as w:
     for js in jsList:
         for dup in js['dupe_rids']:
             if dup != js['keep_rid']:
                 w.write("TRUNCATE RECORD %s;\n" % dup)
+sys.exit(0)
+
+# Example: converting list JSON to Dict (Map) for Groovy
+rtn = {}
+for js in jsList:
+    if js['repo_name'] not in rtn:
+        rtn[js['repo_name']] = [js['name']]
+    else:
+        rtn[js['repo_name']].append(js['name'])
+
+with open(sys.argv[2], 'w') as f:
+    json.dump(rtn, f)
