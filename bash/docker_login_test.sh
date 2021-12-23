7e5cc982ccd32dad6ea5b61260df2a5e14f1b536
hajime <github@hajigle.com>
2021-12-23 18:16:50 +1000

    It seems the value of "service" does not matter but fixed.

--- bash/docker_login_test.sh
+++ bash/docker_login_test.sh
@@ -1,4 +1,5 @@
-# hosako "https://dh1.standalone.localdomain:5000/" "http://node-nxrm-ha1.standalone.localdomain:8081/repository/docker-hosted" "http://node-nxrm-ha1.standalone.localdomain:18181/"
+# login_userId docker_registry_host_port1 docker_registry_host_port2 ...
+# eg: "hosako" "https://dh1.standalone.localdomain:5000/" "http://node-nxrm-ha1.standalone.localdomain:8081/repository/docker-hosted" "http://node-nxrm-ha1.standalone.localdomain:18181/"
 
 function _print_token() {
     python -c "import sys,json
@@ -13,7 +14,7 @@ except:
 
 for _h in ${@:2}; do
   echo "# Testing ${_h} ..."
-  _TOKEN="$(curl -s -k -u "$1" "${_h%/}/v2/token?account=$1&client_id=docker&offline_token=true&service=https%3A%2F%2Fdocker-dgsi.rec.etat-ge.ch%2Fv2%2Ftoken" | _print_token)"
+  _TOKEN="$(curl -s -k -u "$1" "${_h%/}/v2/token?account=$1&client_id=docker&offline_token=true" --get --data-urlencode "service=${_h%/}/v2/token" | _print_token)"
   echo "# TODKEN: ${_TOKEN}"
   curl -I -k -H "Authorization: Bearer ${_TOKEN}" -H "Accept: application/json" "${_h%/}/v2/"
 done
