c0a492de2e81e6e44e47a903a1ab1c35e27aea70
hajime <github@hajigle.com>
2022-02-25 16:42:18 +1000

    followed IDE recommendation.

--- golang/ReverseProxy/reverseProxy.go
+++ golang/ReverseProxy/reverseProxy.go
@@ -42,24 +42,24 @@ Also, this script utilise the following environment variables:
 `)
 }
 
-var server_addr string  // Listening server address eg: $(hostname -f):8080
-var proxy_pass string   // forwarding proxy URL
-var scheme string       // http or https (decided by given certificate)
-var dump_body bool      // If true, output
-var save_body_to string // if dump_body is true, save request/response bodies into files
+var serverAddr string // Listening server address eg: $(hostname -f):8080
+var proxyPass string  // forwarding proxy URL
+var scheme string     // http or https (decided by given certificate)
+var dumpBody bool     // If true, output
+var saveBodyTo string // if dumpBody is true, save request/response bodies into files
 
 func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
-		req.URL.Host = server_addr
+		req.URL.Host = serverAddr
 		req.URL.Scheme = scheme
 		req.Header.Set("X-Real-IP", req.RemoteAddr)
 		//req.Header.Set("X-Forwarded-For", req.RemoteAddr)	// TODO: not sure which value to use for client addr
 		req.Header.Set("X-Forwarded-Proto", scheme)
-		reqHeader, err := httputil.DumpRequest(req, dump_body)
+		reqHeader, err := httputil.DumpRequest(req, dumpBody)
 		if err != nil {
 			log.Printf("DumpRequest error: %s\n", err)
 		} else {
-			log.Printf("REQUEST HEAD to %s\n%s\n", proxy_pass, string(reqHeader))
+			log.Printf("REQUEST HEAD to %s\n%s\n", proxyPass, string(reqHeader))
 			req.Body = logBody(req.Body, "REQUEST")
 		}
 		p.ServeHTTP(w, req)
@@ -67,11 +67,11 @@ func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) 
 }
 
 func logResponseHeader(resp *http.Response) (err error) {
-	respHeader, err := httputil.DumpResponse(resp, dump_body)
+	respHeader, err := httputil.DumpResponse(resp, dumpBody)
 	if err != nil {
 		log.Printf("DumpResponse error: %s\n", err)
 	} else {
-		log.Printf("RESPONSE HEAD from %s\n%s\n", proxy_pass, string(respHeader))
+		log.Printf("RESPONSE HEAD from %s\n%s\n", proxyPass, string(respHeader))
 		resp.Body = logBody(resp.Body, "RESPONSE")
 	}
 	return nil
@@ -84,9 +84,9 @@ func logBody(body io.ReadCloser, prefix string) (bodyRewinded io.ReadCloser) {
 		return
 	}
 	var log_msg string
-	if len(save_body_to) > 0 {
+	if len(saveBodyTo) > 0 {
 		time_ms_str := strconv.FormatInt(time.Now().UnixNano()/1000000, 10)
-		fname := save_body_to + "/" + time_ms_str + "_" + prefix + ".out"
+		fname := saveBodyTo + "/" + time_ms_str + "_" + prefix + ".out"
 		err := ioutil.WriteFile(fname, bodyBytes, 0644)
 		if err != nil {
 			panic(err)
@@ -113,14 +113,14 @@ func main() {
 
 	// handling args
 	if len(os.Args) > 1 {
-		server_addr = os.Args[1]
+		serverAddr = os.Args[1]
 	}
 	ptn := "/"
 	if len(os.Args) > 2 {
 		ptn = os.Args[2]
 	}
 	if len(os.Args) > 3 {
-		proxy_pass = os.Args[3]
+		proxyPass = os.Args[3]
 	}
 	certFile := ""
 	if len(os.Args) > 4 {
@@ -130,18 +130,18 @@ func main() {
 	if len(os.Args) > 5 {
 		keyFile = os.Args[5]
 	}
-	dump_body = false
+	dumpBody = false
 	if os.Getenv("_DUMP_BODY") == "true" {
-		dump_body = true
-		log.Printf("dump_body is set to true.\n")
+		dumpBody = true
+		log.Printf("dumpBody is set to true.\n")
 	}
-	save_body_to = os.Getenv("_SAVE_BODY_TO")
-	if len(save_body_to) > 0 {
-		log.Printf("save_body_to is set to #{save_body_to}.\n")
+	saveBodyTo = os.Getenv("_SAVE_BODY_TO")
+	if len(saveBodyTo) > 0 {
+		log.Printf("saveBodyTo is set to #{saveBodyTo}.\n")
 	}
 
 	// start reverse proxy
-	remote, err := url.Parse(proxy_pass)
+	remote, err := url.Parse(proxyPass)
 	if err != nil {
 		panic(err)
 	}
@@ -149,13 +149,13 @@ func main() {
 	proxy.ModifyResponse = logResponseHeader
 	// registering handler function for this pattern
 	http.HandleFunc(ptn, handler(proxy))
-	log.Printf("Starting listener on %s\n\n", server_addr)
+	log.Printf("Starting listener on %s\n\n", serverAddr)
 	if len(keyFile) > 0 {
 		scheme = "https"
-		err = http.ListenAndServeTLS(server_addr, certFile, keyFile, nil)
+		err = http.ListenAndServeTLS(serverAddr, certFile, keyFile, nil)
 	} else {
 		scheme = "http"
-		err = http.ListenAndServe(server_addr, nil)
+		err = http.ListenAndServe(serverAddr, nil)
 	}
 	if err != nil {
 		panic(err)
