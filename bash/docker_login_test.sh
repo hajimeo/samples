d5c9fe0231c4d97b60bec12e80e7bfecce4ba72d
hajime <github@hajigle.com>
2022-03-17 17:12:13 +1000

    now accept username:password format.

--- bash/docker_login_test.sh
+++ bash/docker_login_test.sh
@@ -1,5 +1,5 @@
 # login_userId docker_registry_host_port1 docker_registry_host_port2 ...
-# eg: "hosako" "https://dh1.standalone.localdomain:5000/" "http://node-nxrm-ha1.standalone.localdomain:8081/repository/docker-hosted" "http://node-nxrm-ha1.standalone.localdomain:18181/"
+# eg: "admin:admin123" "https://dh1.standalone.localdomain:5000/" "http://node-nxrm-ha1.standalone.localdomain:8081/repository/docker-hosted" "http://node-nxrm-ha1.standalone.localdomain:18181/"
 
 function _print_token() {
     python -c "import sys,json
@@ -14,7 +14,7 @@ except:
 
 for _h in ${@:2}; do
   echo "# Testing ${_h} ..."
-  _TOKEN="$(curl -s -k -u "$1" "${_h%/}/v2/token?account=$1&client_id=docker&offline_token=true" --get --data-urlencode "service=${_h%/}/v2/token" | _print_token)"
+  _TOKEN="$(curl -s -k -u "$1" "${_h%/}/v2/token?account=${1%%:*}&client_id=docker&offline_token=true" --get --data-urlencode "service=${_h%/}/v2/token" | _print_token)"
   echo "# Testing TOKEN: ${_TOKEN} with curl ${_h%/}/v2/"
   curl -D- -k -H "Authorization: Bearer ${_TOKEN}" -H "Accept: application/json" "${_h%/}/v2/"
 done
