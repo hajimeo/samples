2ff9eee75b4791dd7b56600b55e0bc04f81de06f
hajime <github@hajigle.com>
2022-07-06 14:19:13 +1000

    Incorrect "wait" location.

--- bash/start_my_containers.sh
+++ bash/start_my_containers.sh
@@ -10,6 +10,7 @@ main() {
     bash -x /usr/local/bin/setup_standalone.sh -n node-nxrm-ha1 &>/tmp/setup_standalone_node-nxrm-ha1.out &
     sleep 1
     bash -l /usr/local/bin/setup_standalone.sh -N -n nexus-client &>/tmp/setup_standalone_nexus-client
+    wait
 
     if ! systemctl restart dnsmasq.service; then
         systemctl status dnsmasq.service
@@ -19,7 +20,6 @@ main() {
         systemctl status haproxy.service
         return 1
     fi
-    wait
 }
 
 main
