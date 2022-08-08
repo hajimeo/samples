a65e2df462f7384770a0118972dbcc7d7010466b
hajime <github@hajigle.com>
2022-08-08 15:57:04 +1000

    added ";" just in case

--- bash/_setup_host.sh
+++ bash/_setup_host.sh
@@ -360,12 +360,12 @@ function f_nfs_server() {
     # https://support.sonatype.com/hc/en-us/articles/213465258-Optimizing-Nexus-Disk-IO-Performance
     # NOTE: Trying vers=4.2 automatically falls back to a lower version if not supported
     # TODO: how about ,hard,noacl,proto=tcp,nolock,sync options?
-echo '_DIR="/mnt/nfs"
-mount -t nfs -vvv -o vers=3,noatime,nodiratime,rsize=1048576,wsize=1048576,timeo=600,retrans=2 '$(hostname)':'${_dir%/}' ${_DIR%/}
-mount -t nfs -vvv -o vers=4.2,noatime,nodiratime,rsize=1048576,wsize=1048576,timeo=600,retrans=2 '$(hostname)':'${_dir%/}' ${_DIR%/}
+echo '_DIR="/mnt/nfs";
+mount -t nfs -vvv -o vers=3,noatime,nodiratime,rsize=1048576,wsize=1048576,timeo=600,retrans=2 '$(hostname)':'${_dir%/}' ${_DIR%/};
+mount -t nfs -vvv -o vers=4.2,noatime,nodiratime,rsize=1048576,wsize=1048576,timeo=600,retrans=2 '$(hostname)':'${_dir%/}' ${_DIR%/};
 grep -wE "(nfs|nfs4)" /proc/mounts # to check the nfs version
-dd if=/dev/zero of=/tmp/test.img bs=33M count=1 oflag=dsync
-time (for i in {1..100}; do bash -c "cp -v /tmp/test.img ${_DIR%/}/test_${i}.img && mv -v ${_DIR%/}/test_${i}.img ${_DIR%/}/test_${i}_deleting.img && rm -v -f ${_DIR%/}/test_${i}_deleting.img" & done; wait)
+dd if=/dev/zero of=/tmp/test.img bs=33M count=1 oflag=dsync;
+time (for i in {1..100}; do bash -c "cp -v /tmp/test.img ${_DIR%/}/test_${i}.img && mv -v ${_DIR%/}/test_${i}.img ${_DIR%/}/test_${i}_deleting.img && rm -v -f ${_DIR%/}/test_${i}_deleting.img" & done; wait);
 umount -f -l ${_DIR%/}'
     # Somehow Mac requires '-o resvport,rw'
 }
