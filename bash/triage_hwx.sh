e66151bd100f3571ad9c887c5d46ea16663cdacc
hajime <github@hajigle.com>
2022-09-23 16:29:07 +1000

    comment change

--- bash/triage_hwx.sh
+++ bash/triage_hwx.sh
@@ -76,7 +76,7 @@ function f_check_system() {
     timeout 3 time head -n 1 /dev/./urandom &> ${_work_dir%/}/random.out
     echo '-' &>> ${_work_dir%/}/random.out
     timeout 3 time head -n 1 /dev/random &>> ${_work_dir%/}/random.out
-    lslocks -u > ${_work_dir%/}/lslocks.out    # *local* file system lock (/proc/locks)
+    lslocks -u > ${_work_dir%/}/lslocks.out    # file system lock (/proc/locks)
     cat /sys/fs/cgroup/cpuset/cpuset.cpus &> ${_work_dir%/}/cpuset.cpus.out # for kubernetes pod / docker container
 
     #top -b -n1 -c -o +%MEM  # '+' (default) for reverse order (opposite of 'ps')
