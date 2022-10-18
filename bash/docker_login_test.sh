6036a3cf9eca4ad6a74c06e053294bc8e9e2b049
hajime <github@hajigle.com>
2022-10-19 08:23:17 +1000

    added more comments for myself

--- bash/docker_login_test.sh
+++ bash/docker_login_test.sh
@@ -2,9 +2,11 @@
 # eg: "admin:admin123" "https://dh1.standalone.localdomain:5000/" "http://node-nxrm-ha1.standalone.localdomain:8081/repository/docker-hosted" "http://node-nxrm-ha1.standalone.localdomain:18181/"
 for _h in ${@:2}; do
   _userid="${1%%:*}"
-  echo "# Testing ${_h} with userId: ${_userid} ..."
+  echo "# Accessing ${_h%/}/v2/ should return 401 ..."
+  curl -D/dev/stderr -k -H "Accept: application/json" -L "${_h%/}/v2/"
+  echo "# Generating token from host:${_h} with userId:${_userid} ..."
   # --get --data-urlencode "service=${_h%/}/v2/token"
-  _TOKEN="$(curl -s -k -u "$1" -L "${_h%/}/v2/token?account=${_userid}&client_id=docker&offline_token=true&service=${_h%/}/v2/token" | sed -E 's/.+"token":"([^"]+)".+/\1/')"
-  echo "# Testing TOKEN: ${_TOKEN} with curl ${_h%/}/v2/"
-  curl -D- -k -H "Authorization: Bearer ${_TOKEN}" -H "Accept: application/json" -L "${_h%/}/v2/"
+  _TOKEN="$(curl -D/dev/stderr -s -k -u "$1" -L "${_h%/}/v2/token?account=${_userid}&client_id=docker&offline_token=true&service=${_h%/}/v2/token" | sed -E 's/.+"token":"([^"]+)".+/\1/')"
+  echo "# Testing TOKEN:${_TOKEN} by requesting to ${_h%/}/v2/"
+  curl -D/dev/stderr -k -H "Authorization: Bearer ${_TOKEN}" -H "Accept: application/json" -L "${_h%/}/v2/"
 done
