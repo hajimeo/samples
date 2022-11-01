823c297fce5a60041a47d1a752fa1e68a2e84aa0
hajime <github@hajigle.com>
2022-11-01 12:45:24 +1000

    added findutils for gfind.

--- bash/setup_work_env.sh
+++ bash/setup_work_env.sh
@@ -70,9 +70,9 @@ function f_install_misc() {
   if type brew &>/dev/null; then
     # 'q' is installable with brew
     if type ibrew &>/dev/null; then
-      ibrew install gnu-sed grep q coreutils #Not installable on ARM
+      ibrew install gnu-sed grep q coreutils findutils #Not installable on ARM
     else
-      brew install gnu-sed grep q coreutils
+      brew install gnu-sed grep q coreutils findutils
     fi
   fi
 }
