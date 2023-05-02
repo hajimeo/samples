0449b441081509729ce87ffae69314c0bcef9746
hajime <github@hajigle.com>
2023-05-02 14:09:42 +1000

    Updated to 2.2.3 and modified because of that.

--- java/btrace/examples/Profiling.java
+++ java/btrace/examples/Profiling.java
@@ -1,33 +1,43 @@
 /**
- * Replace clazz values, and timer integer, then
- *
- * _PID=`lsof -ti:10502 -sTCP:LISTEN` && echo ${_PID}
- * export JAVA_HOME="$(dirname $(dirname `readlink /proc/${_PID}/exe`))" && echo $JAVA_HOME
- * ./btrace ${_PID} ./Profiling.java
+ * NOTE:
+ * Do not forget to replace 'clazz' values (and 'OnTimer' integer)
+ * May want to append " | tee btraceProfiling.tsv"
  */
 
-import com.sun.btrace.BTraceUtils;
-import com.sun.btrace.Profiler;
-import com.sun.btrace.annotations.*;
+import org.openjdk.btrace.core.Profiler;
+import org.openjdk.btrace.core.BTraceUtils;
+import org.openjdk.btrace.core.annotations.BTrace;
+import org.openjdk.btrace.core.annotations.Duration;
+import org.openjdk.btrace.core.annotations.Kind;
+import org.openjdk.btrace.core.annotations.Location;
+import org.openjdk.btrace.core.annotations.OnMethod;
+import org.openjdk.btrace.core.annotations.OnTimer;
+import org.openjdk.btrace.core.annotations.ProbeMethodName;
+import org.openjdk.btrace.core.annotations.Property;
+
+import static org.openjdk.btrace.core.BTraceUtils.*;
+
 
 @BTrace
 class Profiling {
+    //final static String CLAZZ="/javax\\.swing\\..*/"; // Doesn't work
+
     @Property
     Profiler p = BTraceUtils.Profiling.newProfiler();
 
     // Can't use variable in 'clazz' because of static variables are not allowed (using BTrace short syntax)
-    @OnMethod(clazz="/javax\\.swing\\..*/", method="/.*/")
-    void entry(@ProbeMethodName(fqn=true) String probeMethod) {
+    @OnMethod(clazz = "/com\\.sonatype\\.insight\\.brain\\.api\\.v2\\.service\\.legal\\.ApiLicenseLegalService/", method = "/.*/")
+    void entry(@ProbeMethodName(fqn = true) String probeMethod) {
         BTraceUtils.Profiling.recordEntry(p, probeMethod);
     }
 
-    @OnMethod(clazz="/javax\\.swing\\..*/", method="/.*/", location=@Location(value=Kind.RETURN))
-    void exit(@ProbeMethodName(fqn=true) String probeMethod, @Duration long duration) {
+    @OnMethod(clazz = "/com\\.sonatype\\.insight\\.brain\\.api\\.v2\\.service\\.legal\\.ApiLicenseLegalService/", method = "/.*/", location = @Location(value = Kind.RETURN))
+    void exit(@ProbeMethodName(fqn = true) String probeMethod, @Duration long duration) {
         BTraceUtils.Profiling.recordExit(p, probeMethod, duration);
     }
 
-    @OnTimer(10000)
+    @OnTimer(20000)
     void timer() {
-        BTraceUtils.Profiling.printSnapshot("performance profile", p);
+        BTraceUtils.Profiling.printSnapshot("# [" + Time.timestamp("yyyy-MM-dd hh:mm:ss") + "] performance profile", p);
     }
 }
