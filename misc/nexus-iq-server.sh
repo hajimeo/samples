079669a100b42485f6c04f497e5ad41f5dc77c7d
hajime <github@hajigle.com>
2023-05-15 11:16:05 +1000

    consistent global variables namings in this script.

--- misc/nexus-iq-server.sh
+++ misc/nexus-iq-server.sh
@@ -17,21 +17,24 @@
 
 NEXUS_IQ_SERVER_HOME=/opt/sonatype/nexus-iq-server
 NEXUS_IQ_SONATYPEWORK=/opt/sonatype/sonatype-work/clm-server
-# The user ID which should be used to run the IQ Server
-# # IMPORTANT - Make sure that the user has the required privileges to write into the IQ Server work directory.
+# The user ID which should be used to run the IQ Server - Make sure that the user has the write privilege for the IQ Server work/log directory.
 RUN_AS_USER="${_NXIQ_USER:-"sonatype"}"
-_SUDO="sudo -u ${RUN_AS_USER}"
-[ "${RUN_AS_USER}" == "${USER}" ] && _SUDO=""
+SUDO="sudo -u ${RUN_AS_USER}"
+[ "${RUN_AS_USER}" == "${USER}" ] && SUDO=""
 
-_JAVA="java"
-[ -n "${JAVA_HOME}" ] && _JAVA="${JAVA_HOME%/}/bin/java"
+JAVA="java"
+# If JAVA_HOME is specified, use it.
+[ -n "${JAVA_HOME}" ] && JAVA="${JAVA_HOME%/}/bin/java"
 
-_XMX="${_NXIQ_HEAPSIZE:-"2G"}"
-JAVA_OPTIONS="-Xms${_XMX} -Xmx${_XMX} -XX:ActiveProcessorCount=2 -XX:-OmitStackTraceInFastThrow"
-#JAVA_OPTIONS="${JAVA_OPTIONS} -XX:OnOutOfMemoryError='kill %p'"    # TODO: need to escape this properly
-JAVA_OPTIONS="${JAVA_OPTIONS} -XX:+UnlockDiagnosticVMOptions -XX:+LogVMOutput -XX:LogFile=${NEXUS_IQ_SONATYPEWORK}/log/jvm.log" # For kill -3 (too see the stdout, use journalctl _PID=<PID>). Also jvm_%p.log works too
+# Minimum recommended options (but not using -XX:-OmitStackTraceInFastThrow as this is for my test server)
+JAVA_OPTIONS="-Xms${_NXIQ_HEAPSIZE:-"2G"} -Xmx${_NXIQ_HEAPSIZE:-"2G"} -XX:ActiveProcessorCount=2"
+
+# For kill -3 (too see the stdout, use journalctl _PID=<PID>). Also jvm_%p.log works too
+JAVA_OPTIONS="${JAVA_OPTIONS} -XX:+UnlockDiagnosticVMOptions -XX:+LogVMOutput -XX:LogFile=${NEXUS_IQ_SONATYPEWORK}/log/jvm.log"
+
 # GC log related options are different by Java version.
-if ${_JAVA} -XX:+PrintFlagsFinal -version 2>/dev/null | grep -q PrintClassHistogramBeforeFullGC; then
+#JAVA_OPTIONS="${JAVA_OPTIONS} -XX:OnOutOfMemoryError='kill %p'"    # TODO: this doesn't work
+if ${JAVA} -XX:+PrintFlagsFinal -version 2>/dev/null | grep -q PrintClassHistogramBeforeFullGC; then
     # probably java 8 (-verbose:gc = -XX:+PrintGC)
     JAVA_OPTIONS="${JAVA_OPTIONS} -XX:+UseG1GC -XX:+ExplicitGCInvokesConcurrent -XX:+PrintGCApplicationStoppedTime -XX:+PrintClassHistogramBeforeFullGC -XX:+TraceClassLoading -XX:+TraceClassUnloading"
     # https://confluence.atlassian.com/confkb/how-to-enable-garbage-collection-gc-logging-300813751.html
@@ -42,6 +45,7 @@ else
     # default, expecting java 11 (more: https://foojay.io/today/embracing-jvm-unified-logging/ )
     JAVA_OPTIONS="${JAVA_OPTIONS} -Xlog:gc*:file=${NEXUS_IQ_SONATYPEWORK}/log/gc.%t.log:time,uptime:filecount=10,filesize=100m,classhisto*=trace"
 fi
+# for testing
 JAVA_OPTIONS="${JAVA_OPTIONS} -Dinsight.threads.monitor=3"
 
 chmod a+w /tmp/nexus_iq_server.out /tmp/nexus_iq_server.err &>/dev/null
@@ -49,14 +53,14 @@ do_start()
 {
     cd ${NEXUS_IQ_SONATYPEWORK}
     # Original uses su -m which can inherits almost all env of current user (eg: root), not sure if it was intentional
-    ${_SUDO} ${_JAVA} ${JAVA_OPTIONS} -jar ${NEXUS_IQ_SERVER_HOME%/}/nexus-iq-server-[0-9.]*-??.jar server ${NEXUS_IQ_SERVER_HOME%/}/config.yml > /tmp/nexus_iq_server.out 2> /tmp/nexus_iq_server.err &
+    ${SUDO} ${JAVA} ${JAVA_OPTIONS} -jar ${NEXUS_IQ_SERVER_HOME%/}/nexus-iq-server-[0-9.]*-??.jar server ${NEXUS_IQ_SERVER_HOME%/}/config.yml > /tmp/nexus_iq_server.out 2> /tmp/nexus_iq_server.err &
     echo "Started nexus-iq-server"
 }
 
 do_console()
 {
     cd ${NEXUS_IQ_SONATYPEWORK}
-    ${_SUDO} ${_JAVA} ${JAVA_OPTIONS} -jar ${NEXUS_IQ_SERVER_HOME%/}/nexus-iq-server-[0-9.]*-??.jar server ${NEXUS_IQ_SERVER_HOME%/}/config.yml
+    ${SUDO} ${JAVA} ${JAVA_OPTIONS} -jar ${NEXUS_IQ_SERVER_HOME%/}/nexus-iq-server-[0-9.]*-??.jar server ${NEXUS_IQ_SERVER_HOME%/}/config.yml
 }
 
 do_stop()
