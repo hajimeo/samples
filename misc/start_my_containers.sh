50c0b306cb19b0a15a1d997465f4fd0668d79be2
hajime <github@hajigle.com>
2023-06-07 12:10:58 +1000

    adding a simple retry

--- misc/start_my_containers.sh
+++ misc/start_my_containers.sh
@@ -4,12 +4,17 @@ export _SERVICE="sonatype"
 
 main() {
     # Run one-by-one
-    for n in node-freeipa node-nxiq node-nxrm-ha1; do
-        bash /usr/local/bin/setup_standalone.sh -n $n &>/tmp/setup_standalone_$n.out
+    bash /usr/local/bin/setup_standalone.sh -N -n node-freeipa &>/tmp/setup_standalone_node-freeipa.out || return 1
+    sleep 1
+    for n in node-nxiq node-nxrm-ha1; do
+        bash /usr/local/bin/setup_standalone.sh -n $n &>/tmp/setup_standalone_$n.out || return 1
         sleep 1
     done
+    bash /usr/local/bin/setup_standalone.sh -n node-nxrm2 &>/tmp/setup_standalone_node-nxrm2.out #|| return 1
+    sleep 1
+
     # If -ha2 -ha3 exist, run but without starting
-    for n in $(docker ps -a --format "{{.Names}}" | grep -E "^(node-nxrm2|node-nxrm-ha2|node-nxrm-ha3|nexus-client)$" | sort); do
+    for n in $(docker ps -a --format "{{.Names}}" | grep -E "^(node-nxrm-ha2|node-nxrm-ha3|nexus-client)$" | sort); do
         bash /usr/local/bin/setup_standalone.sh -N -n $n &>/tmp/setup_standalone_$n.out &
         sleep 1
     done
@@ -25,4 +30,4 @@ main() {
     fi
 }
 
-main
+main || main
