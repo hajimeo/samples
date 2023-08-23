f0e1d322adec8aff64fc2444b6f5a0f0a1391578
hajime <github@hajigle.com>
2023-08-23 10:39:03 +1000

    added duration

--- golang/EchoLines/EchoLines.go
+++ golang/EchoLines/EchoLines.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 var _DEBUG = ""
@@ -23,18 +24,28 @@ Read one file and output only necessary lines.
 # HOW TO USE:
 	echolines some_file1,some_file2 FROM_REGEXP [END_REGEXP] [file_prefix]
 
+NOTE: If no capture group is used in the END_REGEXP, the end line is not echoed.
+
+## NXRM2 thread dumps
 	echolines "wrapper.log.2,wrapper.log.1,wrapper.log" "^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$" "(^\s+class space.+)" > threads.txt
+
+## NXRM3 thread dumps
 	cat "./jvm.log" | echolines "" "^\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d$" "(^\s+class space.+)" "thread_"
 
-	NOTE: if no capture group is used in the END_REGEXP, the end line is not echoed.
+## Get duration of query
+	cat ./nexus.log | ELAPSED_REGEX="^(\d\d\d\d-\d\d-\d\d.\d\d:\d\d:\d\d.\d\d\d)" echolines "" "Preparing:" "Total:"
 
 # ENV VARIABLES:
+	HTML_REMOVE=Y
+		Remove all HTML tags and convert HTML entities
 	INCL_REGEX=<some regex strings>
 		If regular expression is specified, only matching lines are included.
 	EXCL_REGEX=<some regex strings>
 		If regular expression is specified, matching lines are excluded.
-	HTML_REMOVE=Y
-		Remove all HTML tags and convert HTML entities
+	ELAPSED_REGEX=<some datetime like regex string>
+		If provided, calculate the duration between FROM_REGEX matching line and END_REGEXP (or next FROM_REGEX) line
+	ELAPSED_FORMAT=<golang time library acceptable string>
+		Default is "2006-01-02 15:04:05,000" @see: https://pkg.go.dev/time
 END`)
 }
 
@@ -44,6 +55,10 @@ var INCL_REGEX = os.Getenv("INCL_REGEX")
 var INCL_REGEXP *regexp.Regexp
 var EXCL_REGEX = os.Getenv("EXCL_REGEX")
 var EXCL_REGEXP *regexp.Regexp
+var ELAPSED_REGEX = os.Getenv("ELAPSED_REGEX")
+var ELAPSED_REGEXP *regexp.Regexp
+var ELAPSED_FORMAT = os.Getenv("ELAPSED_FORMAT")
+var FROM_DATETIME_STR = ""
 var HTML_REMOVE = os.Getenv("HTML_REMOVE")
 var TAG_REGEXP = regexp.MustCompile(`<[^>]+>`)
 var IN_FILES []string
@@ -71,9 +86,11 @@ func processFile(inFile *os.File) {
 	var err error
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		if !FOUND_FROM_LINE && FROM_REGEXP != nil && FROM_REGEXP.MatchString(line) {
 			FOUND_FROM_LINE = true
 			FOUND_COUNT++
+
 			if len(OUT_PREFIX) > 0 {
 				outFilePath := fmt.Sprintf("%s%d", OUT_PREFIX, FOUND_COUNT)
 				if _, err := os.Stat(outFilePath); err == nil {
@@ -88,6 +105,15 @@ func processFile(inFile *os.File) {
 					log.Fatal(err)
 				}
 			}
+
+			if ELAPSED_REGEXP != nil {
+				elapsedStartMatches := ELAPSED_REGEXP.FindStringSubmatch(line)
+				if len(elapsedStartMatches) > 0 {
+					_dlog(elapsedStartMatches)
+					FROM_DATETIME_STR = elapsedStartMatches[0]
+				}
+			}
+
 			echoLine(line, OUT_FILE)
 			continue
 		}
@@ -97,6 +123,15 @@ func processFile(inFile *os.File) {
 			if len(matches) > 0 {
 				_dlog(matches)
 				FOUND_FROM_LINE = false
+
+				if ELAPSED_REGEXP != nil {
+					elapsedEndMatches := ELAPSED_REGEXP.FindStringSubmatch(line)
+					if len(elapsedEndMatches) > 0 {
+						_dlog(elapsedEndMatches)
+						_ = timeStrDuration(FROM_DATETIME_STR, elapsedEndMatches[0], true)
+					}
+				}
+
 				if len(matches) > 1 {
 					echoLine(strings.Join(matches[1:], ""), OUT_FILE)
 				}
@@ -121,6 +156,24 @@ func processFile(inFile *os.File) {
 	}
 }
 
+func timeStrDuration(startTime string, endTime string, printLine bool) time.Duration {
+	start, err := time.Parse(ELAPSED_FORMAT, startTime)
+	if err != nil {
+		fmt.Println(err)
+	}
+	end := time.Now()
+	if len(endTime) > 0 {
+		end, err = time.Parse(ELAPSED_FORMAT, endTime)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	duration := end.Sub(start)
+	if printLine {
+		fmt.Printf("# start:%s, end:%s, ms:%d\n", startTime, endTime, duration.Milliseconds())
+	}
+	return duration
+}
 func removeHTML(line string) string {
 	return html.UnescapeString(TAG_REGEXP.ReplaceAllString(line, ``))
 }
@@ -159,6 +212,13 @@ func main() {
 
 	if len(EXCL_REGEX) > 0 {
 		EXCL_REGEXP = regexp.MustCompile(EXCL_REGEX)
+	}
+
+	if len(ELAPSED_REGEX) > 0 {
+		ELAPSED_REGEXP = regexp.MustCompile(ELAPSED_REGEX)
+		if len(ELAPSED_FORMAT) == 0 {
+			ELAPSED_FORMAT = "2006-01-02 15:04:05,000"
+		}
 	}
 
 	if IN_FILES == nil || len(IN_FILES) == 0 {
