a160c9c39f99ba537b21f10565dadcb598a6ba38
hajime <github@hajigle.com>
2023-10-31 13:50:54 +1000

    modified for my latest test

--- java/CauseOOMEThread.java
+++ java/CauseOOMEThread.java
@@ -10,19 +10,26 @@ import java.time.format.DateTimeFormatter;
 class CauseOOMEThread {
     private void OOMEing(int maxThread, int waitIntervalMs) throws Exception {
         for (int i = 0; i < maxThread; i++) {
-            if (i % 100 == 0)
-                CauseOOMEThread.log("Creating thread "+i);
-            new Thread(() -> {
-                try {
-                    Thread.sleep(600000);
-                } catch (InterruptedException e) {
-                    CauseOOMEThread.log("Interrupted "+this.toString());
+            try {
+                if (i % 1000 == 0) {
+                    CauseOOMEThread.log("Creating thread " + (i + 1));
+                    if (waitIntervalMs > 0)
+                        Thread.sleep(waitIntervalMs);
                 }
-            }).start();
-            if (waitIntervalMs > 0)
-                Thread.sleep(waitIntervalMs);
+                new Thread(() -> {
+                    try {
+                        Thread.sleep(600000);
+                    } catch (InterruptedException e) {
+                        CauseOOMEThread.log("Interrupted " + this.toString());
+                    }
+                }).start();
+            } catch (OutOfMemoryError e) {
+                e.printStackTrace();
+                log("Completed test. (thread: " + i + ")");
+                System.exit(100);
+            }
         }
-        CauseOOMEThread.log("Finished creating "+maxThread+" threads");
+        CauseOOMEThread.log("Finished creating " + maxThread + " threads");
     }
 
     private static String getCurrentLocalDateTimeStamp() {
@@ -35,19 +42,13 @@ class CauseOOMEThread {
 
     public static void main(String[] args) throws Exception {
         int maxThread = (args.length > 0) ? Integer.parseInt(args[0]) : 100000;
-        int waitIntervalMs = (args.length > 1) ? Integer.parseInt(args[1]) : 0;
+        int waitIntervalMs = (args.length > 1) ? Integer.parseInt(args[1]) : 100;
 
         if (maxThread > 0) {
             CauseOOMEThread cl = new CauseOOMEThread();
-            log("Starting test with max threads=" + maxThread + " ...");
-            try {
-                cl.OOMEing(maxThread, waitIntervalMs);
-                System.exit(0);
-            } catch (OutOfMemoryError e) {
-                e.printStackTrace();
-                log("Completed test. (Free Mem: " + Runtime.getRuntime().freeMemory() + ")");
-                System.exit(100);
-            }
+            log("Starting test with max threads=" + maxThread + " with interval=" + waitIntervalMs + " ms (every 100 threads) ...");
+            cl.OOMEing(maxThread, waitIntervalMs);
+            System.exit(0);
         }
     }
 }
