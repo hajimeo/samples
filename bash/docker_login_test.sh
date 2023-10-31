7d9e002cce10025d6b19cfd8c8f90b1ad62ce087
hajime <github@hajigle.com>
2023-10-31 13:44:13 +1000

    added `-sSf` in the curl commands

--- bash/docker_login_test.sh
+++ bash/docker_login_test.sh
@@ -3,10 +3,10 @@
 for _h in ${@:2}; do
   _userid="${1%%:*}"
   echo "# Accessing ${_h%/}/v2/ should return 401 ..."
-  curl -D/dev/stderr -k -H "Accept: application/json" -L "${_h%/}/v2/"
+  curl -D/dev/stderr -sSf -k -H "Accept: application/json" -L "${_h%/}/v2/"
   echo "# Generating token from host:${_h} with userId:${_userid} ..."
   # --get --data-urlencode "service=${_h%/}/v2/token"
-  _TOKEN="$(curl -D/dev/stderr -s -k -u "$1" -L "${_h%/}/v2/token?account=${_userid}&client_id=docker&offline_token=true&service=${_h%/}/v2/token" | sed -E 's/.+"token":"([^"]+)".+/\1/')"
+  _TOKEN="$(curl -D/dev/stderr -sSf -k -u "$1" -L "${_h%/}/v2/token?account=${_userid}&client_id=docker&offline_token=true&service=${_h%/}/v2/token" | sed -E 's/.+"token":"([^"]+)".+/\1/')"
   echo "# Testing TOKEN:${_TOKEN} by requesting to ${_h%/}/v2/"
-  curl -D/dev/stderr -k -H "Authorization: Bearer ${_TOKEN}" -H "Accept: application/json" -L "${_h%/}/v2/"
+  curl -D/dev/stderr -sSf -k -H "Authorization: Bearer ${_TOKEN}" -H "Accept: application/json" -L "${_h%/}/v2/"
 done
