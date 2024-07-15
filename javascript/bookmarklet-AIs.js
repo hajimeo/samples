0d6153fe174d92219d1da9284cae2af10db59feb
hajime <github@hajigle.com>
2024-07-15 10:46:23 +1000

    TODO

---
+++ javascript/bookmarklet-AIs.js
@@ -0,0 +1,13 @@
+javascript:w = window;
+d = document;
+s = "" + (w.getSelection ? w.getSelection() : d.getSelection ? d.getSelection() : d.selection.createRange().text);
+q = prompt("search word", s);
+if (q != null) {
+    (function (s) {
+        q = encodeURIComponent(s);
+        /* TODO: add Gemini (@gemini, AWS Q and Github Copilot (probably not possible though) */
+        w.open("https://www.perplexity.ai/search?q=" + q, "t_perplex");
+        w.open("https://www.phind.com/search?q=" + q + "&ignoreSearchResults=false&allowMultiSearch=false", "t_phind");
+    })(q)
+}
+void 1;
