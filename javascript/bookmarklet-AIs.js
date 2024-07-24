ab3dc4e2a26198b4bcc3d331012b328c6ce48bb3
hajime <github@hajigle.com>
2024-07-24 11:38:10 +1000

    added todo basically.

--- javascript/bookmarklet-AIs.js
+++ javascript/bookmarklet-AIs.js
@@ -5,7 +5,10 @@ q = prompt("search word", s);
 if (q != null) {
     (function (s) {
         q = encodeURIComponent(s);
-        /* TODO: add Gemini (@gemini, AWS Q and Github Copilot (probably not possible though) */
+        /* TODO: add Gemini (@gemini), AWS Q and Github Copilot (probably not possible though) */
+        w.open("https://gemini.google.com/app", "t_gemini");
+        w.open("https://github.com/search?l=&q=" + q, "t_github");
+        /*w.open("https://docs.aws.amazon.com/search/doc-search.html?searchPath=documentation&searchQuery=" + q, "t_awsdoc");*/
         w.open("https://www.perplexity.ai/search?q=" + q, "t_perplex");
         w.open("https://www.phind.com/search?q=" + q + "&ignoreSearchResults=false&allowMultiSearch=false", "t_phind");
     })(q)
