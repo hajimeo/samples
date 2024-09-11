3cbb9245333054570e164bc719d77a68f1e9822d
hajime <github@hajigle.com>
2024-09-11 14:23:04 +1000

    misc.

--- misc/start_my_containers.sh
+++ misc/start_my_containers.sh
@@ -7,18 +7,20 @@ main() {
     bash /usr/local/bin/setup_standalone.sh -N -n node-freeipa &>/tmp/setup_standalone_node-freeipa.out || return 1
     sleep 1
     for n in node-nxiq node-nxrm-ha1; do
-        bash /usr/local/bin/setup_standalone.sh -n $n &>/tmp/setup_standalone_$n.out || return 1
+        bash /usr/local/bin/setup_standalone.sh -N -n $n &>/tmp/setup_standalone_$n.out #|| return 1
         sleep 1
     done
     bash /usr/local/bin/setup_standalone.sh -n node-nxrm2 &>/tmp/setup_standalone_node-nxrm2.out #|| return 1
     sleep 1
 
     # If -ha2 -ha3 exist, run but without starting
-    for n in $(docker ps -a --format "{{.Names}}" | grep -E "^(node-nxrm-ha2|node-nxrm-ha3|nexus-client)$" | sort); do
-        bash /usr/local/bin/setup_standalone.sh -N -n $n &>/tmp/setup_standalone_$n.out &
-        sleep 1
-    done
-    wait
+    #for n in $(docker ps -a --format "{{.Names}}" | grep -E "^(node-nxrm-ha2|node-nxrm-ha3|nexus-client)$" | sort); do
+    #    bash /usr/local/bin/setup_standalone.sh -N -n $n &>/tmp/setup_standalone_$n.out &
+    #    sleep 1
+    #done
+    #wait
+    bash /usr/local/bin/setup_standalone.sh -N -n nexus-client &>/tmp/setup_standalone_nexus-client.out
+    sleep 60
 
     if ! systemctl restart dnsmasq.service; then
         systemctl status dnsmasq.service
