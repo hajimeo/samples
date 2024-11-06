1b25dd906088cea1a1632d8bc468961a22d131da
hajime <github@hajigle.com>
2024-11-06 16:40:25 +1000

    Now accept the regex with LINE_REGEX env variable.

--- golang/fixed2csv/Fixed2CSV.go
+++ golang/fixed2csv/Fixed2CSV.go
@@ -1,3 +1,9 @@
+/*
+ * This program reads a file which uses fixed width for columns and converts it into CSV format.
+ * If the input file is a JSON file, it converts it into CSV format.
+ * If LINE_REGEX is set, it uses the regex to extract columns.
+ */
+
 package main
 
 import (
@@ -14,6 +20,9 @@ import (
 )
 
 var nonNumericRegexp = regexp.MustCompile(`[^0-9\-.]`)
+var lineRegex = os.Getenv("LINE_REGEX")
+var lineRegexp *regexp.Regexp
+var delimiter = os.Getenv("LINE_DELIMITER")
 
 func nonSpacePos(line string) []int {
 	positions := make([]int, 0)
@@ -34,10 +43,13 @@ func nonSpacePos(line string) []int {
 func line2CSV(line string, positions []int) string {
 	from := 0
 	csvStr := ""
+	if len(delimiter) == 0 {
+		delimiter = ","
+	}
 	for _, pos := range positions {
 		col := line[from:pos]
 		if len(csvStr) > 0 {
-			csvStr += ","
+			csvStr += delimiter
 		}
 		if nonNumericRegexp.MatchString(col) {
 			csvStr += "\"" + strings.Trim(col, " ") + "\""
@@ -45,6 +57,24 @@ func line2CSV(line string, positions []int) string {
 			csvStr += strings.Trim(col, " ")
 		}
 		from = pos
+	}
+	return csvStr
+}
+
+func matches2CSV(matches []string) string {
+	csvStr := ""
+	if len(delimiter) == 0 {
+		delimiter = ","
+	}
+	for _, col := range matches {
+		if len(csvStr) > 0 {
+			csvStr += delimiter
+		}
+		if nonNumericRegexp.MatchString(col) {
+			csvStr += "\"" + strings.Trim(col, " ") + "\""
+		} else {
+			csvStr += strings.Trim(col, " ")
+		}
 	}
 	return csvStr
 }
@@ -68,7 +98,7 @@ func jsList2csv(jsonListObj []interface{}) string {
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide an input file which uses fixed width for columns")
+		fmt.Println("Please provide an input file which uses fixed width for columns or a JSON file.")
 		return
 	}
 	inFile := os.Args[1]
@@ -79,6 +109,10 @@ func main() {
 	jsonKey := ""
 	if len(os.Args) > 3 && len(os.Args[3]) > 0 {
 		jsonKey = os.Args[3]
+	}
+
+	if len(lineRegex) > 0 {
+		lineRegexp = regexp.MustCompile(lineRegex)
 	}
 
 	inputFile, err := os.Open(inFile)
@@ -116,24 +150,43 @@ func main() {
 		csvStr := jsList2csv(jsonObject)
 		_, err = io.WriteString(outputFile, csvStr)
 		if err != nil {
-			fmt.Printf("Error writing %s as JSON into %s file: %v\n", inFile, outputFile, err)
+			fmt.Printf("Error writing %s as JSON into %v file: %v\n", inFile, outputFile, err)
 			return
 		}
 	} else {
 		var positions []int = nil
 		scanner := bufio.NewScanner(inputFile)
+		isFirstLine := true
 		for scanner.Scan() {
 			line := scanner.Text()
 			// Ignore comment lines
 			if strings.HasPrefix(line, "#") {
 				continue
 			}
-			// Assuming the first line is the header, which can be used to determine the width
-			if positions == nil {
+
+			// If first line, assuming as header, which can be used to determine the width
+			if isFirstLine {
 				positions = nonSpacePos(line)
+				if positions != nil && len(positions) > 0 {
+					csvStr := line2CSV(line, positions)
+					fmt.Fprintln(outputFile, csvStr)
+					isFirstLine = false
+					continue
+				}
 			}
-			csvStr := line2CSV(line, positions)
-			fmt.Fprintln(outputFile, csvStr)
+			isFirstLine = false
+			// Decide use regex or treat as fixed width
+			if lineRegexp != nil {
+				matches := lineRegexp.FindStringSubmatch(line)
+				if matches == nil {
+					continue
+				}
+				csvStr := matches2CSV(matches[1:])
+				fmt.Fprintln(outputFile, csvStr)
+			} else {
+				csvStr := line2CSV(line, positions)
+				fmt.Fprintln(outputFile, csvStr)
+			}
 		}
 		// Check for errors.
 		if err := scanner.Err(); err != nil {
