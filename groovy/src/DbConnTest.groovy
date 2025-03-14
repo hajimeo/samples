69c8b664fbcc8bea57dfb2f8d41300a1caf60089
hajime <github@hajigle.com>
2025-03-14 12:01:21 +1000

    just comment change.

--- groovy/src/DbConnTest.groovy
+++ groovy/src/DbConnTest.groovy
@@ -1,6 +1,8 @@
 /*
 sysPath="/opt/sonatype/nexus/system"
-java -Dgroovy.classpath="$(find ${sysPath%/}/org/postgresql/postgresql -type f -name 'postgresql-42.*.jar' | tail -n1)" -jar "${sysPath%/}/org/codehaus/groovy/groovy-all/2.4.17/groovy-all-2.4.17.jar" \
+#groovyJar="${sysPath%/}/org/codehaus/groovy/groovy-all/2.4.17/groovy-all-2.4.17.jar"
+groovyJar="${sysPath%/}/org/codehaus/groovy/groovy/3.0.19/groovy-3.0.19.jar"
+java -Dgroovy.classpath="$(find ${sysPath%/}/org/postgresql/postgresql -type f -name 'postgresql-42.*.jar' | tail -n1)" -jar "${groovyJar}" \
     ./DbConnTest.groovy /nexus-data/etc/fabric/nexus-store.properties
  */
 import org.postgresql.*
