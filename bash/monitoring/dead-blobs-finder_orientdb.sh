4797a5dd11b174ad5262a32a0ad3d0b04b4aea0f
hajime <github@hajigle.com>
2025-04-11 13:12:16 +1000

    example script of utilising the server mode

---
+++ bash/monitoring/dead-blobs-finder_orientdb.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# HOW TO:
+#   bash ./dead-blobs-finder_orientdb.sh "/path/to/blobs/some_blob_store/content/vol-*" | tee -a ./orphaned_result.txt
+
+### Global variables ######################################################
+# If you would like to check par 'vol-<n>' for multi-processing, change this path
+_FILE_BLOB_STORE_VOL_PATH="$1" # eg. /opt/sonatype/nexus3/blobs/default/content/vol-*
+_PARALLELISM="${2:-"3"}"       # Number of parallel processes in xargs
+_ORIENT_DB_API_URL="${3:-"http://localhost:2480/command/component/sql"}"
+###########################################################################
+
+function f_query_db {
+    local _file_path="$1"
+    local _blob_id="$(basename "${_file_path}" ".properties")"
+    local _repo_name="$(sed -n 's/^@Bucket\.repo-name=//p' "${_file_path}")"
+    if [ -z "${_repo_name}" ]; then
+        echo "Invalid file: ${_file_path}"
+        return 1
+    fi
+    local _result_json="$(curl -sSf -u "admin:admin" -X POST "${_ORIENT_DB_API_URL}" -d "SELECT blob_ref FROM asset WHERE bucket.repository_name = '${_repo_name}' AND blob_ref like '%25@${_blob_id}' LIMIT 1")"
+    local _rc="$?"
+    if [ "${_rc}" -ne 0 ] || [ -z "${_result_json}" ]; then
+        echo "[ERROR] querying the database for blob ID: ${_blob_id} failed (${_rc})" >&2
+        return ${_rc}
+    fi
+    if [[ ! "${_result_json}" =~ ${_blob_id} ]]; then
+        echo "Orphaned blob: ${_file_path}"
+    fi
+}
+export -f f_query_db
+export _ORIENT_DB_API_URL
+
+# To avoid checking the newly created/modified files
+touch /tmp/until_$$.tmp
+find ${_FILE_BLOB_STORE_VOL_PATH%/} -maxdepth 2 -name '*.properties' -not -newer /tmp/until_$$.tmp -print0 | head -n1 | xargs -t -0 -I @@ -P${_PARALLELISM} bash -c 'f_query_db "@@"'
