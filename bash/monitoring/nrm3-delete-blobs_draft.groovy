a68ec5265d8f71b8b02ea32214139b306e51dd56
hajime <github@hajigle.com>
2026-01-02 13:10:42 +1000

    Delete blobs only if blob_ref matches.

--- bash/monitoring/nrm3-delete-blobs_draft.groovy
+++ bash/monitoring/nrm3-delete-blobs_draft.groovy
@@ -2,10 +2,6 @@ import groovy.json.JsonSlurper
 import org.sonatype.nexus.common.log.LogManager
 import org.sonatype.nexus.common.log.LoggerLevel
 import groovy.json.JsonOutput
-import org.sonatype.nexus.repository.manager.RepositoryManager
-import org.sonatype.nexus.repository.content.fluent.FluentAsset
-
-FORCE_BROWSE_DELETE = false
 
 def main(params) {
     def blobIdPtnNew = '([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})@([0-9]{4})-([0-9]{2})-([0-9]{2}).([0-9]{2}):([0-9]{2}).*'
@@ -14,17 +10,25 @@ def main(params) {
     def blobIdPtn = '([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}).*'
     def lineCounter = 0
     def deletedNum = 0
-
+    def repositoryManager = container.lookup(org.sonatype.nexus.repository.manager.RepositoryManager)
+    def store = container.lookup(org.sonatype.nexus.blobstore.api.BlobStoreManager.class.name).get(params.blobStore)
+    if (!store) {
+        def logMsg = "params.blobStore: ${params.blobStore} is invalid"
+        if (params.noBsChk) {
+            log.warn(logMsg + ", but continuing as noBsChk is true")
+        } else {
+            log.error(logMsg)
+            return ['error': logMsg]
+        }
+    }
     def blobIDs = (params.blobIDs as String).split(",")
     if (!blobIDs || blobIDs.size() == 0) {
         def logMsg = "params.blobIDs is empty"
         log.error(logMsg)
         return ['error': logMsg]
     }
-    // 'params' should contain 'blobIDs', 'blobStore', 'dryRun', and 'debug'
-    log.info("Checking ${blobIDs.length} blobIds with blobStore: ${params.blobStore},  dryRun: ${params.dryRun}, debug: ${params.debug}")
-
-    def repositoryManager = container.lookup(RepositoryManager)
+    // 'params' should contain 'blobIDs', 'blobStore', 'noBsChk', 'dryRun', and 'debug'
+    log.info("Checking ${blobIDs.length} blobIds with blobStore: ${params.blobStore}, noBsChk: ${params.noBsChk}, dryRun: ${params.dryRun}, debug: ${params.debug}")
 
     for (line in blobIDs) {
         log.debug("line = ${line}")
@@ -62,44 +66,35 @@ def main(params) {
                 }
             }
             log.debug("match[0] = ${match[0]}")
-            def blobIdStr = blobId
+            def blobRefStr = params.blobStore + "@" + blobId
             if (blobCreatedRef) {
-                // Actually no need to append createdRef to blobIdStr for searching but as deleting, to be safe
-                blobIdStr = blobId + "@" + blobCreatedRef.toString()
+                blobRefStr = blobRefStr + "@" + blobCreatedRef.toString()
             }
-            log.debug("Deleting blobId:{}", blobIdStr)
+            log.debug("Deleting blobRef:{}", blobRefStr)
 
             def isDeleted = false
-            repositoryManager.browse().each { repo ->
-                def repoBlobStore = repo.getConfiguration().attributes.storage.blobStoreName
-                if (params.blobStore && params.blobStore.trim().length() > 0 && repoBlobStore != params.blobStore) {
-                    log.debug("Skipping repository {} as blobStore {} does not match {}", repo.name, repoBlobStore, params.blobStore)
+            repositoryManager.browse().each {
+                def repoBlobStore = it.getConfiguration().attributes.storage.blobStoreName
+                if (!params.noBsChk && params.blobStore && params.blobStore.trim().length() > 0 && repoBlobStore != params.blobStore) {
+                    log.debug("Skipping repository {} as blobStore {} does not match {}", it.name, repoBlobStore, params.blobStore)
                     return
                 }
-
-                // Can not use findByBlobRef() as can't guess the blobname before @ to create blobRef.
-                def facet = repo.facet(org.sonatype.nexus.repository.content.facet.ContentFacet) as org.sonatype.nexus.repository.content.facet.ContentFacetSupport
-                facet.stores().assetStore.browseAssets(facet.contentRepositoryId(), null, null, "blob_ref like '%" + blobIdStr + "%'", null, 1).each {
-                    def fa = (FluentAsset) (it instanceof FluentAsset ? (FluentAsset) it : new org.sonatype.nexus.repository.content.fluent.internal.FluentAssetImpl(facet, it))
+                def repositoryId = org.sonatype.nexus.repository.content.store.InternalIds.contentRepositoryId(it).get()
+                def content = it.facet(org.sonatype.nexus.repository.content.facet.ContentFacet)
+                def asset = ((org.sonatype.nexus.repository.content.facet.ContentFacetSupport) content).stores().assetStore.findByBlobRef(repositoryId, org.sonatype.nexus.blobstore.api.BlobRef.parse(blobRefStr))
+                if (asset.isPresent()) {
                     if (!params.dryRun) {
-                        if (FORCE_BROWSE_DELETE) { // Always False for now (Not using currently)
-                            // FluentAsset.delete() generates the AssetDeletedEvent but the Browse is not cleaned with 3.82
-                            def internalAssetId = org.sonatype.nexus.repository.content.store.InternalIds.internalAssetId(fa);
-                            repo.optionalFacet(org.sonatype.nexus.repository.content.browse.BrowseFacet.class).ifPresent({ fc ->
-                                log.info('Deleting browse node: {} internal asset id: {}', fa.path(), internalAssetId)
-                                fc.deleteByAssetIdAndPath(internalAssetId, fa.path())
-                            });
-                        }
-                        // repo.facet(org.sonatype.nexus.repository.content.maintenance.ContentMaintenanceFacet).deleteAsset(it)
-                        fa.delete()
+                        it.facet(org.sonatype.nexus.repository.content.maintenance.ContentMaintenanceFacet).deleteAsset(asset.get())
                     }
-                    log.info("Deleted asset.path:{}, blobId:{} from repository {} (DryRun:{})", fa.path(), blobIdStr, repo.name, params.dryRun)
+                    log.info("Deleted asset:{}, blobRef:{} from repository {} (DryRun:{})", asset, blobRefStr, it.name, params.dryRun)
                     deletedNum++
                     isDeleted = true
+                    return  // break out of repo loop for performance
                 }
             }
             if (!isDeleted) {
-                log.warn("No asset in DB with blobId: {} from any repository", blobIdStr)
+                // Already deleted?
+                log.warn("No asset in DB with blobRef: {} from any repository", blobRefStr)
             }
         }
         catch (Exception e) {
@@ -108,6 +103,7 @@ def main(params) {
                 throw e
             }
         }
+        // NOTE: not doing blobStoreIntegrityCheck as wouldn't need for this script
     }
     log.info("Deleted {}/{}", deletedNum, blobIDs.size())
     return ['checked': lineCounter, 'deleted': deletedNum, 'dryRun': params.dryRun]
