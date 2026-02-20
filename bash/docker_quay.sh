bec09510cb6ba9bdeba1f188543bc8edba81374e
hajime <github@hajigle.com>
2026-02-20 18:16:08 +1000

    bash script for docker tags/list

---
+++ bash/docker_quay.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+QUAY_IMAGE="$1"
+QUAY_API_PATH="$2"  # Optional, default to "/tags/list"
+QUAY_USERNAME="$3"
+QUAY_PASSWORD="$4"
+QUAY_URL="https://quay.io/"
+
+function test_quay() {
+    local _img="${1:-"${QUAY_IMAGE}"}"   #"hosako/test-repo"
+    local _path="${2:-"${QUAY_API_PATH:-"/tags/list"}"}"
+    local _username="${2:-"${QUAY_USERNAME}"}"
+    local _password="${3:-"${QUAY_PASSWORD}"}"
+    if [ -n "${_password}" ]; then
+        _password=":${_password}"
+    fi
+    local _query="scope=repository:${_img#/}:pull"
+    echo "# Getting token for user ${_username} for ${_query} ..." >&2
+    local _token="$(curl -sf -u "${_username}${_password}" -L "${QUAY_URL%/}/v2/auth" --get --data-urlencode "${_query}" --data-urlencode "service=quay.io" | sed -E 's/.+"token":"([^"]+)".+/\1/')"
+
+    if [ -z "${_token}" ]; then
+      echo "Failed to get token for user ${_username}" >&2
+      return 1
+    fi
+
+    echo "# Testing Token against ${_img} with -D ./test_quay-header.out -o ./test_quay-result.json" >&2
+    curl -sf -k -H "Authorization: Bearer ${_token}" -H "Accept: application/json" -k -L "${QUAY_URL%/}/v2/${_img#/}${_path}" -D ./test_quay-header.out -o ./test_quay-result.json
+    return $?
+}
+
+test_quay
