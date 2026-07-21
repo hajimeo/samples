5577849c9d27d0a111652f01d84a8de03bb85279
hajime <github@hajigle.com>
2026-07-21 19:30:07 +1000

    draft

---
+++ bash/monitoring/nrm3-rebuild-search.sh
@@ -0,0 +1,130 @@
+#!/usr/bin/env bash
+usage() {
+    cat << 'EOF'
+PURPOSE:
+    Rebuild specific components' search indexes.
+
+NOTE:
+    This script is not guaranteed to work for all Nexus versions. Please test this script on a test system first.
+
+REQUIREMENTS:
+    'curl' for uploading the script and initiating the script
+    'nexus.scripts.allowCreation=true' in nexus.properties
+
+EXAMPLES:
+    cd /some/workDir
+    curl --compressed -o nrm3-undelete.sh -L https://raw.githubusercontent.com/sonatype/nexus-monitoring/main/scripts/nrm3-rebuild-search.sh
+    export _ADMIN_USER="admin" _ADMIN_PWD="******" _NEXUS_URL="http://localhost:8081/" #_DRY_RUN="true" _DEBUG="true"
+    bash ./nrm3-rebuild-search.sh -I      # only once to register the script
+    bash ./nrm3-rebuild-search.sh -r {repo-name} [-c {component-name}] [-n {artifact-name}] [-v {artifact-version}] [-b {batch-size}]
+
+OPTIONS:
+    -I  Installing the groovy script (only once per Nexus)
+    -r  repository name
+    -c  component/group/namespace name
+    -n  artifact name
+    -v  artifact version
+    -b  batch size (default 100)
+EOF
+}
+
+
+### Global variables #################
+: "${_ADMIN_USER:="admin"}"
+: "${_ADMIN_PWD:="admin123"}"
+: "${_NEXUS_URL:="http://localhost:8081/"}"
+: "${_INSTALL:=""}"
+: "${_TMP:="/tmp"}"
+_SCRIPT_NAME="rebuildSpecificSearch"
+# Below is used in the POST json string
+: "${_REPO_NAME:=""}"
+: "${_COMPONENT:=""}"
+: "${_NAME:=""}"
+: "${_VERSION:=""}"
+: "${_BATCH_SIZE:="100"}"
+: "${_DRY_RUN:="false"}"
+: "${_DEBUG:="false"}"
+
+
+### Functions ########################
+function genScriptContent() {
+    # How to generate the below (NOTE: remove the beginning and ending double-quotes):
+    #   python3 -c "import sys,json;print(json.dumps(open('nrm3-rebuild-search_draft.groovy').read()))"
+    cat <<'EOF'
+import groovy.json.JsonSlurper\nimport groovy.json.JsonOutput\nimport org.sonatype.nexus.common.log.LogManager\nimport org.sonatype.nexus.common.log.LoggerLevel\nimport static org.sonatype.nexus.logging.task.TaskLoggingMarkers.TASK_LOG_ONLY\n\n\ndef processRepository(final repository, final String filterCondition, final batchSize) {\n    try {\n        def fluentComponents = repository.facet(org.sonatype.nexus.repository.content.facet.ContentFacet.class).components();\n        def filteredQuery = fluentComponents.byFilter(filterCondition, java.util.Collections.emptyMap());\n\n        return reindexFilteredComponents(repository, filteredQuery, batchSize);\n    }\n    catch (Exception e) {\n        log.error(\"Error processing repository {}: {}\", repository.getName(), e.getMessage(), e);\n        return 0;\n    }\n}\n\ndef reindexFilteredComponents(\n        final repository,\n        final filteredQuery,\n        final batchSize) {\n    long processed = 0;\n    def components = filteredQuery.browse(batchSize, null);\n    if (components.isEmpty()) {\n        log.info(TASK_LOG_ONLY, \"No components matching filter in repository {}\", repository.getName());\n        return 0;\n    }\n    def sqlSearchIndexService = container.lookup(org.sonatype.nexus.repository.search.sql.index.SqlSearchIndexService.class.name);\n    while (!components.isEmpty()) {\n        sqlSearchIndexService.indexBatch(components, repository);\n        processed += components.size();\n        log.info(TASK_LOG_ONLY, \"Re-indexed {} components in {}\", processed, repository.getName());\n        components = filteredQuery.browse(batchSize, components.nextContinuationToken());\n    }\n\n    log.info(TASK_LOG_ONLY, \"Completed re-indexing {} components for repository {}\", processed, repository.getName());\n    return processed;\n}\n\ndef main(params) {\n    // params = {\"repo_name\":\"maven-releases\",\"component\":\"com.group\",\"name\":\"artifact\",\"version\":\"0.0.1\",\"batch_size\":100,\"dryRun\":false,\"debug\":false}\n    if (params.repo_name == null || params.repo_name.isEmpty()) {\n        log.warn(\"Repository name is not provided. Please provide a valid repository name.\")\n        return\n    }\n    // component is also mandatory for now (if no component, not much different form normal task)\n    if (params.component == null || params.component.isEmpty()) {\n        log.warn(\"Component/Group/Namespace is not provided. Please provide a valid component.\")\n        return\n    }\n    def processed = 0L\n    def batchSize = (params.batch_size != null && params.batch_size > 0) ? params.batch_size : 100\n    def compOperator = (params.component?.contains('%')) ? 'like' : '='\n    def condition = \"namespace ${compOperator} '${params.component}'\"\n    if (params.name != null && !params.name.isEmpty()) {\n        def nameOperator = (params.name?.contains('%')) ? 'like' : '='\n        condition += \" and name ${nameOperator} '${params.name}'\"\n    }\n    if (params.version != null && !params.version.isEmpty()) {\n        def versionOperator = (params.version?.contains('%')) ? 'like' : '='\n        condition += \" and version ${versionOperator} '${params.version}'\"\n    }\n    try {\n        repository.repositoryManager.browse().each { repo ->\n            if (repo.name == params.repo_name) {\n                log.info(TASK_LOG_ONLY, 'Checking repository: {}, type: {}, format: {}', repo.name, repo.type.value, repo.format.value)\n                processed = processRepository(repo, condition, batchSize)\n            }\n        }\n    }\n    catch (Exception e) {\n        log.warn('Exception details: {}', e.getMessage())\n        log.debug(TASK_LOG_ONLY, \"{}\", e.printStackTrace())\n        if (params.dryRun) {    // If dryRun stops at the exception\n            throw e\n        }\n    }\n    return [processed: processed, condition: condition, batchSize: batchSize, dryRun: params.dryRun, debug: params.debug]\n}\n\nlog.info(\"Rebuilding specific search index script started.\")\ndef logMgr = container.lookup(LogManager.class.name) as LogManager\ndef currentLevel = logMgr.getLoggerLevel(\"org.sonatype.nexus.internal.script.ScriptTask\")\ntry {\n    def params = (args) ? new JsonSlurper().parseText(args as String) : null\n    if (params.debug && (params.debug == \"true\" || params.debug == true)) {\n        logMgr.setLoggerLevel(\"org.sonatype.nexus.internal.script.ScriptTask\", LoggerLevel.DEBUG)\n        logMgr.setLoggerLevel(\"org.sonatype.nexus.script.plugin.internal.rest.ScriptResource\", LoggerLevel.DEBUG)\n        //logMgr.setLoggerLevel(\"org.sonatype.nexus.content.maven.store.Maven2ComponentDAO.browseComponents\", LoggerLevel.DEBUG)\n        logMgr.setLoggerLevel(\"org.sonatype.nexus.repository.search.sql.store.SearchTableDAO.saveBatch\", LoggerLevel.DEBUG)\n    }\n    return JsonOutput.toJson(main(params))\n} finally {\n    logMgr.setLoggerLevel(\"org.sonatype.nexus.internal.script.ScriptTask\", currentLevel)\n    logMgr.setLoggerLevel(\"org.sonatype.nexus.script.plugin.internal.rest.ScriptResource\", currentLevel)\n    logMgr.setLoggerLevel(\"org.sonatype.nexus.repository.search.sql.store.SearchTableDAO.saveBatch\", currentLevel)\n    log.info(\"Rebuilding specific search index script completed.\")\n}\n
+EOF
+}
+
+main() {
+    local _repo_name="${1:-"${_REPO_NAME}"}"
+    local _component="${2:-"${_COMPONENT}"}"
+    local _name="${3:-"${_NAME}"}"
+    local _version="${4:-"${_VERSION}"}"
+    local _batch_size="${5:-"${_BATCH_SIZE:-100}"}"
+    local _install="${6:-"${_INSTALL}"}"
+
+    if [[ "${_install}" =~ ^[yY] ]]; then
+        echo "{\"name\":\"${_SCRIPT_NAME}\",\"content\":\"$(genScriptContent)\",\"type\":\"groovy\"}" > ${_TMP%/}/${_SCRIPT_NAME}.json || return $?
+        # Delete if exists, and not showing error if not exists, but if install fails, it will show error and exit
+        curl -s -L -k -u "${_ADMIN_USER}:${_ADMIN_PWD}" -H 'Content-Type: application/json' "${_NEXUS_URL%/}/service/rest/v1/script/${_SCRIPT_NAME}" -X DELETE
+        sleep 1
+        curl -sSf -L -k -u "${_ADMIN_USER}:${_ADMIN_PWD}" -H 'Content-Type: application/json' "${_NEXUS_URL%/}/service/rest/v1/script" -d@${_TMP%/}/${_SCRIPT_NAME}.json || return $?
+        echo "Script installed" >&2
+        sleep 1
+    fi
+
+    if [ -z "${_repo_name}" ]; then
+        echo "No repository name specified (-r)" >&2
+        return
+    fi
+
+    if [ -z "${_component}" ]; then
+        # At least component is required
+        echo "No component (namespace) specified (-c)" >&2
+        return
+    fi
+
+    curl -sSf -L -k -u "${_ADMIN_USER}:${_ADMIN_PWD}" -H 'Content-Type: application/json' "${_NEXUS_URL%/}/service/rest/v1/script/${_SCRIPT_NAME}/run" -d'{"repo_name":"'${_repo_name}'","component":"'${_component}'","name":"'${_name}'","version":"'${_version}'","batch_size":'${_batch_size}',"dryRun":'${_DRY_RUN:-"false"}',"debug":'${_DEBUG:-"false"}'}'
+}
+
+
+if [ "$0" = "${BASH_SOURCE[0]}" ]; then
+    if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "help" ]; then
+        usage
+        exit 0
+    fi
+
+    while getopts "Ir:c:n:v:b:h" opts; do
+        case $opts in
+        I)
+            _INSTALL="Y"
+            ;;
+        r)
+            [ -n "$OPTARG" ] && _REPO_NAME="$OPTARG"
+            ;;
+        c)
+            [ -n "$OPTARG" ] && _COMPONENT="$OPTARG"
+            ;;
+        n)
+            [ -n "$OPTARG" ] && _NAME="$OPTARG"
+            ;;
+        v)
+            [ -n "$OPTARG" ] && _VERSION="$OPTARG"
+            ;;
+        b)
+            [ -n "$OPTARG" ] && _BATCH_SIZE="$OPTARG"
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "$opts $OPTARG is not supported. Ignored." >&2
+            ;;
+        esac
+    done
+
+    main
+    echo "" >&2
+    echo "Completed." >&2
+fi
---
+++ bash/monitoring/nrm3-rebuild-search_draft.groovy
@@ -0,0 +1,106 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import org.sonatype.nexus.common.log.LogManager
+import org.sonatype.nexus.common.log.LoggerLevel
+import static org.sonatype.nexus.logging.task.TaskLoggingMarkers.TASK_LOG_ONLY
+
+
+def processRepository(final repository, final String filterCondition, final batchSize, final dryRun) {
+    try {
+        def fluentComponents = repository.facet(org.sonatype.nexus.repository.content.facet.ContentFacet.class).components();
+        def filteredQuery = fluentComponents.byFilter(filterCondition, java.util.Collections.emptyMap());
+
+        return reindexFilteredComponents(repository, filteredQuery, batchSize, dryRun);
+    }
+    catch (Exception e) {
+        log.error("Error processing repository {}: {}", repository.getName(), e.getMessage(), e);
+        return 0;
+    }
+}
+
+def reindexFilteredComponents(
+        final repository,
+        final filteredQuery,
+        final batchSize,
+        final dryRun) {
+    long processed = 0;
+    Collection<org.sonatype.nexus.repository.content.fluent.FluentComponent> components = filteredQuery.browse(batchSize, null);
+    if (components.isEmpty()) {
+        log.info(TASK_LOG_ONLY, "No components matching filter in repository {}", repository.getName());
+        return 0;
+    }
+    def sqlSearchIndexService = container.lookup(org.sonatype.nexus.repository.search.sql.index.SqlSearchIndexService.class.name);
+    while (!components.isEmpty()) {
+        if (dryRun) {
+            log.info(TASK_LOG_ONLY, "Dry run: would re-index {} components in {}", components.size(), repository.getName());
+        } else {
+            sqlSearchIndexService.indexBatch(components, repository);
+        }
+        processed += components.size();
+        log.info(TASK_LOG_ONLY, "Re-indexed {} components in {} (dryRun: {})", processed, repository.getName(), dryRun);
+        components = filteredQuery.browse(batchSize, components.nextContinuationToken());
+    }
+
+    log.info(TASK_LOG_ONLY, "Completed re-indexing {} components for repository {}", processed, repository.getName());
+    return processed;
+}
+
+def main(params) {
+    // params = {"repo_name":"maven-releases","component":"com.group","name":"artifact","version":"0.0.1","batch_size":100,"dryRun":false,"debug":false}
+    if (params.repo_name == null || params.repo_name.isEmpty()) {
+        log.warn("Repository name is not provided. Please provide a valid repository name.")
+        return
+    }
+    // component is also mandatory for now (if no component, not much different form normal task)
+    if (params.component == null || params.component.isEmpty()) {
+        log.warn("Component/Group/Namespace is not provided. Please provide a valid component.")
+        return
+    }
+    def processed = 0L
+    def batchSize = (params.batch_size != null && params.batch_size > 0) ? params.batch_size : 100
+    def compOperator = (params.component?.contains('%')) ? 'like' : '='
+    def condition = "namespace ${compOperator} '${params.component}'"
+    if (params.name != null && !params.name.isEmpty()) {
+        def nameOperator = (params.name?.contains('%')) ? 'like' : '='
+        condition += " and name ${nameOperator} '${params.name}'"
+    }
+    if (params.version != null && !params.version.isEmpty()) {
+        def versionOperator = (params.version?.contains('%')) ? 'like' : '='
+        condition += " and version ${versionOperator} '${params.version}'"
+    }
+    try {
+        repository.repositoryManager.browse().each { repo ->
+            if (repo.name == params.repo_name) {
+                log.info(TASK_LOG_ONLY, 'Checking repository: {}, type: {}, format: {}', repo.name, repo.type.value, repo.format.value)
+                processed = processRepository(repo, condition, batchSize, params.dryRun)
+            }
+        }
+    }
+    catch (Exception e) {
+        log.warn('Exception details: {}', e.getMessage())
+        log.debug(TASK_LOG_ONLY, "{}", e.printStackTrace())
+        if (params.dryRun) {    // If dryRun stops at the exception
+            throw e
+        }
+    }
+    return [processed: processed, condition: condition, batchSize: batchSize, dryRun: params.dryRun, debug: params.debug]
+}
+
+log.info("Rebuilding specific search index script started.")
+def logMgr = container.lookup(LogManager.class.name) as LogManager
+def currentLevel = logMgr.getLoggerLevel("org.sonatype.nexus.internal.script.ScriptTask")
+try {
+    def params = (args) ? new JsonSlurper().parseText(args as String) : null
+    if (params.debug && (params.debug == "true" || params.debug == true)) {
+        logMgr.setLoggerLevel("org.sonatype.nexus.internal.script.ScriptTask", LoggerLevel.DEBUG)
+        logMgr.setLoggerLevel("org.sonatype.nexus.script.plugin.internal.rest.ScriptResource", LoggerLevel.DEBUG)
+        //logMgr.setLoggerLevel("org.sonatype.nexus.content.maven.store.Maven2ComponentDAO.browseComponents", LoggerLevel.DEBUG)
+        logMgr.setLoggerLevel("org.sonatype.nexus.repository.search.sql.store.SearchTableDAO.saveBatch", LoggerLevel.DEBUG)
+    }
+    return JsonOutput.toJson(main(params))
+} finally {
+    logMgr.setLoggerLevel("org.sonatype.nexus.internal.script.ScriptTask", currentLevel)
+    logMgr.setLoggerLevel("org.sonatype.nexus.script.plugin.internal.rest.ScriptResource", currentLevel)
+    logMgr.setLoggerLevel("org.sonatype.nexus.repository.search.sql.store.SearchTableDAO.saveBatch", currentLevel)
+    log.info("Rebuilding specific search index script completed.")
+}
